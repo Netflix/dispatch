d2f6ecacecc46a7888643c0badea515398ea828d
kevgliss <kevgliss@gmail.com>
2020-02-20 11:09:32 -0800

    Refactors syncing of tasks, also fixes a bug were only the first task would be synced.

--- src/dispatch/task/flows.py
+++ src/dispatch/task/flows.py
@@ -10,9 +10,17 @@ import logging
 from collections import defaultdict
 from datetime import datetime
 
-from dispatch.config import INCIDENT_PLUGIN_EMAIL_SLUG
+from dispatch.config import (
+    INCIDENT_PLUGIN_EMAIL_SLUG,
+    INCIDENT_PLUGIN_CONVERSATION_SLUG,
+    INCIDENT_RESOURCE_INCIDENT_TASK,
+)
 from dispatch.messaging import INCIDENT_TASK_REMINDER
 from dispatch.plugins.base import plugins
+from dispatch.messaging import INCIDENT_TASK_NEW_NOTIFICATION, INCIDENT_TASK_RESOLVED_NOTIFICATION
+from dispatch.task.models import TaskStatus
+from dispatch.task import service as task_service
+
 
 log = logging.getLogger(__name__)
 
@@ -46,3 +54,78 @@ def create_reminder(db_session, assignee, tasks):
     for task in tasks:
         task.last_reminder_at = datetime.utcnow()
         db_session.commit()
+
+
+def send_task_notification(conversation_id, notification_type, assignees, description, weblink):
+    """Sends a task notification."""
+    # we send a notification to the incident conversation
+    notification_text = "Incident Notification"
+    notification_type = "incident-notification"
+    convo_plugin = plugins.get(INCIDENT_PLUGIN_CONVERSATION_SLUG)
+    convo_plugin.send(
+        conversation_id,
+        notification_text,
+        notification_type,
+        task_assignees=assignees,
+        task_description=description,
+        task_weblink=weblink,
+    )
+
+
+def create_or_update_task(db_session, incident, task: dict, notify: bool = False):
+    """Creates a new task database or updates an existing one."""
+    # TODO we should standarize this interface (kglisson)
+    creator = task["owner"]
+    assignees = ", ".join(task["assignees"])
+    description = task["description"][0]
+    status = TaskStatus.open if not task["status"] else TaskStatus.resolved
+    resource_id = task["id"]
+    weblink = task["web_link"]
+
+    incident_task = task_service.get_by_resource_id(db_session=db_session, resource_id=task["id"])
+    if incident_task:
+        if status == TaskStatus.open:
+            # we don't need to take any actions if the status of the task in the collaboration doc is open
+            return
+        else:
+            if incident_task.status == TaskStatus.resolved:
+                # we don't need to take any actions if the task has already been marked as resolved in the database
+                return
+            else:
+                # we mark the task as resolved in the database
+                incident_task.status = TaskStatus.resolved
+                db_session.add(incident_task)
+                db_session.commit()
+
+                if notify:
+                    send_task_notification(
+                        incident.conversation.channel_id,
+                        INCIDENT_TASK_RESOLVED_NOTIFICATION,
+                        assignees,
+                        description,
+                        weblink,
+                    )
+    else:
+        # we add the task to the incident
+        task = task_service.create(
+            db_session=db_session,
+            creator=creator,
+            assignees=assignees,
+            description=description,
+            status=status,
+            resource_id=resource_id,
+            resource_type=INCIDENT_RESOURCE_INCIDENT_TASK,
+            weblink=weblink,
+        )
+        incident.tasks.append(task)
+        db_session.add(incident)
+        db_session.commit()
+
+        if notify:
+            send_task_notification(
+                incident.conversation.channel_id,
+                INCIDENT_TASK_NEW_NOTIFICATION,
+                assignees,
+                description,
+                weblink,
+            )
