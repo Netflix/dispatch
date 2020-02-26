b52f6b6570e457374d9d8e47ee06a3eee7ac7113
Daniel Gallagher <daniel.gallagher@elastic.co>
2020-02-26 16:22:50 -0500

    Revert to original

--- src/dispatch/config.py
+++ src/dispatch/config.py
@@ -35,101 +35,101 @@ except Exception:
     from starlette.datastructures import Secret
 
 
-#config = Config(".env")
+config = Config(".env")
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", default=logging.WARNING)
-ENV = os.getenv("ENV", default="local")
+LOG_LEVEL = config("LOG_LEVEL", default=logging.WARNING)
+ENV = config("ENV", default="local")
 
-DISPATCH_HELP_EMAIL = os.getenv("DISPATCH_HELP_EMAIL", default="")
-DISPATCH_HELP_SLACK_CHANNEL = os.getenv("DISPATCH_HELP_SLACK_CHANNEL", default="")
+DISPATCH_HELP_EMAIL = config("DISPATCH_HELP_EMAIL")
+DISPATCH_HELP_SLACK_CHANNEL = config("DISPATCH_HELP_SLACK_CHANNEL")
 
 # authentication
-JWKS_URL = os.getenv("JWKS_URL", default="")
+JWKS_URL = config("JWKS_URL")
 
 DEFAULT_STATIC_DIR = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), "static/dispatch/dist"
 )
-STATIC_DIR = os.getenv("STATIC_DIR", default=DEFAULT_STATIC_DIR)
+STATIC_DIR = config("STATIC_DIR", default=DEFAULT_STATIC_DIR)
 
 # metrics
-METRIC_PROVIDERS = os.getenv("METRIC_PROVIDERS", cast=CommaSeparatedStrings, default="")
+METRIC_PROVIDERS = config("METRIC_PROVIDERS", cast=CommaSeparatedStrings, default="")
 
 # sentry middleware
-SENTRY_DSN = os.getenv("SENTRY_DSN", cast=Secret, default=None)
+SENTRY_DSN = config("SENTRY_DSN", cast=Secret, default=None)
 
 # database
-DATABASE_HOSTNAME = os.getenv("DATABASE_HOSTNAME", default="")
-DATABASE_CREDENTIALS = os.getenv("DATABASE_CREDENTIALS", cast=Secret)
-DATABASE_NAME = os.getenv("DATABASE_NAME", default="dispatch")
-DATABASE_PORT = os.getenv("DATABASE_PORT", default="5432")
+DATABASE_HOSTNAME = config("DATABASE_HOSTNAME")
+DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret)
+DATABASE_NAME = config("DATABASE_NAME", default="dispatch")
+DATABASE_PORT = config("DATABASE_PORT", default="5432")
 SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DATABASE_CREDENTIALS}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # incident plugins
-INCIDENT_PLUGIN_CONTACT_SLUG = os.getenv("INCIDENT_PLUGIN_CONTACT_SLUG", default="slack-contact")
-INCIDENT_PLUGIN_CONVERSATION_SLUG = os.getenv(
+INCIDENT_PLUGIN_CONTACT_SLUG = config("INCIDENT_PLUGIN_CONTACT_SLUG", default="slack-contact")
+INCIDENT_PLUGIN_CONVERSATION_SLUG = config(
     "INCIDENT_PLUGIN_CONVERSATION_SLUG", default="slack-conversation"
 )
-INCIDENT_PLUGIN_DOCUMENT_SLUG = os.getenv(
+INCIDENT_PLUGIN_DOCUMENT_SLUG = config(
     "INCIDENT_PLUGIN_DOCUMENT_SLUG", default="google-docs-document"
 )
-INCIDENT_PLUGIN_DOCUMENT_RESOLVER_SLUG = os.getenv(
+INCIDENT_PLUGIN_DOCUMENT_RESOLVER_SLUG = config(
     "INCIDENT_PLUGIN_DOCUMENT_RESOLVER_SLUG", default="dispatch-document-resolver"
 )
-INCIDENT_PLUGIN_EMAIL_SLUG = os.getenv(
+INCIDENT_PLUGIN_EMAIL_SLUG = config(
     "INCIDENT_PLUGIN_EMAIL_SLUG", default="google-gmail-conversation"
 )
-INCIDENT_PLUGIN_GROUP_SLUG = os.getenv(
+INCIDENT_PLUGIN_GROUP_SLUG = config(
     "INCIDENT_PLUGIN_GROUP_SLUG", default="google-group-participant-group"
 )
-INCIDENT_PLUGIN_PARTICIPANT_SLUG = os.getenv(
+INCIDENT_PLUGIN_PARTICIPANT_SLUG = config(
     "INCIDENT_PLUGIN_PARTICIPANT_SLUG", default="dispatch-participants"
 )
-INCIDENT_PLUGIN_STORAGE_SLUG = os.getenv(
+INCIDENT_PLUGIN_STORAGE_SLUG = config(
     "INCIDENT_PLUGIN_STORAGE_SLUG", default="google-drive-storage"
 )
-INCIDENT_PLUGIN_TICKET_SLUG = os.getenv("INCIDENT_PLUGIN_TICKET_SLUG", default="jira-ticket")
-INCIDENT_PLUGIN_TASK_SLUG = os.getenv("INCIDENT_PLUGIN_TASK_SLUG", default="google-drive-task")
+INCIDENT_PLUGIN_TICKET_SLUG = config("INCIDENT_PLUGIN_TICKET_SLUG", default="jira-ticket")
+INCIDENT_PLUGIN_TASK_SLUG = config("INCIDENT_PLUGIN_TASK_SLUG", default="google-drive-task")
 
 # incident resources
-INCIDENT_STORAGE_ARCHIVAL_FOLDER_ID = os.getenv("INCIDENT_STORAGE_ARCHIVAL_FOLDER_ID", default="")
-INCIDENT_STORAGE_INCIDENT_REVIEW_FILE_ID = os.getenv("INCIDENT_STORAGE_INCIDENT_REVIEW_FILE_ID", default="")
-INCIDENT_DOCUMENT_INVESTIGATION_SHEET_ID = os.getenv("INCIDENT_DOCUMENT_INVESTIGATION_SHEET_ID", default="")
-INCIDENT_FAQ_DOCUMENT_ID = os.getenv("INCIDENT_FAQ_DOCUMENT_ID", default="")
+INCIDENT_STORAGE_ARCHIVAL_FOLDER_ID = config("INCIDENT_STORAGE_ARCHIVAL_FOLDER_ID")
+INCIDENT_STORAGE_INCIDENT_REVIEW_FILE_ID = config("INCIDENT_STORAGE_INCIDENT_REVIEW_FILE_ID")
+INCIDENT_DOCUMENT_INVESTIGATION_SHEET_ID = config("INCIDENT_DOCUMENT_INVESTIGATION_SHEET_ID")
+INCIDENT_FAQ_DOCUMENT_ID = config("INCIDENT_FAQ_DOCUMENT_ID")
 
-INCIDENT_NOTIFICATION_CONVERSATIONS = os.getenv(
+INCIDENT_NOTIFICATION_CONVERSATIONS = config(
     "INCIDENT_NOTIFICATION_CONVERSATIONS", cast=CommaSeparatedStrings, default=""
 )
 
-INCIDENT_NOTIFICATION_DISTRIBUTION_LISTS = os.getenv(
+INCIDENT_NOTIFICATION_DISTRIBUTION_LISTS = config(
     "INCIDENT_NOTIFICATION_DISTRIBUTION_LISTS", cast=CommaSeparatedStrings, default=""
 )
 
-INCIDENT_DAILY_SUMMARY_ONCALL_SERVICE_ID = os.getenv(
+INCIDENT_DAILY_SUMMARY_ONCALL_SERVICE_ID = config(
     "INCIDENT_DAILY_SUMMARY_ONCALL_SERVICE_ID", default=None
 )
 
-INCIDENT_RESOURCE_TACTICAL_GROUP = os.getenv(
+INCIDENT_RESOURCE_TACTICAL_GROUP = config(
     "INCIDENT_RESOURCE_TACTICAL_GROUP", default="google-group-participant-tactical-group"
 )
-INCIDENT_RESOURCE_NOTIFICATIONS_GROUP = os.getenv(
+INCIDENT_RESOURCE_NOTIFICATIONS_GROUP = config(
     "INCIDENT_RESOURCE_NOTIFICATIONS_GROUP", default="google-group-participant-notifications-group"
 )
-INCIDENT_RESOURCE_FAQ_DOCUMENT = os.getenv(
+INCIDENT_RESOURCE_FAQ_DOCUMENT = config(
     "INCIDENT_RESOURCE_FAQ_DOCUMENT", default="google-docs-faq-document"
 )
-INCIDENT_RESOURCE_INVESTIGATION_DOCUMENT = os.getenv(
+INCIDENT_RESOURCE_INVESTIGATION_DOCUMENT = config(
     "INCIDENT_RESOURCE_INVESTIGATION_DOCUMENT", default="google-docs-investigation-document"
 )
-INCIDENT_RESOURCE_INVESTIGATION_SHEET = os.getenv(
+INCIDENT_RESOURCE_INVESTIGATION_SHEET = config(
     "INCIDENT_RESOURCE_INVESTIGATION_SHEET", default="google-docs-investigation-sheet"
 )
-INCIDENT_RESOURCE_INCIDENT_REVIEW_DOCUMENT = os.getenv(
+INCIDENT_RESOURCE_INCIDENT_REVIEW_DOCUMENT = config(
     "INCIDENT_RESOURCE_INCIDENT_REVIEW_DOCUMENT", default="google-docs-incident-review-document",
 )
-INCIDENT_RESOURCE_INCIDENT_TASK = os.getenv(
+INCIDENT_RESOURCE_INCIDENT_TASK = config(
     "INCIDENT_RESOURCE_INCIDENT_TASK", default="google-docs-incident-task"
 )
 
 # Incident Cost Configuration
-ANNUAL_COST_EMPLOYEE = os.getenv("ANNUAL_COST_EMPLOYEE", cast=int, default="650000")
-BUSINESS_HOURS_YEAR = os.getenv("BUSINESS_HOURS_YEAR", cast=int, default="2080")
+ANNUAL_COST_EMPLOYEE = config("ANNUAL_COST_EMPLOYEE", cast=int, default="650000")
+BUSINESS_HOURS_YEAR = config("BUSINESS_HOURS_YEAR", cast=int, default="2080")
