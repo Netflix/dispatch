1e292246a9d109efe8d1f89834cdb48ff734bc17
kevgliss <kevgliss@gmail.com>
2020-03-10 09:34:53 -0700

    Bugfix/sentry env (#87)
    * Removes sentry DSN, adds envvar VUE_APP_SENTRY_DSN

--- src/dispatch/static/dispatch/src/main.js
+++ src/dispatch/static/dispatch/src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue"
 import vuetify from "./vuetify/"
 import router from "./router/"
 import store from "./store"
-import filters from "./filters"
 import "./registerServiceWorker"
 import "roboto-fontface/css/roboto/roboto-fontface.css"
 import "font-awesome/css/font-awesome.css"
@@ -11,10 +10,12 @@ import "font-awesome/css/font-awesome.css"
 import * as Sentry from "@sentry/browser"
 import * as Integrations from "@sentry/integrations"
 
-//Sentry.init({
-//  dsn: "https://6df40584b7814b2aab2f47d7b4a21124@sentry.mgmt.netflix.net/38",
-//  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
-//})
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+  })
+}
 
 Vue.config.productionTip = false
 
