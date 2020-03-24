bd459febc21f6e8daaf6ca669c5662676e21954e
kevgliss <kevgliss@gmail.com>
2020-03-24 16:58:14 -0700

    Initial work on porting forecasting

--- src/dispatch/incident/metrics.py
+++ src/dispatch/incident/metrics.py
@@ -1,32 +1,53 @@
+from itertools import groupby
+
+from datetime import date
+from calendar import monthrange
+
 from fbprophet import Prophet
 import pandas as pd
-import numpy as np
 
-from .service import get_all, get_all_by_incident_type
+from .models import Incident
+from dispatch.incident_type.models import IncidentType
 
 
 def make_forecast(
     db_session, incident_type: str = None, periods: int = 24, grouping: str = "month"
 ):
     """Makes an incident forecast."""
+    # filter out all simulations, exclude current month
 
-    if not incident_type:
-        incidents = get_all(db_session=db_session).all()
-    else:
-        incidents = get_all_by_incident_type(db_session=db_session, incident_type=incident_type)
+    query = db_session.query(Incident).join(IncidentType)
 
-    if grouping == "month":
-        incident_data = [x.reported_at.month for x in incidents]
+    # exclude simulations
+    query = query.filter(IncidentType.name != "Simulation")
+
+    if incident_type:
+        query = query.filter(IncidentType.name == incident_type)
+
+    incidents = query.all()
+
+    def grouper(item):
+        key = date(
+            item.reported_at.year,
+            item.reported_at.month,
+            monthrange(item.reported_at.year, item.reported_at.month)[-1],
+        )
+        return key
+
+    incidents_sorted = sorted(incidents, key=grouper)
+
+    dataframe_dict = {"ds": [], "y": []}
+    for (last_day, items) in groupby(incidents_sorted, grouper):
+        dataframe_dict["ds"].append(last_day)
+        dataframe_dict["y"].append(len(list(items)))
+
+    dataframe = pd.DataFrame.from_dict(dataframe_dict)
 
     forecaster = Prophet()
-    future = forecaster.make_future_dataframe(periods=periods, freq="M")
+    forecaster.fit(dataframe, algorithm="LBFGS")
 
-    regressions = {"content_cost": "", "employee_count": ""}
-    for k, v in regressions.items():
-        forecaster.add_regressor(k)
-        incidents_by_month[k] = v
+    future = forecaster.make_future_dataframe(periods=2, freq="M")
 
-    forecaster.fit(incidents_by_month, algorithm="LBFGS")
     forecast = forecaster.predict(future)
 
     return forecast
