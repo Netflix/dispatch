3460e9e3c6082c8ec00c84c422b080e62597dd66
Marc Vilanova <mvilanova@netflix.com>
2020-03-31 15:38:22 -0700

    Adds uuid, additional timestamps, and converts author to an individual relationship

--- src/dispatch/event/models.py
+++ src/dispatch/event/models.py
@@ -2,7 +2,17 @@ from enum import Enum
 
 from typing import Optional
 
-from sqlalchemy import Column, Integer, String, ForeignKey, PrimaryKeyConstraint, Table
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    PrimaryKeyConstraint,
+    String,
+    Table,
+    event,
+)
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import TSVectorType
 
@@ -26,30 +36,40 @@ assoc_event_tags = Table(
 
 # SQLAlchemy Model
 class Event(Base, TimeStampMixin):
+    # columns
     id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    started_at = Column(DateTime)
+    ended_at = Column(DateTime)
     source = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    author = Column(String)
 
     # relationships
+    individual_id = Column(Integer, ForeignKey("individual.id"))
     incident_id = Column(Integer, ForeignKey("incident.id"))
     tags = relationship("Tag", secondary=assoc_event_tags, backref="events")
 
+    # full text search capabilities
     search_vector = Column(
-        TSVectorType(
-            "source",
-            "description",
-            "author",
-            weights={"source": "A", "description": "B", "author": "C"},
-        )
+        TSVectorType("source", "description", weights={"source": "A", "description": "B"})
     )
+
+    @staticmethod
+    def _ended_at(mapper, connection, target):
+        if not target.ended_at:
+            target.ended_at = target.started_at
+
+    @classmethod
+    def __declare_last__(cls):
+        event.listen(cls, "before_update", cls._ended_at)
 
 
 # Pydantic Models
 class EventBase(DispatchBase):
+    started_at: DateTime
+    ended_at: DateTime
     source: EventSource
     description: str
-    author: Optional[str]
 
 
 class EventCreate(EventBase):
