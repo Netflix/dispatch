16800b182ea11fa8dd57a8f2cb879b9547305c16
kevgliss <kevgliss@gmail.com>
2020-04-17 14:07:39 -0700

    Adding tab item container (#231)

--- src/dispatch/static/dispatch/src/incident/TimelineTab.vue
+++ src/dispatch/static/dispatch/src/incident/TimelineTab.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-container>
-    <v-timeline dense clipped>
-      <v-timeline-item v-for="event in events" :key="event.id" class="mb-4" color="blue" small>
-        <v-row justify="space-between">
-          <v-col cols="7">
-            {{ event.description }}
-            <div class="caption">{{ event.source }}</div>
-          </v-col>
-          <v-col class="text-right" cols="5">{{ event.started_at | formatDate }}</v-col>
-        </v-row>
-      </v-timeline-item>
-    </v-timeline>
-  </v-container>
+  <v-tab-item key="timeline">
+    <v-container>
+      <v-timeline dense clipped>
+        <v-timeline-item v-for="event in events" :key="event.id" class="mb-4" color="blue" small>
+          <v-row justify="space-between">
+            <v-col cols="7">
+              {{ event.description }}
+              <div class="caption">{{ event.source }}</div>
+            </v-col>
+            <v-col class="text-right" cols="5">{{ event.started_at | formatDate }}</v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
+    </v-container>
+  </v-tab-item>
 </template>
 
 <script>
