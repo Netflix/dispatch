818a79997881c8891cbef11f650821bc3a90b0b7
kevgliss <kevgliss@gmail.com>
2020-04-20 15:08:41 -0700

    Adjusting tabs so they render correctly. (#236)

--- src/dispatch/static/dispatch/src/incident/TimelineTab.vue
+++ src/dispatch/static/dispatch/src/incident/TimelineTab.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-tab-item key="timeline">
-    <v-container>
-      <v-timeline dense clipped>
-        <v-timeline-item v-for="event in events" :key="event.id" class="mb-4" color="blue" small>
-          <v-row justify="space-between">
-            <v-col cols="7">
-              {{ event.description }}
-              <div class="caption">{{ event.source }}</div>
-            </v-col>
-            <v-col class="text-right" cols="5">{{ event.started_at | formatDate }}</v-col>
-          </v-row>
-        </v-timeline-item>
-      </v-timeline>
-    </v-container>
-  </v-tab-item>
+  <v-container>
+    <v-timeline dense clipped>
+      <v-timeline-item v-for="event in events" :key="event.id" class="mb-4" color="blue" small>
+        <v-row justify="space-between">
+          <v-col cols="7">
+            {{ event.description }}
+            <div class="caption">{{ event.source }}</div>
+          </v-col>
+          <v-col class="text-right" cols="5">{{ event.started_at | formatDate }}</v-col>
+        </v-row>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
 
 <script>
