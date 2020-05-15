be7ff2ad23af792d43eb4cf8502377f0dc8cd8d3
kevgliss <kevgliss@gmail.com>
2020-05-15 13:25:37 -0700

    Initial search work

--- src/dispatch/static/dispatch/src/tag/List.vue
+++ src/dispatch/static/dispatch/src/tag/List.vue
@@ -1,50 +1,30 @@
 <template>
-  <v-layout wrap>
-    <div class="headline">Tags</div>
-    <v-flex xs12>
-      <v-layout column>
-        <v-flex>
-          <v-list two-line class="elevation-2">
-            <v-list-group v-for="item in items" :key="item.id" no-action>
-              <template v-slot:activator>
-                <v-list-item>
-                  <v-layout justify-space-around>
-                    <v-flex xs3>
-                      <v-list-item>
-                        <v-list-item-action>
-                          <v-icon>fa-user-circle</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                          <v-list-item-title>{{ item.name }}</v-list-item-title>
-                          <v-list-item-subtitle>Name</v-list-item-subtitle>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </v-flex>
-                  </v-layout>
-                </v-list-item>
-              </template>
-              <v-card-title primary-title>
-                <div>{{ item.description }}</div>
-              </v-card-title>
-            </v-list-group>
-          </v-list>
-        </v-flex>
-      </v-layout>
-    </v-flex>
-  </v-layout>
+  <v-list v-if="items.length">
+    <v-list-group prepend-icon="label" no-action color="primary">
+      <template v-slot:activator>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Tags ({{ items.length }})</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+      <v-list-item v-for="item in items" :key="item.content.id" @click="">
+        <v-list-item-content>
+          <v-list-item-title v-text="item.content.name"></v-list-item-title>
+          <v-list-item-subtitle>Name</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
 </template>
-
 <script>
 export default {
   name: "TagList",
   props: {
     items: {
-      type: Array,
-      default: null
+      default: null,
+      type: Array
     }
-  },
-  data() {
-    return {}
   }
 }
 </script>
