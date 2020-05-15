19560fb0de92e290d50f6275a8bd002e34cd7a79
Will Bengtson <william.bengtson@gmail.com>
2020-05-15 12:40:57 -0500

    Add PKCE overrides and support the key rotation

--- src/dispatch/plugins/dispatch_core/config.py
+++ src/dispatch/plugins/dispatch_core/config.py
@@ -1,3 +1,6 @@
 from starlette.config import Config
 
 config = Config(".env")
+
+DISPATCH_JWT_AUDIENCE = config("DISPATCH_JWT_AUDIENCE")
+DISPATCH_JWT_EMAIL_OVERRIDE = config("DISPATCH_JWT_EMAIL_OVERRIDE")
