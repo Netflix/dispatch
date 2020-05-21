35a75fcd61de2c1f0af49a46b5e8f8e90cd3290f
kevgliss <kevgliss@gmail.com>
2020-05-21 14:40:20 -0700

    Initial work

---
+++ src/dispatch/static/dispatch/src/incident/IncidentHeatmapCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card>
+    <v-card-title>Heatmap</v-card-title>
+    <apexchart type="heatmap" height="350" :options="chartOptions" :series="series"></apexchart>
+  </v-card>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts"
+export default {
+  name: "IncidentHeatMapChartCard",
+
+  props: {
+    value: {
+      type: Object,
+      default: function() {
+        return {}
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: function() {
+        return false
+      }
+    }
+  },
+  components: {
+    apexchart: VueApexCharts
+  },
+
+  computed: {
+    chartOptions: {
+      chart: {
+        height: 350,
+        type: "heatmap"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ["#008FFB"],
+      title: {
+        text: "HeatMap Chart (Single color)"
+      }
+    }
+  },
+  series() {
+    let series = { name: "Weekday", data: [] }
+  }
+}
+</script>
