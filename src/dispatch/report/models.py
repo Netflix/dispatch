7a65dfc253c4dfe0e83e3783c3a428f2cd91eeaf
Marc Vilanova <mvilanova@netflix.com>
2020-05-27 11:54:20 -0700

    Updates data model

--- src/dispatch/report/models.py
+++ src/dispatch/report/models.py
@@ -2,8 +2,10 @@ from datetime import datetime
 
 from typing import List, Optional
 
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, event
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import JSONType, TSVectorType
+
 
 from dispatch.database import Base
 from dispatch.models import DispatchBase
@@ -14,29 +16,31 @@ from .enums import ReportTypes
 class Report(Base):
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow)
-    conditions = Column(String)
-    actions = Column(String)
-    needs = Column(String)
-    overview = Column(String)
-    current_status = Column(String)
-    next_steps = Column(String)
-    report_type = Column(String, nullable=False, server_default=ReportTypes.status_report)
+    details = Column(JSONType, nullable=True)
+    details_raw = Column(String, nullable=True)
+    type = Column(String, nullable=False, server_default=ReportTypes.status_report)
 
     # relationships
     incident_id = Column(Integer, ForeignKey("incident.id"))
     participant_id = Column(Integer, ForeignKey("participant.id"))
     document = relationship("Document", uselist=False, backref="report")
 
+    # full text search capabilities
+    search_vector = Column(TSVectorType("details_raw"))
+
+    @staticmethod
+    def _details_raw(mapper, connection, target):
+        target.details_raw = " ".join([f"{k}: {v}" for k, v in target.details.items()])
+
+    @classmethod
+    def __declare_last__(cls):
+        event.listen(cls, "before_update", cls._details_raw)
+
 
 # Pydantic models...
 class ReportBase(DispatchBase):
-    conditions: Optional[str] = None
-    actions: Optional[str] = None
-    needs: Optional[str] = None
-    overview: Optional[str] = None
-    current_status: Optional[str] = None
-    next_steps: Optional[str] = None
-    report_type: ReportTypes
+    details: Optional[dict] = None
+    type: ReportTypes
 
 
 class ReportCreate(ReportBase):
