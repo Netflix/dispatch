1a1fe219149cea972352b360bcb8471e8a82684f
kevgliss <kevgliss@gmail.com>
2020-05-28 14:44:22 -0700

    Allow for customAuthProviders

--- src/dispatch/static/dispatch/src/auth/basicAuthProvider.js
+++ src/dispatch/static/dispatch/src/auth/basicAuthProvider.js
@@ -1,18 +1,18 @@
 import store from "@/store"
 export function login(to, from, next) {
   let token = localStorage.getItem("token")
-  // we already have a token tell vuex about it
+
   if (token) {
     store.commit("auth/SET_USER_LOGIN", token)
     next()
+  } else {
+    // prevent redirect loop
+    if (to.path !== "/login") {
+      next("/login")
+    } else {
+      next()
+    }
   }
-
-  // prevent redirect loop
-  if (to.path !== "/login") {
-    next("/login")
-  }
-
-  next()
 }
 
 export function logout(next) {
