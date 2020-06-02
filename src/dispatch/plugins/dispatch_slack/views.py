d71e4ab02a0ba6920d87425b5e85b0b2b03da4b4
kevgliss <kevgliss@gmail.com>
2020-06-02 09:54:23 -0700

    Fixing the rendering of the list-tasks slack command (#396)

--- src/dispatch/plugins/dispatch_slack/views.py
+++ src/dispatch/plugins/dispatch_slack/views.py
@@ -240,6 +240,7 @@ def list_tasks(incident_id: int, command: dict = None, db_session=None):
         )
 
         for task in tasks:
+            assignees = [a.individual.email for a in task.assignees]
             blocks.append(
                 {
                     "type": "section",
@@ -247,7 +248,7 @@ def list_tasks(incident_id: int, command: dict = None, db_session=None):
                         "type": "mrkdwn",
                         "text": (
                             f"*Description:* <{task.weblink}|{task.description}>\n"
-                            f"*Assignees:* {task.assignees}"
+                            f"*Assignees:* {", ".join(assignees)}"
                         ),
                     },
                 }
