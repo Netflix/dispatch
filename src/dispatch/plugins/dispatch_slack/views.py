8253fc9fca80c579e27ddfca3d5165daa8eab25a
kevgliss <kevgliss@gmail.com>
2020-06-05 14:04:26 -0700

    Update src/dispatch/plugins/dispatch_slack/views.py
    Co-authored-by: Marc Vilanova <39573146+mvilanova@users.noreply.github.com>

--- src/dispatch/plugins/dispatch_slack/views.py
+++ src/dispatch/plugins/dispatch_slack/views.py
@@ -561,7 +561,7 @@ def add_user_to_conversation(
         dispatch_slack_service.add_users_to_conversation(
             slack_client, incident.conversation.channel_id, [user_id]
         )
-        message = f"Success! We've added you {incident.name}. Please check your side bar for the new channel."
+        message = f"Success! We've added you to incident {incident.name}. Please check your side bar for the new channel."
         dispatch_slack_service.send_ephemeral_message(
             slack_client, action["container"]["channel_id"], user_id, message
         )
