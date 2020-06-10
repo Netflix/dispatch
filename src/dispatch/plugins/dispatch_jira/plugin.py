05ebc03e3fa335e13e79b937ad62353fc36c8cc3
Marc Vilanova <mvilanova@netflix.com>
2020-06-10 14:21:57 -0700

    Fixes bugs and improve Jira plugin

--- src/dispatch/plugins/dispatch_jira/plugin.py
+++ src/dispatch/plugins/dispatch_jira/plugin.py
@@ -21,8 +21,9 @@ from .config import (
     JIRA_ISSUE_TYPE_ID,
 )
 
-INCIDENT_TEMPLATE = """
-{color:red}*CONFIDENTIAL -- Internal use only{color}*
+
+ISSUE_SUMMARY_TEMPLATE = """
+{color:red}*Confidential Information - For Internal Use Only{color}*
 
 Commander: [~{{commander_username}}]
 
@@ -38,26 +39,6 @@ Cost: ${{cost}}
 [Storage|{{storage_weblink}}]
 [Conference|{{conference_weblink}}]
 """
-
-
-def create_sec_issue(
-    client: Any,
-    title: str,
-    priority: str,
-    incident_type: str,
-    commander_username: str,
-    reporter_username: str,
-):
-    issue_fields = {
-        "project": {"key": JIRA_PROJECT_KEY},
-        "issuetype": {"id": JIRA_ISSUE_TYPE_ID},
-        "summary": title,
-        "assignee": {"name": commander_username},
-        "components": [{"name": incident_type}],
-        "reporter": {"name": reporter_username},
-    }
-
-    return create(client, issue_fields, type=JIRA_PROJECT_KEY)
 
 
 def create_issue_fields(
@@ -80,7 +61,7 @@ def create_issue_fields(
     issue_fields.update({"assignee": {"name": commander_username}})
     issue_fields.update({"reporter": {"name": reporter_username}})
 
-    description = Template(INCIDENT_TEMPLATE).render(
+    description = Template(ISSUE_SUMMARY_TEMPLATE).render(
         description=description,
         incident_type=incident_type,
         priority=priority,
@@ -149,13 +130,21 @@ class JiraTicketPlugin(TicketPlugin):
         reporter: str,
         incident_type_plugin_metadata: dict = {},
     ):
-        """Creates a Jira ticket."""
+        """Creates a Jira issue."""
         client = JIRA(str(JIRA_API_URL), basic_auth=(JIRA_USERNAME, str(JIRA_PASSWORD)))
+
         commander_username = get_user_name(commander)
         reporter_username = get_user_name(reporter)
-        return create_sec_issue(
-            client, title, incident_priority, incident_type, commander_username, reporter_username
-        )
+
+        issue_fields = {
+            "project": {"key": JIRA_PROJECT_KEY},
+            "issuetype": {"id": JIRA_ISSUE_TYPE_ID},
+            "summary": title,
+            "assignee": {"name": commander_username},
+            "reporter": {"name": reporter_username},
+        }
+
+        return create(client, issue_fields, type=JIRA_PROJECT_KEY)
 
     def update(
         self,
@@ -174,11 +163,11 @@ class JiraTicketPlugin(TicketPlugin):
         cost: float,
         incident_type_plugin_metadata: dict = {},
     ):
-        """Updates Jira ticket fields."""
+        """Updates Jira issue fields."""
+        client = JIRA(str(JIRA_API_URL), basic_auth=(JIRA_USERNAME, str(JIRA_PASSWORD)))
+
         commander_username = get_user_name(commander_email)
         reporter_username = get_user_name(reporter_email)
-
-        client = JIRA(str(JIRA_API_URL), basic_auth=(JIRA_USERNAME, str(JIRA_PASSWORD)))
 
         issue = client.issue(ticket_id)
         issue_fields = create_issue_fields(
@@ -194,4 +183,5 @@ class JiraTicketPlugin(TicketPlugin):
             storage_weblink=storage_weblink,
             cost=cost,
         )
+
         return update(client, issue, issue_fields, status)
