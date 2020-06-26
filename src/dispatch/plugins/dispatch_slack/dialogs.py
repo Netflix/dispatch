dafeaafceb272c70bca411481b64074cf914c693
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2020-06-26 16:09:02 -0700

    Adds hint to executive report dialog (#471)

--- src/dispatch/plugins/dispatch_slack/dialogs.py
+++ src/dispatch/plugins/dispatch_slack/dialogs.py
@@ -12,6 +12,8 @@ from dispatch.report import service as report_service
 from dispatch.report.enums import ReportTypes
 from dispatch.service import service as service_service
 
+from .config import SLACK_COMMAND_UPDATE_NOTIFICATIONS_GROUP_SLUG
+
 
 slack_client = dispatch_slack_service.create_slack_client()
 log = logging.getLogger(__name__)
@@ -228,7 +230,13 @@ def create_executive_report_dialog(incident_id: int, command: dict = None, db_se
                 "value": current_status,
             },
             {"type": "textarea", "label": "Overview", "name": "overview", "value": overview},
-            {"type": "textarea", "label": "Next Steps", "name": "next_steps", "value": next_steps},
+            {
+                "type": "textarea",
+                "label": "Next Steps",
+                "name": "next_steps",
+                "value": next_steps,
+                "hint": f"Use {SLACK_COMMAND_UPDATE_NOTIFICATIONS_GROUP_SLUG} to update the list of recipients of this report.",
+            },
         ],
     }
 
