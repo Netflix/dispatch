7fa9889b543a562905415e2d8d09c2a2639fc396
kevgliss <kevgliss@gmail.com>
2020-10-08 12:34:42 -0700

    Migrates to the pdpyras client library

--- src/dispatch/plugins/dispatch_pagerduty/config.py
+++ src/dispatch/plugins/dispatch_pagerduty/config.py
@@ -1,4 +1,5 @@
 from dispatch.config import config, Secret
 
 PAGERDUTY_API_KEY = config("PAGERDUTY_API_KEY", cast=Secret)
+print(PAGERDUTY_API_KEY)
 PAGERDUTY_API_FROM_EMAIL = config("PAGERDUTY_API_FROM_EMAIL")
--- src/dispatch/plugins/dispatch_pagerduty/plugin.py
+++ src/dispatch/plugins/dispatch_pagerduty/plugin.py
@@ -5,12 +5,13 @@
     :license: Apache, see LICENSE for more details.
 """
 import logging
-
+from pdpyras import APISession
 from dispatch.decorators import apply, counter, timer
 from dispatch.plugins import dispatch_pagerduty as pagerduty_oncall_plugin
 from dispatch.plugins.bases import OncallPlugin
 
 from .service import get_oncall, page_oncall
+from .config import PAGERDUTY_API_KEY
 
 
 log = logging.getLogger(__name__)
@@ -28,10 +29,18 @@ class PagerDutyOncallPlugin(OncallPlugin):
 
     def get(self, service_id: str = None, service_name: str = None):
         """Gets the oncall person."""
-        return get_oncall(service_id=service_id, service_name=service_name)
+        client = APISession(PAGERDUTY_API_KEY)
+        return get_oncall(client=client, service_id=service_id, service_name=service_name)
 
     def page(
         self, service_id: str, incident_name: str, incident_title: str, incident_description: str
     ):
         """Pages the oncall person."""
-        return page_oncall(service_id, incident_name, incident_title, incident_description)
+        client = APISession(PAGERDUTY_API_KEY)
+        return page_oncall(
+            client=client,
+            service_id=service_id,
+            incident_name=incident_name,
+            incident_title=incident_title,
+            incident_description=incident_description,
+        )
