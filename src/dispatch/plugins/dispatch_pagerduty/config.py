847dec70ab70db5c942e6a130b779c431fa05e3f
kevgliss <kevgliss@gmail.com>
2020-10-08 12:35:34 -0700

    Remove print

--- src/dispatch/plugins/dispatch_pagerduty/config.py
+++ src/dispatch/plugins/dispatch_pagerduty/config.py
@@ -1,5 +1,4 @@
 from dispatch.config import config, Secret
 
 PAGERDUTY_API_KEY = config("PAGERDUTY_API_KEY", cast=Secret)
-print(PAGERDUTY_API_KEY)
 PAGERDUTY_API_FROM_EMAIL = config("PAGERDUTY_API_FROM_EMAIL")
