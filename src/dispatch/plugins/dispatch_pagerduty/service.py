4c1ebbc5e4e57e4e833df943ef41e80b403775de
kevgliss <kevgliss@gmail.com>
2020-10-09 09:18:57 -0700

    Migrates to the pdpyras client library (#603)
    * Migrates to the pdpyras client library

--- src/dispatch/plugins/dispatch_pagerduty/service.py
+++ src/dispatch/plugins/dispatch_pagerduty/service.py
@@ -1,60 +1,40 @@
-import pypd
-from pypd.models.service import Service
-
-from dispatch.exceptions import DispatchPluginException
-
-from .config import PAGERDUTY_API_KEY, PAGERDUTY_API_FROM_EMAIL
+from .config import PAGERDUTY_API_FROM_EMAIL
 
 
-pypd.api_key = PAGERDUTY_API_KEY
-
-
-def get_oncall_email(service: Service):
+def get_oncall_email(client, service: dict) -> str:
     """Fetches the oncall's email for a given service."""
-    escalation_policy_id = service.json["escalation_policy"]["id"]
-    escalation_policy = pypd.EscalationPolicy.fetch(escalation_policy_id)
+    escalation_policy_id = service["escalation_policy"]["id"]
+    escalation_policy = client.rget(f"/escalation_policies/{escalation_policy_id}")
+    schedule_id = escalation_policy["escalation_rules"][0]["targets"][0]["id"]
 
-    schedule_id = escalation_policy.json["escalation_rules"][0]["targets"][0]["id"]
-
-    oncall = pypd.OnCall.find_one(
-        escalation_policy_ids=[escalation_policy_id], schedule_ids=[schedule_id]
+    oncalls = client.iter_all(
+        "oncalls",  # method
+        {
+            # "include[]": "users", # including users doesn't give us the contact details
+            "schedule_ids[]": [schedule_id],
+            "escalation_policy_ids[]": [escalation_policy_id],
+        },  # params
     )
 
-    user_id = oncall.json["user"]["id"]
-    user = pypd.User.fetch(user_id)
+    user_id = list(oncalls)[0]["user"]["id"]
+    user = client.rget(f"/users/{user_id}")
 
-    return user.json["email"]
+    return user["email"]
 
 
-def get_oncall(service_id: str = None, service_name: str = None):
+def get_oncall(client, service_id: str):
     """Gets the oncall for a given service id or name."""
-    if service_id:
-        service = pypd.Service.fetch(service_id)
-        return get_oncall_email(service)
-    elif service_name:
-        service = pypd.Service.find(query=service_name)
-
-        if len(service) > 1:
-            raise DispatchPluginException(
-                f"More than one PagerDuty service found with service name: {service_name}"
-            )
-
-        if not service:
-            raise DispatchPluginException(
-                f"No on-call service found with service name: {service_name}"
-            )
-
-        return get_oncall_email(service[0])
-
-    raise DispatchPluginException("Cannot fetch oncall. Must specify service_id or service_name.")
+    service = client.rget(f"/services/{service_id}")
+    return get_oncall_email(client, service)
 
 
 def page_oncall(
-    service_id: str, incident_name: str, incident_title: str, incident_description: str
+    client, service_id: str, incident_name: str, incident_title: str, incident_description: str
 ):
     """Pages the oncall for a given service id."""
-    service = pypd.Service.fetch(service_id)
-    escalation_policy_id = service.json["escalation_policy"]["id"]
+    service = client.rget(f"/services/{service_id}")
+
+    escalation_policy_id = service["escalation_policy"]["id"]
 
     data = {
         "type": "incident",
@@ -64,7 +44,7 @@ def page_oncall(
         "body": {"type": "incident_body", "details": incident_description},
         "escalation_policy": {"id": escalation_policy_id, "type": "escalation_policy_reference"},
     }
-
-    incident = pypd.Incident.create(data=data, add_headers={"from": PAGERDUTY_API_FROM_EMAIL})
+    headers = {"from": PAGERDUTY_API_FROM_EMAIL}
+    incident = client.rpost("/incidents", json=data, headers=headers)
 
     return incident
