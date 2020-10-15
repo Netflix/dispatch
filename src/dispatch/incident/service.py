e3ceaaf73fe2fd5b31993fb8b03bded3cb794275
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2020-10-15 12:26:53 -0700

    Makes incident cost calculation more robust (#626)

--- src/dispatch/incident/service.py
+++ src/dispatch/incident/service.py
@@ -316,16 +316,17 @@ def calculate_cost(incident_id: int, db_session: SessionLocal, incident_review=T
 
             participant_role_assumed_at = participant_role.assumed_at
 
+            if incident.status == IncidentStatus.active.value:
+                # the incident is still active. we use the current time
+                participant_role_renounced_at = datetime.utcnow()
+            else:
+                # the incident is stable or closed. we use the stable_at time
+                participant_role_renounced_at = incident.stable_at
+
             if participant_role.renounced_at:
                 # the participant left the conversation or got assigned another role
                 # we use the renounced_at time
                 participant_role_renounced_at = participant_role.renounced_at
-            elif incident.status != IncidentStatus.active:
-                # the incident is stable or closed. we use the stable_at time
-                participant_role_renounced_at = incident.stable_at
-            else:
-                # the incident is still active. we use the current time
-                participant_role_renounced_at = datetime.utcnow()
 
             # we calculate the time the participant has spent in the incident role
             participant_role_time = participant_role_renounced_at - participant_role_assumed_at
