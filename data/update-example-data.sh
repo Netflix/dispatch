fddee10655bdc02ff37dbf746782c95e138d79ad
kevgliss <kevgliss@gmail.com>
2020-11-18 15:30:47 -0500

    Adds sample data migration script.

---
+++ data/update-example-data.sh
@@ -0,0 +1,8 @@
+echo "Dropping existing database..."
+dispatch database drop
+echo "Restoring current dump file..."
+dispatch database restore --dump-file ./dispatch-sample-data.dump
+echo "Running database migrations..."
+dispatch database upgrade
+echo "Dumping sql to file..."
+dispatch database dump --dump-file ./dispatch-sample-data.dump
