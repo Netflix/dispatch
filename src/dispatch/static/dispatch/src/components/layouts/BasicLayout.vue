d2792b513b6e4949de9bbd3d8741a3c9fc7dd5d5
kevgliss <kevgliss@gmail.com>
2020-12-15 11:02:18 -0500

    Fixes auth forms

---
+++ src/dispatch/static/dispatch/src/components/layouts/BasicLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <snackbar />
+    <v-content>
+      <v-app-bar app flat class="v-bar--underline" color="white">
+        <router-link to="/" tag="span">
+          <span class="button font-weight-bold">D I S P A T C H</span>
+        </router-link>
+      </v-app-bar>
+      <v-card flat>
+        <router-view :key="$route.path" />
+      </v-card>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Snackbar from "@/components/Snackbar.vue"
+export default {
+  components: {
+    Snackbar
+  },
+  data: () => ({}),
+
+  methods: {}
+}
+</script>
+<style scoped lang="css">
+.split-bg {
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+  z-index: 0;
+}
+</style>
