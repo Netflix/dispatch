269f5ef51435a0f94f5e6bfb4002a2a46d4a4927
Marc Vilanova <mvilanova@netflix.com>
2021-01-20 11:48:27 -0800

    Adds plugins, tags, and terms to data model

--- src/dispatch/notification/models.py
+++ src/dispatch/notification/models.py
@@ -12,61 +12,101 @@ from dispatch.incident_priority.models import (
     IncidentPriorityUpdate,
 )
 from dispatch.incident_type.models import IncidentTypeCreate, IncidentTypeRead, IncidentTypeUpdate
+from dispatch.plugin.models import PluginCreate, PluginRead, PluginUpdate
+from dispatch.tag.models import TagCreate, TagRead, TagUpdate
+from dispatch.term.models import TermCreate, TermRead, TermUpdate
 
 from dispatch.models import DispatchBase, TimeStampMixin
 
 
-# Association tables for many to many relationships
-assoc_notification_incident_types = Table(
+# Association tables for many-to-many relationships
+notification_incident_type_assoc_table = Table(
     "notification_incident_type",
     Base.metadata,
-    Column("incident_type_id", Integer, ForeignKey("incident_type.id", ondelete="CASCADE")),
     Column("notification_id", Integer, ForeignKey("notification.id", ondelete="CASCADE")),
-    PrimaryKeyConstraint("incident_type_id", "notification_id"),
+    Column("incident_type_id", Integer, ForeignKey("incident_type.id", ondelete="CASCADE")),
+    PrimaryKeyConstraint("notification_id", "incident_type_id"),
 )
 
-assoc_notification_incident_priorities = Table(
+notification_incident_priority_assoc_table = Table(
     "notification_incident_priority",
     Base.metadata,
-    Column("incident_priority_id", Integer, ForeignKey("incident_priority.id", ondelete="CASCADE")),
     Column("notification_id", Integer, ForeignKey("notification.id", ondelete="CASCADE")),
-    PrimaryKeyConstraint("incident_priority_id", "notification_id"),
+    Column("incident_priority_id", Integer, ForeignKey("incident_priority.id", ondelete="CASCADE")),
+    PrimaryKeyConstraint("notification_id", "incident_priority_id"),
+)
+
+notification_plugin_assoc_table = Table(
+    "notification_plugin",
+    Base.metadata,
+    Column("notification_id", Integer, ForeignKey("notification.id", ondelete="CASCADE")),
+    Column("plugin_id", Integer, ForeignKey("plugin.id", ondelete="CASCADE")),
+    PrimaryKeyConstraint("notification_id", "plugin_id"),
+)
+
+notification_tag_assoc_table = Table(
+    "notification_tag",
+    Base.metadata,
+    Column("notification_id", Integer, ForeignKey("notification.id", ondelete="CASCADE")),
+    Column("tag_id", Integer, ForeignKey("tag.id", ondelete="CASCADE")),
+    PrimaryKeyConstraint("notification_id", "tag_id"),
+)
+
+notification_term_assoc_table = Table(
+    "notification_term",
+    Base.metadata,
+    Column("notification_id", Integer, ForeignKey("notification.id", ondelete="CASCADE")),
+    Column("term_id", Integer, ForeignKey("term.id", ondelete="CASCADE")),
+    PrimaryKeyConstraint("notification_id", "term_id"),
 )
 
 
 class Notification(Base, TimeStampMixin):
     # Columns
     id = Column(Integer, primary_key=True)
-    conversation_name = Column(String)
-    conversation_description = Column(String)
+    name = Column(String)
+    description = Column(String)
+    type = Column(String)
 
     # Relationships
     incident_types = relationship(
-        "IncidentType", secondary=assoc_notification_incident_types, backref="notifications"
+        "IncidentType", secondary=notification_incident_type_assoc_table, backref="notifications"
     )
     incident_priorities = relationship(
         "IncidentPriority",
-        secondary=assoc_notification_incident_priorities,
+        secondary=notification_incident_priority_assoc_table,
         backref="notifications",
     )
+    plugins = relationship(
+        "Plugin", secondary=notification_plugin_assoc_table, backref="notifications"
+    )
+    tags = relationship("Tag", secondary=notification_tag_assoc_table, backref="notifications")
+    terms = relationship("Term", secondary=notification_term_assoc_table, backref="notifications")
 
     search_vector = Column(TSVectorType("conversation_name", "conversation_description"))
 
 
 # Pydantic models
 class NotificationBase(DispatchBase):
-    conversation_name: str
-    conversation_description: Optional[str] = None
+    name: str
+    description: Optional[str] = None
+    type: str
 
 
 class NotificationCreate(NotificationBase):
     incident_types: Optional[List[IncidentTypeCreate]] = []
     incident_priorities: Optional[List[IncidentPriorityCreate]] = []
+    plugins: Optional[List[PluginCreate]] = []
+    tags: Optional[List[TagCreate]] = []
+    terms: Optional[List[TermCreate]] = []
 
 
 class NotificationUpdate(NotificationBase):
     incident_types: Optional[List[IncidentTypeUpdate]] = []
     incident_priorities: Optional[List[IncidentPriorityUpdate]] = []
+    plugins: Optional[List[PluginUpdate]] = []
+    tags: Optional[List[TagUpdate]] = []
+    terms: Optional[List[TermUpdate]] = []
 
 
 class NotificationRead(NotificationBase):
@@ -75,6 +115,9 @@ class NotificationRead(NotificationBase):
     updated_at: Optional[datetime] = None
     incident_types: Optional[List[IncidentTypeRead]] = []
     incident_priorities: Optional[List[IncidentPriorityRead]] = []
+    plugins: Optional[List[PluginRead]] = []
+    tags: Optional[List[TagRead]] = []
+    terms: Optional[List[TermRead]] = []
 
 
 class NotificationPagination(DispatchBase):
