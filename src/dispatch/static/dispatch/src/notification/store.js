22910aae1d073353c1364e3d41c612903c87b873
Marc Vilanova <mvilanova@netflix.com>
2021-01-21 10:33:00 -0800

    Adds notifications store

---
+++ src/dispatch/static/dispatch/src/notification/store.js
@@ -0,0 +1,195 @@
+import NotificationApi from "@/notification/api"
+
+import { getField, updateField } from "vuex-map-fields"
+import { debounce, forEach, each, has } from "lodash"
+
+const getDefaultSelectedState = () => {
+  return {
+    id: null,
+    name: null,
+    description: null,
+    type: null,
+    target: null,
+    enabled: null,
+    policy: null,
+    loading: false
+  }
+}
+
+const state = {
+  selected: {
+    ...getDefaultSelectedState()
+  },
+  dialogs: {
+    showCreateEdit: false,
+    showRemove: false
+  },
+  table: {
+    rows: {
+      items: [],
+      total: null
+    },
+    options: {
+      filters: {
+        name: [],
+        type: [],
+        target: [],
+        policy: []
+      },
+      q: "",
+      page: 1,
+      itemsPerPage: 10,
+      sortBy: ["created_at"],
+      descending: [true]
+    },
+    loading: false
+  }
+}
+
+const getters = {
+  getField
+}
+
+const actions = {
+  getAll: debounce(({ commit, state }) => {
+    commit("SET_TABLE_LOADING", "primary")
+    let tableOptions = Object.assign({}, state.table.options)
+    delete tableOptions.filters
+
+    tableOptions.fields = []
+    tableOptions.ops = []
+    tableOptions.values = []
+
+    forEach(state.table.options.filters, function(value, key) {
+      each(value, function(value) {
+        if (has(value, "id")) {
+          tableOptions.fields.push(key + ".id")
+          tableOptions.values.push(value.id)
+        } else {
+          tableOptions.fields.push(key)
+          tableOptions.values.push(value)
+        }
+        tableOptions.ops.push("==")
+      })
+    })
+    return NotificationApi.getAll(tableOptions).then(response => {
+      commit("SET_TABLE_LOADING", false)
+      commit("SET_TABLE_ROWS", response.data)
+    })
+  }, 200),
+  createEditShow({ commit }, notification) {
+    commit("SET_DIALOG_CREATE_EDIT", true)
+    if (notification) {
+      commit("SET_SELECTED", notification)
+    }
+  },
+  removeShow({ commit }, notification) {
+    commit("SET_DIALOG_DELETE", true)
+    commit("SET_SELECTED", notification)
+  },
+  closeCreateEdit({ commit }) {
+    commit("SET_DIALOG_CREATE_EDIT", false)
+    commit("RESET_SELECTED")
+  },
+  closeRemove({ commit }) {
+    commit("SET_DIALOG_DELETE", false)
+    commit("RESET_SELECTED")
+  },
+  save({ commit, dispatch }) {
+    if (!state.selected.id) {
+      return NotificationApi.create(state.selected)
+        .then(() => {
+          dispatch("closeCreateEdit")
+          dispatch("getAll")
+          commit(
+            "notification/addBeNotification",
+            { text: "Notification created successfully.", type: "success" },
+            { root: true }
+          )
+        })
+        .catch(err => {
+          commit(
+            "notification/addBeNotification",
+            {
+              text: "Notification not created. Reason: " + err.response.data.detail,
+              type: "error"
+            },
+            { root: true }
+          )
+        })
+    } else {
+      return NotificationApi.update(state.selected.id, state.selected)
+        .then(() => {
+          dispatch("closeCreateEdit")
+          dispatch("getAll")
+          commit(
+            "notification/addBeNotification",
+            { text: "Notification updated successfully.", type: "success" },
+            { root: true }
+          )
+        })
+        .catch(err => {
+          commit(
+            "notification/addBeNotification",
+            {
+              text: "Notification not updated. Reason: " + err.response.data.detail,
+              type: "error"
+            },
+            { root: true }
+          )
+        })
+    }
+  },
+  remove({ commit, dispatch }) {
+    return NotificationApi.delete(state.selected.id)
+      .then(function() {
+        dispatch("closeRemove")
+        dispatch("getAll")
+        commit(
+          "notification/addBeNotification",
+          { text: "Notification deleted successfully.", type: "success" },
+          { root: true }
+        )
+      })
+      .catch(err => {
+        commit(
+          "notification/addBeNotification",
+          {
+            text: "Notification not deleted. Reason: " + err.response.data.detail,
+            type: "error"
+          },
+          { root: true }
+        )
+      })
+  }
+}
+
+const mutations = {
+  updateField,
+  SET_SELECTED(state, value) {
+    state.selected = Object.assign(state.selected, value)
+  },
+  SET_TABLE_LOADING(state, value) {
+    state.table.loading = value
+  },
+  SET_TABLE_ROWS(state, value) {
+    state.table.rows = value
+  },
+  SET_DIALOG_CREATE_EDIT(state, value) {
+    state.dialogs.showCreateEdit = value
+  },
+  SET_DIALOG_DELETE(state, value) {
+    state.dialogs.showRemove = value
+  },
+  RESET_SELECTED(state) {
+    state.selected = Object.assign(state.selected, getDefaultSelectedState())
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
