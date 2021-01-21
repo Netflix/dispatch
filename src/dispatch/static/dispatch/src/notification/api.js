aec068113559589e45e49fc4739e7052f0f6db35
Marc Vilanova <mvilanova@netflix.com>
2021-01-21 10:33:14 -0800

    Adds notifications api

---
+++ src/dispatch/static/dispatch/src/notification/api.js
@@ -0,0 +1,25 @@
+import API from "@/api"
+
+const resource = "/notifications"
+
+export default {
+  getAll(options) {
+    return API.get(`${resource}/`, { params: { ...options } })
+  },
+
+  get(notificationId) {
+    return API.get(`${resource}/${notificationId}`)
+  },
+
+  create(payload) {
+    return API.post(`${resource}/`, payload)
+  },
+
+  update(notificationId, payload) {
+    return API.put(`${resource}/${notificationId}`, payload)
+  },
+
+  delete(notificationId) {
+    return API.delete(`${resource}/${notificationId}`)
+  }
+}
