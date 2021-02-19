c6456f7250f696f960a6d344f76de7722ae6aa5a
kevgliss <kevgliss@gmail.com>
2021-02-19 09:41:38 -0800

    Allows special chars to be used in postgres passwords (#898)
    * Allows special chars to be used in postgres passwords
    
    * Removing print

--- src/dispatch/config.py
+++ src/dispatch/config.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import base64
+from urllib import parse
 from typing import List
 
 from starlette.config import Config
@@ -160,9 +161,13 @@ METRIC_PROVIDERS = config("METRIC_PROVIDERS", cast=CommaSeparatedStrings, defaul
 # database
 DATABASE_HOSTNAME = config("DATABASE_HOSTNAME")
 DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret)
+
+# this will support special chars for credentials
+_DATABASE_CREDENTIAL_USER, _DATABASE_CREDENTIAL_PASSWORD = str(DATABASE_CREDENTIALS).split(":")
+_QUOTED_DATABASE_PASSWORD = parse.quote(str(_DATABASE_CREDENTIAL_PASSWORD))
 DATABASE_NAME = config("DATABASE_NAME", default="dispatch")
 DATABASE_PORT = config("DATABASE_PORT", default="5432")
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DATABASE_CREDENTIALS}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{_DATABASE_CREDENTIAL_USER}:{_QUOTED_DATABASE_PASSWORD}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # incident resources
 INCIDENT_STORAGE_FOLDER_ID = config("INCIDENT_STORAGE_FOLDER_ID", default=None)
