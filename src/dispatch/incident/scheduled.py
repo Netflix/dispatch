ab843b3e023a6cdeb0ad9fc8b20959caa25fd6b0
kevgliss <kevgliss@gmail.com>
2021-02-18 18:01:57 -0800

    Removes incident cost update for external tickets, cost will be updated only on incident state change open/stable/closed.

--- src/dispatch/incident/scheduled.py
+++ src/dispatch/incident/scheduled.py
@@ -202,13 +202,6 @@ def calculate_incidents_cost(db_session=None):
 
             log.debug(f"Incident cost for {incident.name} updated in the database.")
 
-            if incident.ticket:
-                # we update the external ticket
-                update_external_incident_ticket(incident, db_session)
-                log.debug(f"Incident cost for {incident.name} updated in the ticket.")
-            else:
-                log.debug(f"Ticket not found. Incident cost for {incident.name} not updated.")
-
         except Exception as e:
             # we shouldn't fail to update all incidents when one fails
             log.exception(e)
