32b35b84d8ce387b59816319ccc26967d25fb1b6
Prasanth Kommini <prasanth.kommini@snowflake.com>
2021-02-21 14:45:27 -0800

    Fix duplicate key in get_credential_data dict

--- src/dispatch/plugins/dispatch_google/common.py
+++ src/dispatch/plugins/dispatch_google/common.py
@@ -43,7 +43,6 @@ def get_credential_data():
             "private_key": str(GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY).replace("\\n", "\n"),
             "client_email": GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
             "client_id": GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
-            "token_uri": "https://oauth2.googleapis.com/token",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
