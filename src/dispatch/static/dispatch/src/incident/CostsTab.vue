1b748f6951704a75a18556ddb493c54404dbfbb5
Marc Vilanova <mvilanova@netflix.com>
2021-03-08 07:20:19 -0800

    Adds tab to incident details section

--- src/dispatch/static/dispatch/src/incident/CostsTab.vue
+++ src/dispatch/static/dispatch/src/incident/CostsTab.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container grid-list-md>
-    <v-layout wrap></v-layout>
-  </v-container>
+  <!-- <v&#45;container grid&#45;list&#45;md> -->
+  <!--   <v&#45;layout wrap></v&#45;layout> -->
+  <!-- </v&#45;container> -->
+  <div>
+    <span v-for="incident_cost in incident_costs" :key="incident_cost.id">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ incident_cost.amount | toUSD }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ incident_cost.incident_cost_type.name }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+    </span>
+  </div>
 </template>
 
 <script>
@@ -24,7 +39,8 @@ export default {
   // },
 
   computed: {
-    ...mapFields("incident", ["selected.id", "selected.incident_costs"])
+    // ...mapFields("incident", ["selected.id", "selected.incident_costs"])
+    ...mapFields("incident", ["selected.incident_costs"])
   }
 }
 </script>
