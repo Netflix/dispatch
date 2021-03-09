ba5799bdc00a5754fa0e8200276c2f624c713829
kevgliss <kevgliss@gmail.com>
2021-03-08 16:26:37 -0800

    Initial work

--- src/dispatch/tag/recommender.py
+++ src/dispatch/tag/recommender.py
@@ -1,39 +1,171 @@
-def correlate_with_every_tag(df, tag_a, dict_mode = True):
+"""
+.. module: dispatch.tag.recommender
+    :platform: Unix
+    :copyright: (c) 2019 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+"""
+import logging
+from typing import List, Any
+from collections import defaultdict
+
+import tempfile
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+from dispatch.database import SessionLocal
+from dispatch.tag import service as tag_service
+
+log = logging.getLogger(__name__)
+
+
+def save_model(dataframe: DataFrame, model_name: str):
+    """Saves a correlation dataframe to disk."""
+    file_name = f"{tempfile.gettempdir()}/{model_name}.pkl"
+    dataframe.to_pickle(file_name)
+
+
+def load_model(model_name: str):
+    """Loads a correlation dataframe from disk."""
+    file_name = f"{tempfile.gettempdir()}/{model_name}.pkl"
+    return pd.read_pickle(file_name)
+
+
+def correlation(df, tag_a, tag_b):
+    """Determine the probability of correlation/association."""
+    # Find all rows where a AND b == True
+    a_and_b = df[(df[tag_a]) & (df[tag_b])]
+
+    # Find all rows where a == True AND b != True
+    a_not_b = df[(df[tag_a]) & ~(df[tag_b])]
+    # Find all rows where b == True AND a != True
+    b_not_a = df[(df[tag_b]) & ~(df[tag_a])]
+
+    # Calculate the number of possitive and possible outcomes using the shape attribute
+    possible_outcomes = (
+        a_and_b.shape[0] + a_not_b.shape[0] + b_not_a.shape[0]
+    )  # shape[0] returns the number of rows
+    positive_outcomes = a_and_b.shape[0]
+
+    # Calculate the final correlation coefficient
+    r = positive_outcomes / possible_outcomes
+
+    return r
+
+
+def correlate_with_every_tag(df, tag_a):
+    """Create correlations between every tag."""
 
     unique_tags = list(df.columns)
-
-    # In dict_mode, the results are stored in a dict, which is good for analyzing one tag
-    # However, in order to transform the data into a df later, we need a list output
-    if dict_mode:
-        # Loop through every tag and store the correlation in the dict
-        correlation_dict = {}
-        for tag_b in unique_tags:
-            correlation_dict[tag_b] = correlation(df, tag_a, tag_b)
-        return correlation_dict
-    else:
-        # Loop through every tag and store the correlation in a list
-        correlation_list = []
-        for tag_b in unique_tags:
-            correlation_list.append(correlation(df, tag_a, tag_b))
-        return correlation_list
+    # Loop through every tag and store the correlation in a list
+    correlation_list = []
+    for tag_b in unique_tags:
+        correlation_list.append(correlation(df, tag_a, tag_b))
+    return correlation_list
 
 
-def get_recommendations(df, tag, num_of_recommendations):
-
-    corr_df = find_correlations(df, tag)
-
-    recommendations_df = find_highest_correlations(corr_df, num_of_recommendations)
-
-    print("Recommendations:", list(recommendations_df["tag"]))
-
-
-def build_model():
-    pass
+def get_unique_tags(items: List[Any]):
+    """Get unique tags."""
+    unique_tags = {}
+    for i in items:
+        for t in i.tags:
+            unique_tags[t.id] = t.id
+    return unique_tags
 
 
-def save_model():
-    pass
+def create_correlation_dataframe(dataframe):
+    """Create the correlation dataframe based on the boolean dataframe."""
+    unique_tags = list(dataframe.columns)
+
+    correlation_matrix_dict = {}
+
+    for tag_a in unique_tags:
+        correlation_matrix_dict[tag_a] = correlate_with_every_tag(dataframe, tag_a)
+
+    correlated_dataframe = pd.DataFrame(correlation_matrix_dict)
+    correlated_dataframe["index"] = unique_tags
+    return correlated_dataframe.set_index("index")
 
 
-def load_model():
-    pass
+def create_boolean_dataframe(items: List[Any]):
+    """Create a boolean dataframe with tag and item data."""
+    unique_tags = get_unique_tags(items)
+    boolean_df = pd.DataFrame(columns=unique_tags.values())
+
+    data_dict = defaultdict(list)
+    for col in boolean_df:
+        for i in items:
+            tag_ids = [t.id for t in i.tags]
+            data_dict[col].append(col in tag_ids)
+
+    for col in boolean_df:
+        boolean_df[col] = data_dict[col]
+
+    return boolean_df
+
+
+def find_correlations(dataframe, tag):
+    """Find all correlations for the given tag."""
+    # Setup empty list
+    correlations = []
+    columns = []
+
+    # Loop through all column at the row with the tag as its index
+    for i, corr in enumerate(dataframe.loc[tag, :]):
+
+        # Find the column
+        col = dataframe.columns[i]
+
+        # Append the correlation to the list
+        correlations.append(corr)
+        columns.append(col)
+
+    # Create a df out of the lists
+    results_df = pd.DataFrame({"tag": columns, "correlation": correlations})
+
+    return results_df
+
+
+def find_highest_correlations(correlated_dataframe, recommendations):
+    """Find the correlations with the highest relevancy."""
+    # Sort the input df
+    corr_df_sorted = correlated_dataframe.sort_values(by=["correlation"], ascending=False)
+
+    # Extract the relevant correlations
+    corr_df_sliced = corr_df_sorted.iloc[1 : recommendations + 1]  # noqa
+
+    return corr_df_sliced
+
+
+def get_recommendations(db_session: SessionLocal, tag_ids: List[str], model_name: str, recommendations: int = 5):
+    """Get recommendations based on current tag."""
+    try:
+        correlation_dataframe = load_model(model_name)
+    except FileNotFoundError:
+        log.warning(f"No model file found. ModelName: {model_name}")
+        return []
+
+    recommendations_dataframe = pd.DataFrame()
+    for tag_id in tag_ids:
+        correlated_dataframe = find_correlations(correlation_dataframe, tag_id)
+        recommendations_dataframe = pd.concat(
+            [
+                recommendations_dataframe,
+                find_highest_correlations(correlated_dataframe, recommendations),
+            ],
+            ignore_index=True,
+        )
+
+    # convert back to tag objects
+    tags = []
+    for t in recommendations_dataframe[:recommendations]:
+        tags.append(tag_service.get(db_session=db_session, tag_id=t))
+
+    log.debug(f"Making tag recommendation. TagIds: {','.join([t.name for t in tags])} ModelName: {model_name} Recomendations: ", list(recommendations_dataframe["tag"][:recommendations]))
+    return recommendations_dataframe[:recommendations]
+
+
+def build_model(items: List[Any], model_name: str):
+    """Builds the correlation dataframe for items."""
+    boolean_dataframe = create_boolean_dataframe(items)
+    correlation_dataframe = create_correlation_dataframe(boolean_dataframe)
+    save_model(correlation_dataframe, model_name)
--- src/dispatch/tag/scheduled.py
+++ src/dispatch/tag/scheduled.py
@@ -11,8 +11,10 @@ from schedule import every
 from dispatch.decorators import background_task
 from dispatch.plugins.base import plugins
 from dispatch.scheduler import scheduler
+from dispatch.incident import service as incident_service
 from dispatch.tag import service as tag_service
 from dispatch.tag.models import TagCreate
+from dispatch.tag.recommender import build_model
 
 log = logging.getLogger(__name__)
 
@@ -26,3 +28,14 @@ def sync_tags(db_session=None):
         for t in p.get():
             log.debug(f"Adding Tag. Tag: {t}")
             tag_service.get_or_create(db_session=db_session, tag_in=TagCreate(**t))
+
+
+@scheduler.add(every(1).hour, name="tag-model-builder")
+@background_task
+def build_tag_models(db_session=None):
+    """Builds the intensive tag recommandation models."""
+    # incident model
+    incidents = incident_service.get_all(db_session=db_session).all()
+    log.debug("Starting to build the incident/tag model...")
+    build_model(incidents, "incidents")
+    log.debug("Starting succesfully build the incident/tag model.")
