4f1134ae46be80533a2769f4cf58bf3ad0fee985
kevgliss <kevgliss@gmail.com>
2021-03-16 09:21:55 -0700

    Fixing lgtm

--- src/dispatch/__init__.py
+++ src/dispatch/__init__.py
@@ -46,7 +46,6 @@ try:
     from dispatch.organization.models import Organization  # noqa lgtm[py/unused-import]
     from dispatch.project.models import Project  # noqa lgtm[py/unused-import]
 except Exception as e:
-    print(e)
     pass
 
 
