640d6753bb9b0674ef07229434d1342beb4bc7ab
kevgliss <kevgliss@gmail.com>
2021-03-19 09:30:07 -0700

    Update src/dispatch/organization/views.py
    Co-authored-by: Marc Vilanova <39573146+mvilanova@users.noreply.github.com>

--- src/dispatch/organization/views.py
+++ src/dispatch/organization/views.py
@@ -98,7 +98,7 @@ def delete_organization(*, db_session: Session = Depends(get_db), organization_i
     """
     organization = get(db_session=db_session, organization_id=organization_id)
     if not organization:
-        raise HTTPException(status_code=404, detail="The organization with this id does not exist.")
+        raise HTTPException(status_code=404, detail="An organization with this id does not exist.")
 
     delete(db_session=db_session, organization_id=organization_id)
     return organization
