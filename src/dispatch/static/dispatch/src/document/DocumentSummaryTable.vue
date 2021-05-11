afd30e8ef11542b93a3ede2a275e03120ddac8db
Kevin Glisson <kevgliss@gmail.com>
2021-05-11 14:55:05 -0700

    Fixing project label colors

--- src/dispatch/static/dispatch/src/document/DocumentSummaryTable.vue
+++ src/dispatch/static/dispatch/src/document/DocumentSummaryTable.vue
@@ -9,7 +9,7 @@
         </a>
       </template>
       <template v-slot:item.project.name="{ item }">
-        <v-chip color="orange" text-color="white">
+        <v-chip :color="item.project.color" text-color="white">
           {{ item.project.name }}
         </v-chip>
       </template>
