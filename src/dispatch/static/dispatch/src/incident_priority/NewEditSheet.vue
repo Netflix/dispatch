b39bbfa39ff931414f252ad8de60d9432f5abe73
Marc Vilanova <mvilanova@netflix.com>
2021-05-12 12:21:01 -0700

    Fixes display issue with checkboxes in new incident priority UI

--- src/dispatch/static/dispatch/src/incident_priority/NewEditSheet.vue
+++ src/dispatch/static/dispatch/src/incident_priority/NewEditSheet.vue
@@ -106,23 +106,23 @@
               </v-flex>
               <v-flex xs12>
                 <v-checkbox
+                  v-model="page_commander"
+                  label="Page Commander"
+                  hint="Would you like Dispatch to page the incident commander on incident creation?"
+                />
+              </v-flex>
+              <v-flex xs12>
+                <v-checkbox
                   v-model="default_incident_priority"
                   label="Default Incident Priority"
                   hint="Check if this incident priority should be the default."
                 />
               </v-flex>
-              <v-flex>
+              <v-flex xs12>
                 <v-checkbox
                   v-model="enabled"
                   label="Enabled"
                   hint="Determines whether this incident priority is availible for new incidents."
-                />
-              </v-flex>
-              <v-flex>
-                <v-checkbox
-                  v-model="page_commander"
-                  label="Page Commander"
-                  hint="Would you like Dispatch to page the incident commander on incident creation?"
                 />
               </v-flex>
             </v-layout>
