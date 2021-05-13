fc0d14f0baf1ee9d163847d217391faf41979cca
Kevin Glisson <kevgliss@gmail.com>
2021-05-13 09:32:04 -0700

    Adding a potential organization banner

--- src/dispatch/static/dispatch/src/auth/customAuthProvider.js
+++ src/dispatch/static/dispatch/src/auth/customAuthProvider.js
@@ -1,5 +1,30 @@
+import MeechumUser from "meechum-user-lib-js"
+
+import store from "@/store"
+
+function sessionTimeoutCallback() {
+  store.commit(
+    "app/SET_REFRESH",
+    { show: true, message: "Your credentials have expired. Please refresh the page." },
+    { root: true }
+  )
+}
+
+function errorCallback() {
+  store.commit(
+    "app/SET_REFRESH",
+    { show: true, message: "Your credentials have expired. Please refresh the page." },
+    { root: true }
+  )
+}
+
 function login(to, from, next) {
-  next()
+  // meechum auth stuff...
+  MeechumUser.initialize("/meechum", 60, sessionTimeoutCallback, errorCallback).then(function () {
+    let token = MeechumUser.getAccessToken()
+    store.commit("auth/SET_USER_LOGIN", token)
+    next()
+  })
 }
 
 function logout(next) {
---
+++ src/dispatch/static/dispatch/src/organization/OrganizationBanner.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-banner class="mb-6" single-line>
+    <v-icon slot="icon" color="info" size="36"> mdi-information-outline </v-icon>
+    You are currently using the <span class="font-weight-bold">training</span> Dispatch
+    organization.
+  </v-banner>
+</template>
+<script>
+export default {}
+</script>
