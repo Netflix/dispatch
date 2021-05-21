a945dfd5c70b1f2f3ba37b9a9582b497ab4a3b69
Kevin Glisson <kevgliss@gmail.com>
2021-05-21 14:05:07 -0700

    Adding banner fetching logic

--- src/dispatch/static/dispatch/src/organization/OrganizationBanner.vue
+++ src/dispatch/static/dispatch/src/organization/OrganizationBanner.vue
@@ -1,10 +1,48 @@
 <template>
-  <v-banner class="mb-6" color="red lighten-2" single-line>
+  <v-banner
+    v-if="currentOrganization.banner_enabled"
+    class="mb-6"
+    :color="currentOrganization.banner_color"
+    single-line
+  >
     <v-icon slot="icon" color="white" size="36"> mdi-information-outline </v-icon>
-    You are currently using the <span class="font-weight-bold">training</span> Dispatch
-    organization.
+    {{ currentOrganization.banner_text }}
   </v-banner>
 </template>
 <script>
-export default {}
+import { mapFields } from "vuex-map-fields"
+
+import SearchUtils from "@/search/utils"
+import OrganizationApi from "@/organization/api"
+
+export default {
+  data() {
+    return {
+      currentOrganization: {},
+    }
+  },
+
+  created() {
+    let filterOptions = {
+      itemsPerPage: 50,
+      sortBy: ["name"],
+      descending: [false],
+    }
+
+    let nameFilter = [
+      {
+        model: "Organization",
+        field: "name",
+        op: "==",
+        value: this.$route.params.organization,
+      },
+    ]
+
+    filterOptions = SearchUtils.createParametersFromTableOptions({ ...filterOptions }, nameFilter)
+
+    OrganizationApi.getAll(filterOptions).then((response) => {
+      this.currentOrganization = response.data.items[0]
+    })
+  },
+}
 </script>
