63f30ccf7aabd7a33ea3f2464298ed09fc783cc4
Kevin Glisson <kevgliss@gmail.com>
2021-05-25 15:09:46 -0700

    Auth provider

--- src/dispatch/static/dispatch/src/auth/customAuthProvider.js
+++ src/dispatch/static/dispatch/src/auth/customAuthProvider.js
@@ -1,30 +1,5 @@
-import MeechumUser from "meechum-user-lib-js"
-
-import store from "@/store"
-
-function sessionTimeoutCallback() {
-  store.commit(
-    "app/SET_REFRESH",
-    { show: true, message: "Your credentials have expired. Please refresh the page." },
-    { root: true }
-  )
-}
-
-function errorCallback() {
-  store.commit(
-    "app/SET_REFRESH",
-    { show: true, message: "Your credentials have expired. Please refresh the page." },
-    { root: true }
-  )
-}
-
 function login(to, from, next) {
-  // meechum auth stuff...
-  MeechumUser.initialize("/meechum", 60, sessionTimeoutCallback, errorCallback).then(function () {
-    let token = MeechumUser.getAccessToken()
-    store.commit("auth/SET_USER_LOGIN", token)
-    next()
-  })
+  next()
 }
 
 function logout(next) {
