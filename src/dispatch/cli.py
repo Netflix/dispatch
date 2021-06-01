66b97a2a777e0bfd70e8baf913147ffe0c8fba28
Kevin Glisson <kevgliss@gmail.com>
2021-06-01 13:30:49 -0700

    Adding newline

--- src/dispatch/cli.py
+++ src/dispatch/cli.py
@@ -224,6 +224,7 @@ def dispatch_database():
 def database_init():
     """Initializes a new database."""
     from dispatch.database.core import SessionLocal
+
     click.echo("Initializing new database...")
     db_session = SessionLocal()
     init_database(db_session=db_session)
