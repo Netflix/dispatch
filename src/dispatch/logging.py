4642df379fd5b61f01926172a97f106d77fc211a
Kevin Glisson <kevgliss@gmail.com>
2021-06-11 12:39:30 -0700

    Adding timeslice

--- src/dispatch/logging.py
+++ src/dispatch/logging.py
@@ -9,3 +9,4 @@ def configure_logging():
         logging.basicConfig(level=LOG_LEVEL, format=LOGFORMAT)
     else:
         logging.basicConfig(level=LOG_LEVEL)
+    logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
