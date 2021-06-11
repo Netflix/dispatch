846535a3af461c5cb3b083046f89cc92bfb8aa9a
kevgliss <kevgliss@gmail.com>
2021-06-11 11:45:47 -0700

    Setting the access token correctly for pcke login flow. (#1296)
    * Setting the access token correctly for pcke login flow.
    
    * Prefix store
    
    * Adding the ability to route the the implicity callback

--- src/dispatch/static/dispatch/src/auth/pkceAuthProvider.js
+++ src/dispatch/static/dispatch/src/auth/pkceAuthProvider.js
@@ -72,9 +72,9 @@ function login(to, from, next) {
           .performTokenRequest(cfg, req)
           .then((response) => {
             // Redirect to the uri in session storage and then delete it from storage
-            store.dispatch("auth/login", {
-              token: response.accessToken,
-              redirectUri: localStorage.getItem("redirect_uri"),
+            store.commit("auth/SET_USER_LOGIN", response.accessToken)
+            store.dispatch("auth/loginRedirect", localStorage.getItem("redirect_uri")).then(() => {
+              store.dispatch("auth/createExpirationCheck")
             })
             localStorage.removeItem("redirect_uri")
           })
