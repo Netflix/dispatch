ace3c4f13d99bc7710d155e1f6d8f985d8b845c9
Kevin Glisson <kevgliss@gmail.com>
2021-06-16 12:06:14 -0700

    Reworking organization membership

---
+++ src/dispatch/static/dispatch/src/auth/OrganizationMemberTable.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-layout wrap>
+    <new-edit-sheet />
+    <div class="headline">Organization Members</div>
+    <v-spacer />
+    <v-flex xs12>
+      <v-layout column>
+        <v-flex>
+          <v-card elevation="0">
+            <v-card-title>
+              <v-text-field
+                v-model="q"
+                append-icon="search"
+                label="Search"
+                single-line
+                hide-details
+                clearable
+              />
+            </v-card-title>
+            <v-data-table
+              :headers="headers"
+              :items="items"
+              :server-items-length="total"
+              :page.sync="page"
+              :items-per-page.sync="itemsPerPage"
+              :sort-by.sync="sortBy"
+              :sort-desc.sync="descending"
+              :loading="loading"
+              loading-text="Loading... Please wait"
+            >
+              <template v-slot:item.data-table-actions="{ item }">
+                <v-menu bottom left>
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                      <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list>
+                    <v-list-item @click="editShow(item)">
+                      <v-list-item-title>View / Edit</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { mapFields } from "vuex-map-fields"
+import { mapActions } from "vuex"
+import NewEditSheet from "@/auth/OrganizationMemberEdit.vue"
+export default {
+  name: "OrganizationUserTable",
+
+  components: {
+    NewEditSheet,
+  },
+
+  data() {
+    return {
+      headers: [
+        { text: "Email", value: "email", sortable: true },
+        { text: "", value: "data-table-actions", sortable: false, align: "end" },
+      ],
+    }
+  },
+
+  computed: {
+    ...mapFields("auth", [
+      "table.options.q",
+      "table.options.page",
+      "table.options.itemsPerPage",
+      "table.options.sortBy",
+      "table.options.descending",
+      "table.loading",
+      "table.rows.items",
+      "table.rows.total",
+    ]),
+  },
+
+  created() {
+    this.getAll()
+
+    this.$watch(
+      (vm) => [vm.page],
+      () => {
+        this.getAll()
+      }
+    )
+
+    this.$watch(
+      (vm) => [vm.q, vm.itemsPerPage, vm.sortBy, vm.descending],
+      () => {
+        this.page = 1
+        this.getAll()
+      }
+    )
+  },
+
+  methods: {
+    ...mapActions("auth", ["getAll", "editShow"]),
+  },
+}
+</script>
