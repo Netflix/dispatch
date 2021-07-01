8d1127c3cf5514ef2f13319315417bf5821c3664
kevgliss <kevgliss@gmail.com>
2021-07-01 14:44:15 -0700

    Update src/dispatch/static/dispatch/src/router/config.js
    Co-authored-by: Marc Vilanova <39573146+mvilanova@users.noreply.github.com>

--- src/dispatch/static/dispatch/src/router/config.js
+++ src/dispatch/static/dispatch/src/router/config.js
@@ -316,7 +316,7 @@ export const protectedRoute = [
             name: "RunbookTable",
             meta: { title: "Runbooks", subMenu: "project", group: "knowledge" },
             component: () =>
-              import(/* webpackChunkName: "plugin-table" */ "@/document/runbook/RunbookTable.vue"),
+              import(/* webpackChunkName: "runbooks-table" */ "@/document/runbook/RunbookTable.vue"),
           },
           {
             path: "definitions",
