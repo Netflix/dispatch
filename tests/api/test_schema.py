cc0040f08bb40f4244c3d778acfe98d4bec11716
Kevin Glisson <kevgliss@gmail.com>
2021-07-22 15:07:18 -0700

    Switching to asgi app

--- tests/api/test_schema.py
+++ tests/api/test_schema.py
@@ -1,16 +1,12 @@
 import schemathesis
 
-# from dispatch.main import app
+from dispatch.main import app
 
-# schemathesis.fixups.install(["fast_api"])
+schemathesis.fixups.install(["fast_api"])
 
-# schema = schemathesis.from_asgi("/api/v1/docs/openapi.json", app, base_url="/api/v1")
-
-schema = schemathesis.from_uri(
-    "http://127.0.0.1:8080/api/v1/docs/openapi.json", base_url="http://127.0.0.1:8080/api/v1"
-)
+schema = schemathesis.from_asgi("/api/v1/docs/openapi.json", app, base_url="/api/v1")
 
 
 @schema.parametrize()
-def test_api(case):
+def test_api(db, case):
     case.call_and_validate()
