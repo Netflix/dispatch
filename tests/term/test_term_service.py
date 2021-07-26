8ecb8e72d6efaaba56cba4c3711b79d6d28c7107
Marc Vilanova <mvilanova@netflix.com>
2021-07-26 11:51:05 -0700

    Term CRUD tests

---
+++ tests/term/test_term_service.py
@@ -0,0 +1,49 @@
+import pytest
+
+
+def test_get(session, term):
+    from dispatch.term.service import get
+
+    t_term = get(db_session=session, term_id=term.id)
+    assert t_term.id == term.id
+
+
+def test_create(session, project):
+    from dispatch.term.service import create
+    from dispatch.term.models import TermCreate
+
+    text = "text"
+    discoverable = True
+
+    term_in = TermCreate(
+        text=text,
+        discoverable=discoverable,
+        project=project,
+    )
+    term = create(db_session=session, term_in=term_in)
+    assert term
+
+
+@pytest.mark.skip
+def test_update(session, term):
+    from dispatch.term.service import update
+    from dispatch.term.models import TermUpdate
+
+    text = "updated text"
+
+    term_in = TermUpdate(
+        text=text,
+    )
+    term = update(
+        db_session=session,
+        term=term,
+        term_in=term_in,
+    )
+    assert term.text == text
+
+
+def test_delete(session, term):
+    from dispatch.term.service import delete, get
+
+    delete(db_session=session, term_id=term.id)
+    assert not get(db_session=session, term_id=term.id)
