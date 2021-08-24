1b6b1e7f3f2ddea804197aa69a537cb614c7fd89
Kevin Glisson <kevgliss@gmail.com>
2021-08-24 14:59:56 -0700

    Adds some UI documentation and tightens up table interface

--- src/dispatch/static/dispatch/src/task/Table.vue
+++ src/dispatch/static/dispatch/src/task/Table.vue
@@ -1,133 +1,135 @@
 <template>
-  <v-layout wrap>
-    <new-edit-sheet />
-    <delete-dialog />
-    <div class="headline">Tasks</div>
-    <v-spacer />
-    <table-filter-dialog />
-    <table-export-dialog />
-    <v-btn color="info" class="ml-2" @click="createEditShow()"> New </v-btn>
-    <v-flex xs12>
-      <v-layout column>
-        <v-flex>
-          <v-card elevation="0">
-            <v-card-title>
-              <v-text-field
-                v-model="q"
-                append-icon="search"
-                label="Search"
-                single-line
-                hide-details
-                clearable
+  <v-container>
+    <v-row no-gutters>
+      <new-edit-sheet />
+      <delete-dialog />
+      <v-col>
+        <div class="headline">Tasks</div>
+      </v-col>
+      <v-spacer />
+      <v-col cols="3">
+        <table-filter-dialog />
+        <table-export-dialog />
+        <v-btn color="info" class="ml-2" @click="createEditShow()"> New </v-btn>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <v-card elevation="0">
+          <v-card-title>
+            <v-text-field
+              v-model="q"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+              clearable
+            />
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :server-items-length="total"
+            :page.sync="page"
+            :items-per-page.sync="itemsPerPage"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="descending"
+            v-model="selected"
+            :loading="loading"
+            loading-text="Loading... Please wait"
+            show-select
+          >
+            <template v-slot:item.description="{ item }">
+              <div class="text-truncate" style="max-width: 400px">
+                {{ item.description }}
+              </div>
+            </template>
+            <template v-slot:item.project.name="{ item }">
+              <v-chip small :color="item.project.color" text-color="white">
+                {{ item.project.name }}
+              </v-chip>
+            </template>
+            <template v-slot:item.incident_priority.name="{ item }">
+              <incident-priority :priority="item.incident.incident_priority.name" />
+            </template>
+            <template v-slot:item.creator.individual_contact.name="{ item }">
+              <participant :participant="item.creator" />
+            </template>
+            <template v-slot:item.owner.individual_contact.name="{ item }">
+              <participant :participant="item.owner" />
+            </template>
+            <template v-slot:item.incident_type.name="{ item }">
+              {{ item.incident.incident_type.name }}
+            </template>
+            <template v-slot:item.tickets="{ item }">
+              <a
+                v-for="ticket in item.tickets"
+                :key="ticket.weblink"
+                :href="ticket.weblink"
+                target="_blank"
+                style="text-decoration: none"
+              >
+                Ticket
+                <v-icon small>open_in_new</v-icon>
+              </a>
+            </template>
+            <template v-slot:item.assignees="{ item }">
+              <participant
+                v-for="assignee in item.assignees"
+                :key="assignee.id"
+                :participant="assignee"
               />
-            </v-card-title>
-            <v-data-table
-              :headers="headers"
-              :items="items"
-              :server-items-length="total"
-              :page.sync="page"
-              :items-per-page.sync="itemsPerPage"
-              :sort-by.sync="sortBy"
-              :sort-desc.sync="descending"
-              v-model="selected"
-              :loading="loading"
-              loading-text="Loading... Please wait"
-              show-select
-            >
-              <template v-slot:item.description="{ item }">
-                <div class="text-truncate" style="max-width: 400px">
-                  {{ item.description }}
-                </div>
-              </template>
-              <template v-slot:item.project.name="{ item }">
-                <v-chip small :color="item.project.color" text-color="white">
-                  {{ item.project.name }}
-                </v-chip>
-              </template>
-              <template v-slot:item.incident_priority.name="{ item }">
-                <incident-priority :priority="item.incident.incident_priority.name" />
-              </template>
-              <template v-slot:item.creator.individual_contact.name="{ item }">
-                <participant :participant="item.creator" />
-              </template>
-              <template v-slot:item.owner.individual_contact.name="{ item }">
-                <participant :participant="item.owner" />
-              </template>
-              <template v-slot:item.incident_type.name="{ item }">
-                {{ item.incident.incident_type.name }}
-              </template>
-              <template v-slot:item.tickets="{ item }">
-                <a
-                  v-for="ticket in item.tickets"
-                  :key="ticket.weblink"
-                  :href="ticket.weblink"
-                  target="_blank"
-                  style="text-decoration: none"
-                >
-                  Ticket
-                  <v-icon small>open_in_new</v-icon>
-                </a>
-              </template>
-              <template v-slot:item.assignees="{ item }">
-                <participant
-                  v-for="assignee in item.assignees"
-                  :key="assignee.id"
-                  :participant="assignee"
-                />
-              </template>
-              <template v-slot:item.resolve_by="{ item }">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on">{{ item.resolve_by | formatRelativeDate }}</span>
-                  </template>
-                  <span>{{ item.resolve_by | formatDate }}</span>
-                </v-tooltip>
-              </template>
-              <template v-slot:item.created_at="{ item }">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on">{{ item.created_at | formatRelativeDate }}</span>
-                  </template>
-                  <span>{{ item.created_at | formatDate }}</span>
-                </v-tooltip>
-              </template>
-              <template v-slot:item.resolved_at="{ item }">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on">{{
-                      item.resolved_at | formatRelativeDate
-                    }}</span>
-                  </template>
-                  <span>{{ item.resolved_at | formatDate }}</span>
-                </v-tooltip>
-              </template>
-              <template v-slot:item.source="{ item }">
-                <a :href="item.weblink" target="_blank" style="text-decoration: none">
-                  {{ item.source }}
-                  <v-icon small>open_in_new</v-icon>
-                </a>
-              </template>
-              <template v-slot:item.data-table-actions="{ item }">
-                <v-menu bottom left>
-                  <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
-                      <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                  </template>
-                  <v-list>
-                    <v-list-item @click="createEditShow(item)">
-                      <v-list-item-title>View / Edit</v-list-item-title>
-                    </v-list-item>
-                  </v-list>
-                </v-menu>
-              </template>
-            </v-data-table>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-flex>
+            </template>
+            <template v-slot:item.resolve_by="{ item }">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on">{{ item.resolve_by | formatRelativeDate }}</span>
+                </template>
+                <span>{{ item.resolve_by | formatDate }}</span>
+              </v-tooltip>
+            </template>
+            <template v-slot:item.created_at="{ item }">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on">{{ item.created_at | formatRelativeDate }}</span>
+                </template>
+                <span>{{ item.created_at | formatDate }}</span>
+              </v-tooltip>
+            </template>
+            <template v-slot:item.resolved_at="{ item }">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on">{{ item.resolved_at | formatRelativeDate }}</span>
+                </template>
+                <span>{{ item.resolved_at | formatDate }}</span>
+              </v-tooltip>
+            </template>
+            <template v-slot:item.source="{ item }">
+              <a :href="item.weblink" target="_blank" style="text-decoration: none">
+                {{ item.source }}
+                <v-icon small>open_in_new</v-icon>
+              </a>
+            </template>
+            <template v-slot:item.data-table-actions="{ item }">
+              <v-menu bottom left>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item @click="createEditShow(item)">
+                    <v-list-item-title>View / Edit</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
     <bulk-edit-sheet />
-  </v-layout>
+  </v-container>
 </template>
 
 <script>
