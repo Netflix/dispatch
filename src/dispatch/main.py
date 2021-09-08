bbe79521ac0384baf133898cf867cd0cc8b04e8e
Kevin Glisson <kevgliss@gmail.com>
2021-09-08 09:24:43 -0700

    Fixing an issue were mulitple transactions could happen in the same session

--- src/dispatch/main.py
+++ src/dispatch/main.py
@@ -1,6 +1,10 @@
 import time
 import logging
 from os import path
+from uuid import uuid1
+from typing import Optional, Final
+from contextvars import ContextVar
+
 
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
@@ -8,6 +12,7 @@ from pydantic.error_wrappers import ValidationError
 
 from sentry_asgi import SentryMiddleware
 from sqlalchemy import inspect
+from sqlalchemy.orm import scoped_session
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.routing import compile_path
@@ -88,8 +93,18 @@ def get_path_template(request: Request) -> str:
     return ".".join(request.url.path.split("/")[1:4])
 
 
+REQUEST_ID_CTX_KEY: Final[str] = "request_id"
+_request_id_ctx_var: ContextVar[Optional[str]] = ContextVar(REQUEST_ID_CTX_KEY, default=None)
+
+
+def get_request_id() -> Optional[str]:
+    return _request_id_ctx_var.get()
+
+
 @api.middleware("http")
 async def db_session_middleware(request: Request, call_next):
+    request_id = str(uuid1())
+    ctx_token = _request_id_ctx_var.set(request_id)
     path_params = get_path_params_from_request(request)
 
     # if this call is organization specific set the correct search path
@@ -121,12 +136,13 @@ async def db_session_middleware(request: Request, call_next):
             }
         )
     try:
-        session = sessionmaker(bind=schema_engine)
+        session = scoped_session(sessionmaker(bind=schema_engine), scopefunc=get_request_id)
         request.state.db = session()
         response = await call_next(request)
     finally:
         request.state.db.close()
 
+    _request_id_ctx_var.reset(ctx_token)
     return response
 
 
