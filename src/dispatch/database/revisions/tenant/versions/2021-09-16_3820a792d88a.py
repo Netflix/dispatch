2aca40bcacaa46e3fe68dad4f191c90b4254b070
kevgliss <kevgliss@gmail.com>
2021-10-06 13:57:02 -0700

    Fixes issue with missing values during migration (#1689)

--- src/dispatch/database/revisions/tenant/versions/2021-09-16_3820a792d88a.py
+++ src/dispatch/database/revisions/tenant/versions/2021-09-16_3820a792d88a.py
@@ -6,7 +6,7 @@ Create Date: 2021-09-16 16:33:40.605881
 
 """
 from alembic import op
-from pydantic import SecretStr
+from pydantic import SecretStr, ValidationError
 from pydantic.json import pydantic_encoder
 from starlette.datastructures import URL
 from sqlalchemy.dialects import postgresql
@@ -34,6 +34,12 @@ def show_secrets_encoder(obj):
         return obj.get_secret_value()
     else:
         return pydantic_encoder(obj)
+
+
+def migrate_config(instances, slug, config):
+    for instance in instances:
+        if slug == instance.plugin.slug:
+            instance.configuration = config
 
 
 class Plugin(Base):
@@ -74,6 +80,11 @@ def upgrade():
         ),
     )
 
+    bind = op.get_bind()
+    session = Session(bind=bind)
+
+    instances = session.query(PluginInstance).all()
+
     from dispatch.plugins.dispatch_google.config import GoogleConfiguration
 
     GOOGLE_DEVELOPER_KEY = config("GOOGLE_DEVELOPER_KEY", cast=Secret, default=None)
@@ -93,25 +104,42 @@ def upgrade():
     GOOGLE_SERVICE_ACCOUNT_PROJECT_ID = config("GOOGLE_SERVICE_ACCOUNT_PROJECT_ID", default=None)
     GOOGLE_DOMAIN = config("GOOGLE_DOMAIN", default=None)
 
-    google_config = GoogleConfiguration(
-        developer_key=str(GOOGLE_DEVELOPER_KEY),
-        service_account_client_email=GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
-        service_account_client_id=GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
-        service_account_private_key=str(GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY),
-        service_account_private_key_id=GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-        service_account_delegated_account=GOOGLE_SERVICE_ACCOUNT_DELEGATED_ACCOUNT,
-        service_account_project_id=GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
-        google_domain=GOOGLE_DOMAIN,
-    )
+    try:
+        google_config = GoogleConfiguration(
+            developer_key=str(GOOGLE_DEVELOPER_KEY),
+            service_account_client_email=GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+            service_account_client_id=GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+            service_account_private_key=str(GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY),
+            service_account_private_key_id=GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+            service_account_delegated_account=GOOGLE_SERVICE_ACCOUNT_DELEGATED_ACCOUNT,
+            service_account_project_id=GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+            google_domain=GOOGLE_DOMAIN,
+        )
+        migrate_config(instances, "google-calendar-conference", google_config)
+        migrate_config(instances, "google-docs-document", google_config)
+        migrate_config(instances, "google-drive-storage", google_config)
+        migrate_config(instances, "google-drive-task", google_config)
+        migrate_config(instances, "google-gmail-email", google_config)
+        migrate_config(instances, "google-group-participant-group", google_config)
+    except ValidationError:
+        print(
+            "Skipping automatic migration of google plugin credentials, if you are using the google suite of plugins manually migrate credentials."
+        )
 
     from dispatch.plugins.dispatch_pagerduty.plugin import PagerdutyConfiguration
 
     PAGERDUTY_API_KEY = config("PAGERDUTY_API_KEY", cast=Secret, default=None)
     PAGERDUTY_API_FROM_EMAIL = config("PAGERDUTY_API_FROM_EMAIL", default=None)
 
-    pagerduty_config = PagerdutyConfiguration(
-        api_key=str(PAGERDUTY_API_KEY), from_email=PAGERDUTY_API_FROM_EMAIL
-    )
+    try:
+        pagerduty_config = PagerdutyConfiguration(
+            api_key=str(PAGERDUTY_API_KEY), from_email=PAGERDUTY_API_FROM_EMAIL
+        )
+        migrate_config(instances, "pagerduty-oncall", pagerduty_config)
+    except ValidationError:
+        print(
+            "Skipping automatic migration of pagerduty plugin credentials, if you are using the pagerduty plugin manually migrate credentials."
+        )
 
     from dispatch.plugins.dispatch_zoom.plugin import ZoomConfiguration
 
@@ -119,9 +147,14 @@ def upgrade():
     ZOOM_API_KEY = config("ZOOM_API_KEY", default=None)
     ZOOM_API_SECRET = config("ZOOM_API_SECRET", cast=Secret, default=None)
 
-    if ZOOM_API_SECRET:
+    try:
         zoom_config = ZoomConfiguration(
             api_user_id=ZOOM_API_USER_ID, api_key=ZOOM_API_KEY, api_secret=str(ZOOM_API_SECRET)
+        )
+        migrate_config(instances, "zoom-conference", zoom_config)
+    except ValidationError:
+        print(
+            "Skipping automatic migration of zoom plugin credentials, if you are using the zoom plugin manually migrate credentials."
         )
 
     from dispatch.plugins.dispatch_jira.plugin import JiraConfiguration
@@ -134,7 +167,7 @@ def upgrade():
     JIRA_PROJECT_ID = config("JIRA_PROJECT_ID", default=None)
     JIRA_ISSUE_TYPE_NAME = config("JIRA_ISSUE_TYPE_NAME", default=None)
 
-    if JIRA_PASSWORD:
+    try:
         jira_config = JiraConfiguration(
             api_url=str(JIRA_API_URL),
             browser_url=str(JIRA_BROWSER_URL),
@@ -144,13 +177,23 @@ def upgrade():
             default_issue_type_name=JIRA_ISSUE_TYPE_NAME,
             password=str(JIRA_PASSWORD),
         )
+        migrate_config(instances, "jira-ticket", jira_config)
+    except ValidationError:
+        print(
+            "Skipping automatic migration of jira plugin credentials, if you are using the jira plugin manually migrate credentials."
+        )
 
     from dispatch.plugins.dispatch_opsgenie.plugin import OpsgenieConfiguration
 
     OPSGENIE_API_KEY = config("OPSGENIE_API_KEY", default=None, cast=Secret)
 
-    if OPSGENIE_API_KEY:
+    try:
         opsgenie_config = OpsgenieConfiguration(api_key=str(OPSGENIE_API_KEY), default=None)
+        migrate_config(instances, "opsgenie-oncall", opsgenie_config)
+    except ValidationError:
+        print(
+            "Skipping automatic migration of opsgenie plugin credentials, if you are using the opsgenie plugin manually migrate credentials."
+        )
 
     from dispatch.plugins.dispatch_slack.config import (
         SlackContactConfiguration,
@@ -217,8 +260,7 @@ def upgrade():
         "SLUG_COMMAND_LIST_WORKFLOWS_SLUG", default="/dispatch-list-workflows"
     )
 
-    if SLACK_API_BOT_TOKEN:
-
+    try:
         slack_conversation_config = SlackConversationConfiguration(
             api_bot_token=str(SLACK_API_BOT_TOKEN),
             socket_mode_app_token=str(SLACK_SOCKET_MODE_APP_TOKEN),
@@ -253,38 +295,13 @@ def upgrade():
             profile_weblink_field_id=SLACK_PROFILE_WEBLINK_FIELD_ID,
         )
 
-    bind = op.get_bind()
-    session = Session(bind=bind)
+        migrate_config(instances, "slack-conversation", slack_conversation_config)
+        migrate_config(instances, "slack-contact", slack_contact_config)
 
-    for instance in session.query(PluginInstance).all():
-        slug = instance.plugin.slug
-
-        if "google" in slug:
-            instance.configuration = google_config
-            continue
-
-        if slug == "slack-conversation":
-            instance.configuration = slack_conversation_config
-            continue
-
-        if slug == "slack-contact":
-            instance.configuration = slack_contact_config
-            continue
-
-        if slug == "jira-ticket":
-            instance.configuration = jira_config
-            continue
-
-        if slug == "zoom-conference":
-            instance.configuration = zoom_config
-            continue
-
-        if slug == "pagerduty-oncall":
-            instance.configuration = pagerduty_config
-            continue
-
-        if slug == "opsgenie-oncall":
-            instance.configuration = opsgenie_config
+    except ValidationError:
+        print(
+            "Skipping automatic migration of slack plugin credentials, if you are using the slack plugin manually migrate credentials."
+        )
 
     session.commit()
 
