64de34eb9c24f84351b9bd4b4386a8220326fb9f
kevgliss <kevgliss@gmail.com>
2021-10-06 13:57:15 -0700

    Update src/dispatch/incident/service.py
    Co-authored-by: Marc Vilanova <39573146+mvilanova@users.noreply.github.com>

--- src/dispatch/incident/service.py
+++ src/dispatch/incident/service.py
@@ -48,7 +48,7 @@ def resolve_and_associate_role(
         if oncall_plugin:
             email_address = oncall_plugin.instance.get(service_id=service_external_id)
 
-            # don't page commander for closed incidents or stable
+            # we don't page the incident commander for stable or closed incidents
             if incident.status == IncidentStatus.active:
                 if incident.incident_priority.page_commander:
                     oncall_plugin.instance.page(
