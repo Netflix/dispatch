3ab3defc2d031357485a80b0898f3f6e2091709b
Kevin Glisson <kevgliss@gmail.com>
2021-11-15 18:35:01 -0800

    Fixes an issue with marking storage readonly when there is none

--- src/dispatch/incident/flows.py
+++ src/dispatch/incident/flows.py
@@ -959,9 +959,9 @@ def incident_closed_status_flow(incident: Incident, db_session=None):
                 # typically only broad access to the incident document itself is required.
                 storage_plugin.instance.open(incident.incident_document.resource_id)
 
-    if storage_plugin.configuration.read_only:
-        # unfortunately this can't be applied at the folder level so we just mark the incident doc as available.
-        storage_plugin.instance.mark_readonly(incident.incident_document.resource_id)
+            if storage_plugin.configuration.read_only:
+                # unfortunately this can't be applied at the folder level so we just mark the incident doc as available.
+                storage_plugin.instance.mark_readonly(incident.incident_document.resource_id)
 
     # we send a direct message to the incident commander asking to review
     # the incident's information and to tag the incident if appropiate
