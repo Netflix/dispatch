acea461c16558b6be27392c3ad10d51a145e6f73
kevgliss <kevgliss@gmail.com>
2021-11-22 14:38:31 -0800

    Making project owner optional (#1836)
    * Making project owner optional
    
    * Removing import

--- src/dispatch/project/models.py
+++ src/dispatch/project/models.py
@@ -42,8 +42,8 @@ class Project(Base):
 class ProjectBase(DispatchBase):
     id: Optional[PrimaryKey]
     name: NameStr
-    owner_email: Optional[EmailStr] = None
-    owner_conversation: Optional[str] = None
+    owner_email: Optional[EmailStr] = Field(None, nullable=True)
+    owner_conversation: Optional[str] = Field(None, nullable=True)
     annual_employee_cost: Optional[int]
     business_year_hours: Optional[int]
     description: Optional[str] = Field(None, nullable=True)
