a43c92d5c4f9c9278194c4724c1487d3400e9831
Marc Vilanova <mvilanova@netflix.com>
2022-01-26 11:19:33 -0800

    Excludes name on org update

--- src/dispatch/organization/service.py
+++ src/dispatch/organization/service.py
@@ -131,7 +131,8 @@ def update(
     """Updates an organization."""
     organization_data = organization.dict()
 
-    update_data = organization_in.dict(skip_defaults=True, exclude={"banner_color"})
+    # we exclude the organization's name because the organization db schema is named after it
+    update_data = organization_in.dict(skip_defaults=True, exclude={"name", "banner_color"})
 
     for field in organization_data:
         if field in update_data:
