7212837f04cd11f9e61a52b3488e32c3161f531b
Marc Vilanova <mvilanova@netflix.com>
2022-03-07 14:56:07 -0800

    Improves query filter dialog

--- src/dispatch/static/dispatch/src/data/query/TableFilterDialog.vue
+++ src/dispatch/static/dispatch/src/data/query/TableFilterDialog.vue
@@ -7,25 +7,29 @@
     </template>
     <v-card>
       <v-card-title>
-        <span class="headline">Column Filters</span>
+        <span class="headline">Query Filters</span>
       </v-card-title>
       <v-list dense>
         <v-list-item>
           <v-list-item-content>
-            <project-combobox v-model="project" label="Projects" />
+            <project-combobox v-model="local_project" label="Projects" />
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
           <v-list-item-content>
-            <tag-filter-combobox v-model="tag" label="Tags" />
+            <tag-type-filter-combobox v-model="local_tag_type" label="Tag Types" />
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
           <v-list-item-content>
-            <tag-type-filter-combobox v-model="tag_type" label="Tag Types" />
+            <tag-filter-combobox v-model="local_tag" label="Tags" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="info" text @click="applyFilters()"> Apply Filters </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -33,33 +37,49 @@
 <script>
 import { sum } from "lodash"
 import { mapFields } from "vuex-map-fields"
+
+import ProjectCombobox from "@/project/ProjectCombobox.vue"
 import TagFilterCombobox from "@/tag/TagFilterCombobox.vue"
 import TagTypeFilterCombobox from "@/tag_type/TagTypeFilterCombobox.vue"
-import ProjectCombobox from "@/project/ProjectCombobox.vue"
 
 export default {
   name: "QueryTableFilterDialog",
 
   components: {
+    ProjectCombobox,
     TagFilterCombobox,
     TagTypeFilterCombobox,
-    ProjectCombobox,
   },
 
   data() {
     return {
       display: false,
+      local_project: [],
+      local_tag_type: [],
+      local_tag: [],
     }
   },
 
   computed: {
     ...mapFields("query", [
-      "table.options.filters.tag_type",
       "table.options.filters.project",
+      "table.options.filters.tag_type",
       "table.options.filters.tag",
     ]),
     numFilters: function () {
       return sum([this.project.length, this.tag.length, this.tag_type.length])
+    },
+  },
+
+  methods: {
+    applyFilters() {
+      // we set the filter values
+      this.project = this.local_project
+      this.project = this.local_tag_type
+      this.project = this.local_tag
+
+      // we close the dialog
+      this.display = false
     },
   },
 }
