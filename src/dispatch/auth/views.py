80d67c29db91908dc5576ceded41beca3ffb6fd7
Marc Vilanova <mvilanova@netflix.com>
2022-07-01 14:12:12 -0700

    Removes unused import

--- src/dispatch/auth/views.py
+++ src/dispatch/auth/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic.error_wrappers import ErrorWrapper, ValidationError
 from sqlalchemy.orm import Session
 
