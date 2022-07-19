764ad1aa8631a3b3bc9df7921fed2ce9e73b1dfd
Marc Vilanova <mvilanova@netflix.com>
2022-07-19 13:40:11 -0700

    Adds models, service, and views modules for case priority

---
+++ src/dispatch/case/priority/views.py
@@ -0,0 +1,78 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from dispatch.database.core import get_db
+from dispatch.database.service import common_parameters, search_filter_sort_paginate
+from dispatch.auth.permissions import SensitiveProjectActionPermission, PermissionsDependency
+from dispatch.models import PrimaryKey
+
+from .models import (
+    CasePriorityCreate,
+    CasePriorityPagination,
+    CasePriorityRead,
+    CasePriorityUpdate,
+)
+from .service import create, get, update
+
+
+router = APIRouter()
+
+
+@router.get("", response_model=CasePriorityPagination, tags=["case_priorities"])
+def get_case_priorities(*, common: dict = Depends(common_parameters)):
+    """Returns all case priorities."""
+    return search_filter_sort_paginate(model="CasePriority", **common)
+
+
+@router.post(
+    "",
+    response_model=CasePriorityRead,
+    dependencies=[Depends(PermissionsDependency([SensitiveProjectActionPermission]))],
+)
+def create_case_priority(
+    *,
+    db_session: Session = Depends(get_db),
+    case_priority_in: CasePriorityCreate,
+):
+    """Create a new case priority."""
+    case_priority = create(db_session=db_session, case_priority_in=case_priority_in)
+    return case_priority
+
+
+@router.put(
+    "/{case_priority_id}",
+    response_model=CasePriorityRead,
+    dependencies=[Depends(PermissionsDependency([SensitiveProjectActionPermission]))],
+)
+def update_case_priority(
+    *,
+    db_session: Session = Depends(get_db),
+    case_priority_id: PrimaryKey,
+    case_priority_in: CasePriorityUpdate,
+):
+    """Update an existing case priority."""
+    case_priority = get(db_session=db_session, case_priority_id=case_priority_id)
+    if not case_priority:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=[{"msg": "A case priority with this id does not exist."}],
+        )
+
+    case_priority = update(
+        db_session=db_session,
+        case_priority=case_priority,
+        case_priority_in=case_priority_in,
+    )
+    return case_priority
+
+
+@router.get("/{case_priority_id}", response_model=CasePriorityRead)
+def get_case_priority(*, db_session: Session = Depends(get_db), case_priority_id: PrimaryKey):
+    """Get a case priority."""
+    case_priority = get(db_session=db_session, case_priority_id=case_priority_id)
+    if not case_priority:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=[{"msg": "A case priority with this id does not exist."}],
+        )
+    return case_priority
