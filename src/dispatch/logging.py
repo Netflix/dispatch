94c3cb6c826cd6a12ec9d58b66fb835f675bb59a
Filippo Giunchedi <filippog@users.noreply.github.com>
2022-08-20 01:22:25 +0200

    Always upper log_level before logging config (#2440)
    In other words don't require config/environment LOG_LEVEL to be upper
    case, also consistent with what --log-level expects.
    
    Co-authored-by: Filippo Giunchedi <filippo@debian.org>

--- src/dispatch/logging.py
+++ src/dispatch/logging.py
@@ -3,9 +3,10 @@ from dispatch.config import LOG_LEVEL
 
 
 def configure_logging():
-    if LOG_LEVEL == "DEBUG":
+    level = LOG_LEVEL.upper()
+    if level == "DEBUG":
         # log level:logged message:full module path:function invoked:line number of logging call
         LOGFORMAT = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
-        logging.basicConfig(level=LOG_LEVEL, format=LOGFORMAT)
+        logging.basicConfig(level=level, format=LOGFORMAT)
     else:
-        logging.basicConfig(level=LOG_LEVEL)
+        logging.basicConfig(level=level)
