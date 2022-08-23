717f4291f5735a6f32e2a7c0d571f7155645ef43
Marc Vilanova <mvilanova@netflix.com>
2022-08-23 13:11:18 -0700

    Fixes colors for case priorities

--- src/dispatch/static/dispatch/src/case/priority/CasePriority.vue
+++ src/dispatch/static/dispatch/src/case/priority/CasePriority.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="pl-5">
     <div v-if="priority == 'Low'">
-      <v-badge bordered color="info" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="green" dot left offset-x="-10" offset-y="12">
         {{ priority }}
       </v-badge>
     </div>
     <div v-else-if="priority == 'Medium'">
-      <v-badge bordered color="warning" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="orange" dot left offset-x="-10" offset-y="12">
         {{ priority }}
       </v-badge>
     </div>
     <div v-else-if="priority == 'High'">
-      <v-badge bordered color="error" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
         {{ priority }}
       </v-badge>
     </div>
     <div v-else>
-      <v-badge bordered color="error" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
         {{ priority }}
       </v-badge>
     </div>
