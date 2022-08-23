dc2ef55c096635036ccdb06c7efa046672a1c07b
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2022-08-23 13:41:50 -0700

    Fixes case severity colors (#2454)

--- src/dispatch/static/dispatch/src/case/severity/CaseSeverity.vue
+++ src/dispatch/static/dispatch/src/case/severity/CaseSeverity.vue
@@ -6,22 +6,22 @@
       </v-badge>
     </div>
     <div v-else-if="severity == 'Medium'">
-      <v-badge bordered color="yellow" dot left offset-x="-10" offset-y="12">
-        {{ severity }}
-      </v-badge>
-    </div>
-    <div v-else-if="severity == 'High'">
       <v-badge bordered color="orange" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
     </div>
-    <div v-else-if="severity == 'Critical'">
+    <div v-else-if="severity == 'High'">
       <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
     </div>
+    <div v-else-if="severity == 'Critical'">
+      <v-badge bordered color="purple" dot left offset-x="-10" offset-y="12">
+        {{ severity }}
+      </v-badge>
+    </div>
     <div v-else>
-      <v-badge bordered color="error" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
     </div>
