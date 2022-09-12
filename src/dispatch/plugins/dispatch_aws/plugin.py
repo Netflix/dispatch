2826b01070e5645aa810f40ed181d5cb797275b8
Kevin Glisson <kevgliss@gmail.com>
2022-09-12 11:20:10 -0700

    Adding sqs queue consumption

--- src/dispatch/plugins/dispatch_aws/plugin.py
+++ src/dispatch/plugins/dispatch_aws/plugin.py
@@ -4,15 +4,23 @@
     :license: Apache, see LICENSE for more details.
 """
 import logging
-from typing import List
+from pydantic import Field
+
+import boto3
+from dispatch.config import BaseConfigurationModel
 
 from dispatch.decorators import apply, counter, timer
 from dispatch.plugins import dispatch_aws as aws_plugin
 from dispatch.plugins.bases import SignalConsumerPlugin
 
-from .config import AWSConfiguration
-
 log = logging.getLogger(__name__)
+
+
+class AWSSQSConfiguration(BaseConfigurationModel):
+    queue_url: str = Field(title="Queue to pull from.")
+    wait_seconds: int = Field(
+        title="Number of seconds to wait before returning an empty message.", default=10
+    )
 
 
 @apply(timer, exclude=["__init__"])
@@ -27,8 +35,15 @@ class AWSSQSSignalConsumerPlugin(SignalConsumerPlugin):
     author_url = "https://github.com/netflix/dispatch.git"
 
     def __init__(self):
-        self.configuration_schema = AWSConfiguration
+        self.configuration_schema = AWSSQSConfiguration
 
     def consume(self):
-        """Enriches an event."""
-        return {}
+        """Consumes a message from a specified SQS queue."""
+        sqs = boto3.client("sqs")
+        response = sqs.receive_message(
+            QueryUrl=self.configuration.queue_name,
+            AttributeNames=["SentTimestamp"],
+            MessageAttributeNames=["All"],
+            WaitTimeSeconds=self.configuration.wait_seconds,
+        )
+        return response
