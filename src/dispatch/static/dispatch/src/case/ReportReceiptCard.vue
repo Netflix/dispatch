89951f5a4fd7b1face23a0dd079fdb26fefb1509
Kevin Glisson <kevgliss@gmail.com>
2022-09-15 09:16:22 -0700

    Adding report forms

---
+++ src/dispatch/static/dispatch/src/case/ReportReceiptCard.vue
@@ -0,0 +1,263 @@
+<template>
+  <v-card class="mx-auto ma-4" max-width="600" flat outlined>
+    <v-card-text>
+      <p class="display-2 text--primary">Incident Report</p>
+      <p>
+        This page will be populated with case resources as they are created (if available). If you
+        have any questions, please feel free to review the Frequently Asked Questions (FAQ) document
+        linked below, and/or reach out to the listed assignee.
+      </p>
+      <p v-if="project_faq">
+        Checkout this project's case FAQ document:
+        <a :href="project_faq.weblink" target="_blank" style="text-decoration: none">
+          {{ project_faq.name }}
+          <v-icon small>open_in_new</v-icon>
+        </a>
+      </p>
+      <v-list three-line>
+        <v-list-group :value="true">
+          <template v-slot:activator>
+            <v-list-item-title class="title"> Case Details </v-list-item-title>
+          </template>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Assignee</v-list-item-title>
+                <v-list-item-subtitle>
+                  <participant :participant="assignee" />
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Title</v-list-item-title>
+                <v-list-item-subtitle>{{ title }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Description</v-list-item-title>
+                <v-list-item-subtitle>{{ description }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Project</v-list-item-title>
+                <v-list-item-subtitle>{{ project.name }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Type</v-list-item-title>
+                <v-list-item-subtitle>{{ case_type.name }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Priority</v-list-item-title>
+                <v-list-item-subtitle>{{ case_priority.name }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Visibility</v-list-item-title>
+                <v-list-item-subtitle>{{ visibility }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list-group>
+      </v-list>
+      <v-list three-line>
+        <v-list-group :value="true">
+          <template v-slot:activator>
+            <v-list-item-title class="title"> Case Resources </v-list-item-title>
+          </template>
+          <span v-if="activeResourcePlugins.ticket">
+            <v-list-item v-if="ticket" :href="ticket.weblink" target="_blank">
+              <v-list-item-content>
+                <v-list-item-title>Ticket</v-list-item-title>
+                <v-list-item-subtitle>{{ ticket.description }}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-icon>
+                  <v-icon>open_in_new</v-icon>
+                </v-list-item-icon>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item v-else>
+              <v-list-item-content>
+                <v-list-item-title>Creating incident ticket...</v-list-item-title>
+                <v-progress-linear indeterminate />
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+          </span>
+          <span v-if="activeResourcePlugins.storage">
+            <v-list-item v-if="storage" :href="storage.weblink" target="_blank">
+              <v-list-item-content>
+                <v-list-item-title>Storage</v-list-item-title>
+                <v-list-item-subtitle>{{ storage.description }}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-icon>
+                  <v-icon>open_in_new</v-icon>
+                </v-list-item-icon>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item v-else>
+              <v-list-item-content>
+                <v-list-item-title>Creating incident storage...</v-list-item-title>
+                <v-progress-linear indeterminate />
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <span v-if="activeResourcePlugins.document">
+              <span v-if="documents.length">
+                <span v-for="document in documents" :key="document.resource_id">
+                  <v-list-item :href="document.weblink" target="_blank">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ document.resource_type | deslug }}</v-list-item-title>
+                      <v-list-item-subtitle>{{ document.description }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <v-list-item-icon>
+                        <v-icon>open_in_new</v-icon>
+                      </v-list-item-icon>
+                    </v-list-item-action>
+                  </v-list-item>
+                  <v-divider />
+                </span>
+              </span>
+              <span v-else>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title>Creating incident documents... </v-list-item-title>
+                    <v-progress-linear indeterminate />
+                  </v-list-item-content>
+                </v-list-item>
+              </span>
+              <v-divider />
+            </span>
+          </span>
+        </v-list-group>
+      </v-list>
+      <v-container grid-list-md>
+        <v-flex xs12>
+          <v-btn color="info" depressed @click="resetSelected()"> Report another incident </v-btn>
+        </v-flex>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapFields } from "vuex-map-fields"
+import { mapActions } from "vuex"
+import { forEach, find } from "lodash"
+import DocumentApi from "@/document/api"
+import Participant from "@/incident/Participant.vue"
+
+import PluginApi from "@/plugin/api"
+
+export default {
+  name: "CaseReportReceiptCard",
+
+  components: {
+    Participant,
+  },
+  data() {
+    return {
+      isSubmitted: false,
+      activeResourcePlugins: {
+        document: null,
+        ticket: null,
+        storage: null,
+        conversation: null,
+        conference: null,
+      },
+      project_faq: null,
+    }
+  },
+  created() {
+    this.getFAQ()
+    PluginApi.getAllInstances({
+      itemsPerPage: -1,
+      filter: JSON.stringify({
+        and: [
+          {
+            model: "PluginInstance",
+            field: "enabled",
+            op: "==",
+            value: "true",
+          },
+          {
+            model: "Project",
+            field: "name",
+            op: "==",
+            value: this.project.name,
+          },
+        ],
+      }),
+    }).then((response) => {
+      let data = response.data.items
+      let activeResourcePlugins = this.activeResourcePlugins
+      forEach(Object.keys(activeResourcePlugins), function (value) {
+        activeResourcePlugins[value] = find(data, function (o) {
+          return o.plugin.type === value
+        })
+      })
+    })
+  },
+  computed: {
+    ...mapFields("case", [
+      "selected.case_priority",
+      "selected.case_type",
+      "selected.assignee",
+      "selected.title",
+      "selected.tags",
+      "selected.description",
+      "selected.visibility",
+      "selected.storage",
+      "selected.documents",
+      "selected.project",
+      "selected.loading",
+      "selected.ticket",
+      "selected.id",
+    ]),
+  },
+
+  methods: {
+    getFAQ() {
+      DocumentApi.getAll({
+        filter: JSON.stringify({
+          and: [
+            {
+              field: "resource_type",
+              op: "==",
+              value: "dispatch-faq-reference-document",
+            },
+            {
+              model: "Project",
+              field: "name",
+              op: "==",
+              value: this.project.name,
+            },
+          ],
+        }),
+      }).then((response) => {
+        if (response.data && response.data.items.length) {
+          this.project_faq = response.data.items[0]
+        }
+      })
+    },
+    ...mapActions("case", ["report", "get", "resetSelected"]),
+  },
+}
+</script>
---
+++ src/dispatch/static/dispatch/src/case/ReportSubmissionCard.vue
@@ -0,0 +1,205 @@
+<template>
+  <ValidationObserver v-slot="{ invalid, validated }">
+    <v-card class="mx-auto ma-4" max-width="600" flat outlined :loading="loading">
+      <v-card-text>
+        <p class="display-1 text--primary">Report Issue</p>
+        <p>
+          If you suspect a security issue and need help, please fill out this form to the best of
+          your abilities.
+        </p>
+        <p v-if="project_faq">
+          If you have additional questions, please check out the following FAQ document:
+          <a :href="project_faq.weblink" target="_blank" style="text-decoration: none">
+            {{ project_faq.name }}
+            <v-icon small>open_in_new</v-icon>
+          </a>
+        </p>
+        <v-form>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <ValidationProvider name="Title" rules="required" immediate>
+                  <v-textarea
+                    v-model="title"
+                    slot-scope="{ errors, valid }"
+                    :error-messages="errors"
+                    :success="valid"
+                    label="Title"
+                    hint="A brief explanatory title. You can change this later."
+                    clearable
+                    auto-grow
+                    rows="2"
+                    required
+                  />
+                </ValidationProvider>
+              </v-flex>
+              <v-flex xs12>
+                <ValidationProvider name="Description" rules="required" immediate>
+                  <v-textarea
+                    v-model="description"
+                    slot-scope="{ errors, valid }"
+                    :error-messages="errors"
+                    :success="valid"
+                    label="Description"
+                    hint="A summary of what you know so far. It's all right if this is incomplete."
+                    clearable
+                    auto-grow
+                    rows="3"
+                    required
+                  />
+                </ValidationProvider>
+              </v-flex>
+              <v-flex xs12>
+                <project-select v-model="project" />
+              </v-flex>
+              <v-flex xs12>
+                <case-type-select :project="project" v-model="case_type" />
+              </v-flex>
+              <v-flex xs12>
+                <case-priority-select :project="project" v-model="case_priority" />
+              </v-flex>
+              <v-flex xs12>
+                <tag-filter-auto-complete :project="project" v-model="tags" label="Tags" />
+              </v-flex>
+            </v-layout>
+            <template>
+              <v-btn
+                color="info"
+                depressed
+                :loading="loading"
+                :disabled="invalid || !validated"
+                @click="report()"
+              >
+                Submit
+                <template v-slot:loader>
+                  <v-progress-linear indeterminate color="white" />
+                </template>
+              </v-btn>
+            </template>
+          </v-container>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </ValidationObserver>
+</template>
+
+<script>
+import { mapFields } from "vuex-map-fields"
+import { mapActions } from "vuex"
+import { ValidationObserver, ValidationProvider, extend } from "vee-validate"
+import { required } from "vee-validate/dist/rules"
+import CaseTypeSelect from "@/case/type/CaseTypeSelect.vue"
+import CasePrioritySelect from "@/case/priority/CasePrioritySelect.vue"
+import ProjectSelect from "@/project/ProjectSelect.vue"
+import DocumentApi from "@/document/api"
+import TagFilterAutoComplete from "@/tag/TagFilterAutoComplete.vue"
+
+extend("required", {
+  ...required,
+  message: "This field is required",
+})
+
+export default {
+  name: "ReportSubmissionCard",
+
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+    CaseTypeSelect,
+    CasePrioritySelect,
+    ProjectSelect,
+    TagFilterAutoComplete,
+  },
+
+  data() {
+    return {
+      isSubmitted: false,
+      project_faq: null,
+    }
+  },
+
+  computed: {
+    ...mapFields("case", [
+      "selected.case_priority",
+      "selected.case_type",
+      "selected.commander",
+      "selected.title",
+      "selected.tags",
+      "selected.description",
+      "selected.conversation",
+      "selected.conference",
+      "selected.visibility",
+      "selected.storage",
+      "selected.documents",
+      "selected.loading",
+      "selected.ticket",
+      "selected.project",
+      "selected.id",
+    ]),
+    ...mapFields("route", ["query"]),
+  },
+
+  methods: {
+    getFAQ() {
+      DocumentApi.getAll({
+        filter: JSON.stringify({
+          and: [
+            {
+              field: "resource_type",
+              op: "==",
+              value: "dispatch-faq-reference-document",
+            },
+            {
+              model: "Project",
+              field: "name",
+              op: "==",
+              value: this.project.name,
+            },
+          ],
+        }),
+      }).then((response) => {
+        if (response.data.items.length) {
+          this.project_faq = response.data.items[0]
+        }
+      })
+    },
+    ...mapActions("case", ["report", "get", "resetSelected"]),
+  },
+
+  created() {
+    this.project = { name: "" }
+    if (this.query.project) {
+      this.project = { name: this.query.project }
+    }
+
+    this.case_type = { name: "" }
+    if (this.query.case_type) {
+      this.case_type = { name: this.query.case_type }
+    }
+
+    this.case_priority = { name: "" }
+    if (this.query.case_priority) {
+      this.case_priority = { name: this.query.case_priority }
+    }
+
+    this.getFAQ()
+
+    this.$watch(
+      (vm) => [vm.project],
+      () => {
+        this.getFAQ()
+      }
+    )
+
+    if (this.query.tag) {
+      if (Array.isArray(this.query.tag)) {
+        this.tags = this.query.tag.map(function (t) {
+          return { name: t }
+        })
+      } else {
+        this.tags = [{ name: this.query.tag }]
+      }
+    }
+  },
+}
+</script>
