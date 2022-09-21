27b55a7e9e102e93ba1acad43b7a26bd2dd2335d
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2022-09-21 09:22:08 -0700

    Improves logging configuration (#2544)
    * Improves logging configuration
    
    * Moves enum class inside logging.py

--- src/dispatch/logging.py
+++ src/dispatch/logging.py
@@ -1,12 +1,30 @@
 import logging
+
 from dispatch.config import LOG_LEVEL
+from dispatch.enums import DispatchEnum
+
+
+LOG_FORMAT_DEBUG = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
+
+
+class LogLevels(DispatchEnum):
+    info = "INFO"
+    warn = "WARN"
+    error = "ERROR"
+    debug = "DEBUG"
 
 
 def configure_logging():
-    level = LOG_LEVEL.upper()
-    if level == "DEBUG":
-        # log level:logged message:full module path:function invoked:line number of logging call
-        LOGFORMAT = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
-        logging.basicConfig(level=level, format=LOGFORMAT)
-    else:
-        logging.basicConfig(level=level)
+    log_level = str(LOG_LEVEL).upper()  # cast to string
+    log_levels = [level for level in LogLevels]
+
+    if log_level not in log_levels:
+        # we use error as the default log level
+        logging.basicConfig(level=LogLevels.error)
+        return
+
+    if log_level == LogLevels.debug:
+        logging.basicConfig(level=log_level, format=LOG_FORMAT_DEBUG)
+        return
+
+    logging.basicConfig(level=log_level)
