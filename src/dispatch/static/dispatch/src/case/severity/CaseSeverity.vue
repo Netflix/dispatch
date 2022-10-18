a23e27997e3e8b0fdfa06ad1443cdf647bd37b11
Marc Vilanova <mvilanova@netflix.com>
2022-10-18 10:35:52 -0700

    Updates default color for case and incident severities

--- src/dispatch/static/dispatch/src/case/severity/CaseSeverity.vue
+++ src/dispatch/static/dispatch/src/case/severity/CaseSeverity.vue
@@ -5,7 +5,7 @@
         {{ severity }}
       </v-badge>
     </div>
-    <div v-else-if="severity == 'Medium'">
+    <div v-else-if="severity == 'Moderate'">
       <v-badge bordered color="orange" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
@@ -21,7 +21,7 @@
       </v-badge>
     </div>
     <div v-else>
-      <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="grey" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
     </div>
--- src/dispatch/static/dispatch/src/incident/severity/IncidentSeverity.vue
+++ src/dispatch/static/dispatch/src/incident/severity/IncidentSeverity.vue
@@ -21,7 +21,7 @@
       </v-badge>
     </div>
     <div v-else>
-      <v-badge bordered color="red" dot left offset-x="-10" offset-y="12">
+      <v-badge bordered color="grey" dot left offset-x="-10" offset-y="12">
         {{ severity }}
       </v-badge>
     </div>
