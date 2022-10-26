70e2e34f4f5988aeb303a010cf49337c4d112f35
Marc Vilanova <mvilanova@netflix.com>
2022-10-26 16:03:22 -0700

    Moves activity column to participant role model

--- src/dispatch/participant_role/models.py
+++ src/dispatch/participant_role/models.py
@@ -15,6 +15,7 @@ class ParticipantRole(Base):
     assumed_at = Column(DateTime, default=datetime.utcnow)
     renounced_at = Column(DateTime)
     role = Column(String, default=ParticipantRoleType.participant)
+    activity = Column(Integer)
     participant_id = Column(Integer, ForeignKey("participant.id", ondelete="CASCADE"))
 
 
@@ -35,6 +36,7 @@ class ParticipantRoleRead(ParticipantRoleBase):
     id: PrimaryKey
     assumed_at: Optional[datetime] = None
     renounced_at: Optional[datetime] = None
+    activity: Optional[int] = 0
 
 
 class ParticipantRolePagination(ParticipantRoleBase):
