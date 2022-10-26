12ea739c16926db9941c7be1cb5eaf6ae864fba1
Marc Vilanova <mvilanova@netflix.com>
2022-10-26 14:34:46 -0700

    Changes order

--- src/dispatch/participant_role/enums.py
+++ src/dispatch/participant_role/enums.py
@@ -4,7 +4,7 @@ from dispatch.enums import DispatchEnum
 class ParticipantRoleType(DispatchEnum):
     incident_commander = "Incident Commander"
     liaison = "Liaison"
-    observer = "Observer"
-    participant = "Participant"
-    reporter = "Reporter"
     scribe = "Scribe"
+    participant = "Participant"
+    observer = "Observer"
+    reporter = "Reporter"
