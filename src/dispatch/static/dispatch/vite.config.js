66acd97c1d15a1ab6a71bd46e4d787a00d36908e
Kevin Glisson <kevgliss@gmail.com>
2022-10-28 11:21:23 -0700

    Adds vite files

---
+++ src/dispatch/static/dispatch/vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite"
+import vue2 from "@vitejs/plugin-vue2"
+import monacoEditorPlugin from "vite-plugin-monaco-editor"
+import { VuetifyResolver } from "unplugin-vue-components/resolvers"
+import Components from "unplugin-vue-components/vite"
+
+import path from "path"
+
+export default defineConfig({
+  plugins: [
+    vue2(),
+    monacoEditorPlugin({ languageWorkers: ["json"] }),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver(),
+      ],
+    }),
+  ],
+  server: {
+    port: 8080,
+    proxy: {
+      "^/api": {
+        target: "http://127.0.0.1:8000",
+        ws: false,
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+    ],
+  },
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
+})
