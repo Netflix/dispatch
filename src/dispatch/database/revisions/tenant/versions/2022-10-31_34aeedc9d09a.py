9631cf8ce12512ee89c301d38a052b6fdfa8a908
kevgliss <kevgliss@gmail.com>
2022-10-31 09:42:58 -0700

    Fixing import and database revision branches (#2640)
    * Fixing import and database revision branches

---
+++ src/dispatch/database/revisions/tenant/versions/2022-10-31_34aeedc9d09a.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 34aeedc9d09a
+Revises: 4b65941d065a, 01aa49ca0470, 3a5e776ddce4
+Create Date: 2022-10-31 09:41:11.971654
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '34aeedc9d09a'
+down_revision = ('4b65941d065a', '01aa49ca0470', '3a5e776ddce4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
