7fc9e405791307fb2b771bad20151ffe9f5564f8
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2022-11-29 14:36:08 -0800

    Replaces localhost with 127.0.0.1 in Vite config to resolve connection issue with Node and Vite (#2731)

--- src/dispatch/static/dispatch/vite.config.js
+++ src/dispatch/static/dispatch/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       "^/api": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000",
         ws: false,
         changeOrigin: true,
       },
