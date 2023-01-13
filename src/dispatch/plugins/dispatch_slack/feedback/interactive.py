507e04d2049e27dfe7951855d8fae0f27d82d61b
Marc Vilanova <mvilanova@netflix.com>
2023-01-13 15:58:50 -0800

    Fixes incident feedback button and modal

--- src/dispatch/plugins/dispatch_slack/feedback/interactive.py
+++ src/dispatch/plugins/dispatch_slack/feedback/interactive.py
@@ -1,16 +1,30 @@
-from blockkit import Checkboxes, Context, Input, MarkdownText, Modal, PlainTextInput
+from blockkit import (
+    Checkboxes,
+    Context,
+    Input,
+    MarkdownText,
+    Modal,
+    PlainOption,
+    PlainTextInput,
+    Section,
+)
 
-from dispatch.conversation.enums import ConversationButtonActions
-from dispatch.enums import DispatchEnum
+from slack_bolt.async_app import AsyncAck, AsyncBoltContext, AsyncRespond
+from slack_sdk.web.async_client import AsyncWebClient
+
+from sqlalchemy.orm import Session
+
+from dispatch.auth.models import DispatchUser
+from dispatch.database.core import refetch_db_session
 from dispatch.feedback import service as feedback_service
 from dispatch.feedback.enums import FeedbackRating
 from dispatch.feedback.models import FeedbackCreate
 from dispatch.incident import service as incident_service
 from dispatch.participant import service as participant_service
-
-from .bolt import app
-from .fields import static_select_block
-from .middleware import (
+from dispatch.plugins.dispatch_slack.bolt import app
+from dispatch.plugins.dispatch_slack.decorators import handle_lazy_error
+from dispatch.plugins.dispatch_slack.fields import static_select_block
+from dispatch.plugins.dispatch_slack.middleware import (
     action_context_middleware,
     button_context_middleware,
     db_middleware,
@@ -18,22 +32,16 @@ from .middleware import (
     user_middleware,
 )
 
-
-class FeedbackNotificationBlockIds(DispatchEnum):
-    feedback_input = "feedback-notification-feedback-input"
-    rating_select = "feedback-notification-rating-select"
-    anonymous_checkbox = "feedback-notification-anonymous-checkbox"
-
-
-class FeedbackNotificationActionIds(DispatchEnum):
-    feedback_input = "feedback-notification-feedback-input"
-    rating_select = "feedback-notification-rating-select"
-    anonymous_checkbox = "feedback-notification-anonymous-checkbox"
+from .enums import (
+    FeedbackNotificationActionIds,
+    FeedbackNotificationActions,
+    FeedbackNotificationBlockIds,
+)
 
 
-class FeedbackNotificationActions(DispatchEnum):
-    submit = "feedback-notification-submit"
-    provide = ConversationButtonActions.feedback_notification_provide
+def configure(config):
+    """Placeholder configure function."""
+    pass
 
 
 def rating_select(
@@ -43,12 +51,13 @@ def rating_select(
     label: str = "Rate your experience",
     **kwargs,
 ):
+    rating_options = [{"text": r.value, "value": r.value} for r in FeedbackRating]
     return static_select_block(
         action_id=action_id,
         block_id=block_id,
         initial_option=initial_option,
         label=label,
-        options=[{"text": r, "value": r} for r in FeedbackRating],
+        options=rating_options,
         placeholder="Select a rating",
         **kwargs,
     )
@@ -81,11 +90,12 @@ def anonymous_checkbox(
     label: str = "Check the box if you wish to provide your feedback anonymously",
     **kwargs,
 ):
-    options = [{"text": "Anonymize my feedback", "value": "anonymous"}]
+    options = [PlainOption(text="Anonymize my feedback", value="anonymous")]
     return Input(
         block_id=block_id,
-        element=Checkboxes(options=options, initial_value=initial_value, action_id=action_id),
+        element=Checkboxes(options=options, action_id=action_id),
         label=label,
+        optional=True,
         **kwargs,
     )
 
@@ -93,12 +103,17 @@ def anonymous_checkbox(
 @app.action(
     FeedbackNotificationActions.provide, middleware=[button_context_middleware, db_middleware]
 )
-async def provide_feedback_button_click(ack, body, client, respond, db_session, context):
-    print("Hello")
+async def handle_feedback_direct_message_button_click(
+    ack: AsyncAck,
+    body: dict,
+    client: AsyncWebClient,
+    respond: AsyncRespond,
+    db_session: Session,
+    context: AsyncBoltContext,
+):
+    """Handles the feedback button in the feedback direct message."""
     await ack()
-    incident = incident_service.get(
-        db_session=db_session, incident_id=context["subject"].incident_id
-    )
+    incident = incident_service.get(db_session=db_session, incident_id=context["subject"].id)
 
     if not incident:
         message = (
@@ -106,6 +121,7 @@ async def provide_feedback_button_click(ack, body, client, respond, db_session, 
         )
         await respond(message=message, ephemeral=True)
         return
+
     blocks = [
         Context(
             elements=[
@@ -125,18 +141,36 @@ async def provide_feedback_button_click(ack, body, client, respond, db_session, 
         callback_id=FeedbackNotificationActions.submit,
         private_metadata=context["subject"].json(),
     ).build()
+
     await client.views_open(trigger_id=body["trigger_id"], view=modal)
 
 
-@app.view(
-    FeedbackNotificationActions.submit,
-    middleware=[action_context_middleware, db_middleware, user_middleware, modal_submit_middleware],
-)
-async def handle_feedback_submission_event(ack, body, context, db_session, user, client, form_data):
-    await ack()
-    incident = incident_service.get(
-        db_session=db_session, incident_id=context["subject"].incident_id
-    )
+async def ack_feedback_submission_event(ack: AsyncAck) -> None:
+    """Handles the feednack submission event acknowledgement."""
+    modal = Modal(
+        title="Incident Feedback", close="Close", blocks=[Section(text="Submitting feedback...")]
+    ).build()
+    await ack(response_action="update", view=modal)
+
+
+# @app.view(
+# 	  FeedbackNotificationActions.submit,
+# 	  middleware=[action_context_middleware, db_middleware, user_middleware, modal_submit_middleware],
+# )
+@handle_lazy_error
+async def handle_feedback_submission_event(
+    ack: AsyncAck,
+    body: dict,
+    context: AsyncBoltContext,
+    user: DispatchUser,
+    client: AsyncWebClient,
+    form_data: dict,
+):
+    # await ack()
+    # TODO: handle multiple organizations during submission
+    db_session = refetch_db_session(context["subject"].organization_slug)
+
+    incident = incident_service.get(db_session=db_session, incident_id=context["subject"].id)
 
     feedback = form_data.get(FeedbackNotificationBlockIds.feedback_input)
     rating = form_data.get(FeedbackNotificationBlockIds.rating_select, {}).get("value")
@@ -150,7 +184,7 @@ async def handle_feedback_submission_event(ack, body, context, db_session, user,
     # we only really care if this exists, if it doesn't then flag is false
     if not form_data.get(FeedbackNotificationBlockIds.anonymous_checkbox):
         participant = participant_service.get_by_incident_id_and_email(
-            db_session=db_session, incident_id=context["subject"].incident_id, email=user.email
+            db_session=db_session, incident_id=context["subject"].id, email=user.email
         )
         participant.feedback.append(feedback)
         db_session.add(participant)
@@ -158,4 +192,24 @@ async def handle_feedback_submission_event(ack, body, context, db_session, user,
     db_session.add(incident)
     db_session.commit()
 
-    await client.chat_PostMessage(text="Thank you for your feedback!")
+    modal = Modal(
+        title="Incident Feedback",
+        close="Close",
+        blocks=[Section(text="Submitting feedback... Success!")],
+    ).build()
+
+    await client.views_update(
+        view_id=body["view"]["id"],
+        view=modal,
+    )
+
+
+# @app.view(
+# 	  FeedbackNotificationActions.submit,
+# 	  middleware=[action_context_middleware, db_middleware, user_middleware, modal_submit_middleware],
+# )
+
+app.view(
+    FeedbackNotificationActions.submit,
+    middleware=[action_context_middleware, db_middleware, user_middleware, modal_submit_middleware],
+)(ack=ack_feedback_submission_event, lazy=[handle_feedback_submission_event])
