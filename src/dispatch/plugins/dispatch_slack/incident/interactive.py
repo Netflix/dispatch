8af40422ae3c8c0f578e0e0d7b79cb5177a2543d
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-01-19 15:21:50 -0800

    initial work to add missing logic to update_task buttons

--- src/dispatch/plugins/dispatch_slack/incident/interactive.py
+++ src/dispatch/plugins/dispatch_slack/incident/interactive.py
@@ -559,7 +559,7 @@ def handle_list_tasks_command(
                     accessory=Button(
                         text=button_text,
                         value=button_metadata,
-                        action_id=TaskNotificationActionIds.update_status,
+                        action_id=ConversationButtonActions.update_task_status,
                     ),
                 )
             )
