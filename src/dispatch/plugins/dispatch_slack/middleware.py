d6c84b667d407611c4e5913fc56672f3a03b5786
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2023-01-26 09:01:19 -0800

    Fixes timeline reaction event in incident conversation (#2899)
    * Fixes timeline reaction event in incident conversation
    
    * Update src/dispatch/plugins/dispatch_slack/middleware.py
    
    Co-authored-by: Will Sheldon <114631109+wssheldon@users.noreply.github.com>
    
    Co-authored-by: Will Sheldon <114631109+wssheldon@users.noreply.github.com>

--- src/dispatch/plugins/dispatch_slack/middleware.py
+++ src/dispatch/plugins/dispatch_slack/middleware.py
@@ -112,6 +112,15 @@ def message_context_middleware(request: BoltRequest, context: BoltContext, next:
     next()
 
 
+def reaction_context_middleware(context: BoltContext, next: Callable) -> None:
+    """Attemps to determine the current context of a reaction event."""
+    if subject := resolve_context_from_conversation(channel_id=context.channel_id):
+        context.update(subject._asdict())
+    else:
+        raise ContextError("Unable to determine context for reaction.")
+    next()
+
+
 @timer
 def restricted_command_middleware(
     context: BoltContext, db_session, user: DispatchUser, next: Callable, payload: dict
@@ -248,7 +257,7 @@ def command_context_middleware(context: BoltContext, payload: dict, next: Callab
         context.update(subject._asdict())
     else:
         raise ContextError(
-            f"Sorry, I can't determine the correct context to run the command `{payload['command']}`. Are you running this command in an incident channel?"
+            f"Sorry, we were unable to determine the correct context to run the command `{payload['command']}`. Are you running this command in an incident channel?"
         )
     next()
 
