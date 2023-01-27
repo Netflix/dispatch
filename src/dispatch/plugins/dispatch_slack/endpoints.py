8446292e079488729aca3005a68bb7d8bb87ab8f
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-01-27 08:11:57 -0800

    use HTTPStatus constant for status_code

--- src/dispatch/plugins/dispatch_slack/endpoints.py
+++ src/dispatch/plugins/dispatch_slack/endpoints.py
@@ -33,7 +33,7 @@ async def parse_request(request: Request):
     try:
         request = json.loads(request_body_form.get("payload"))
     except Exception:
-        raise HTTPException(status_code=400, detail=[{"msg": "Bad Request"}])
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=[{"msg": "Bad Request"}])
     return request
 
 
