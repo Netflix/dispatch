1d19fc59c16b7ab6f58fa390d35fea748b98c468
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-02-02 14:32:14 -0800

    add gh action CI job and make login util more robust for CI

--- tests/e2e/utils/login.ts
+++ tests/e2e/utils/login.ts
@@ -1,16 +1,21 @@
 import { Page } from "@playwright/test"
 
 async function login(page: Page): Promise<void> {
-  const user = ""
-  const password = ""
+  let email = (Math.random() + 1).toString(36).substring(7) + "@example.com"
+  let password = (Math.random() + 1).toString(36).substring(7)
+
   // baseURL is set in the config to http://localhost:8080/
   // This will navigate to http://localhost:8080/default/auth/login
   await page.goto("./default/auth/login")
+
+  await page.getByRole("link", { name: "Register" }).click()
+
   await page.getByLabel("Email").click()
-  await page.getByLabel("Email").fill(user)
+  await page.getByLabel("Email").fill(email)
+
   await page.getByLabel("Password").click()
   await page.getByLabel("Password").fill(password)
-  await page.getByRole("button", { name: "Login" }).click()
+  await page.getByRole("button", { name: "Register" }).click()
   // Browse to Dashboards page to make sure we logged in
   await page.getByRole("listitem").filter({ hasText: "Dashboards" }).click()
 }
