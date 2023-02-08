6083a376261a3aa807d90b3b55e3ff3654546d69
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-02-08 13:36:57 -0800

    Fix tests to actually test things

--- src/dispatch/static/dispatch/tests/participants-tab.spec.js
+++ src/dispatch/static/dispatch/tests/participants-tab.spec.js
@@ -2,9 +2,8 @@
  * @vitest-environment jsdom
  */
 
-import Vuex from "vuex"
 import { describe, expect, it, vi } from "vitest"
-import { createLocalVue, mount } from "@vue/test-utils"
+import { shallowMount } from "@vue/test-utils"
 import CaseParticipantsTab from "src/case/ParticipantsTab.vue"
 
 vi.mock("vuex-map-fields", () => ({
@@ -12,78 +11,72 @@ vi.mock("vuex-map-fields", () => ({
   mapFields: vi.fn(),
 }))
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
+const participants = [
+  {
+    id: 1,
+    individual: {
+      name: "John Doe",
+      weblink: "https://example.com/john-doe",
+    },
+    participant_roles: [
+      { role: "Participant", renounced_at: null },
+      { role: "Reporter", renounced_at: "2022-01-01T00:00:00.000Z" },
+    ],
+    team: "Team A",
+    location: "Location A",
+  },
+  {
+    id: 2,
+    individual: {
+      name: "Jane Doe",
+      weblink: "https://example.com/jane-doe",
+    },
+    participant_roles: [{ role: "Assignee", renounced_at: null }],
+    team: "Team B",
+    location: "Location B",
+  },
+]
+
+const computed = {
+  participants: () => participants,
+}
 
 describe("CaseParticipantsTab", () => {
-  let store
-  let state
-
-  beforeEach(() => {
-    state = {
-      case_management: {
-        selected: {
-          participants: [
-            {
-              id: 1,
-              individual: {
-                name: "John Doe",
-                weblink: "https://example.com/john-doe",
-              },
-              participant_roles: [
-                { role: "Participant", renounced_at: null },
-                { role: "Reporter", renounced_at: "2022-01-01T00:00:00.000Z" },
-              ],
-              team: "Team A",
-              location: "Location A",
-            },
-            {
-              id: 2,
-              individual: {
-                name: "Jane Doe",
-                weblink: "https://example.com/jane-doe",
-              },
-              participant_roles: [{ role: "Assignee", renounced_at: null }],
-              team: "Team B",
-              location: "Location B",
-            },
-          ],
-        },
-      },
-    }
-
-    store = new Vuex.Store({ state })
-  })
-
-  it("renders the correct number of participants", () => {
-    const wrapper = mount(CaseParticipantsTab, { store, localVue })
-    const participants = wrapper.vm.$store.state.case_management.selected.participants
-    expect(participants).to.have.lengthOf(2)
-  })
-
-  it("displays only active roles", () => {
-    const wrapper = mount(CaseParticipantsTab, { store, localVue })
+  it("assert if there's participants that we render them", () => {
+    const wrapper = shallowMount(CaseParticipantsTab, { computed })
     const participantListItems = wrapper.findAll("v-list-item-title").wrappers
+    expect(participantListItems).to.have.lengthOf(2)
+  })
+
+  it("displays active roles", () => {
+    const wrapper = shallowMount(CaseParticipantsTab, { computed })
+    const participantListItems = wrapper.findAll("v-list-item-title").wrappers
+
     participantListItems.forEach((participantListItem, index) => {
-      const participant = state.case_management.selected.participants[index]
+      const participant = participants[index]
       const activeRoles = participant.participant_roles
         .filter((role) => !role.renounced_at)
         .map((role) => role.role)
         .join(", ")
+
       expect(participantListItem.text()).to.include(activeRoles)
     })
   })
 
   it("does not display renounced roles", () => {
-    const wrapper = mount(CaseParticipantsTab, { store, localVue })
+    const wrapper = shallowMount(CaseParticipantsTab, { computed })
     const participantListItems = wrapper.findAll("v-list-item-title").wrappers
+
     participantListItems.forEach((participantListItem, index) => {
-      const participant = state.case_management.selected.participants[index]
+      const participant = participants[index]
       const renouncedRoles = participant.participant_roles
-        .filter((role) => typeof role.renounced_at === "string" && role.renounced_at.length)
+        .filter((role) => role.renounced_at)
         .map((role) => role.role)
         .join(", ")
-      expect(participantListItem.text()).to.not.include(renouncedRoles)
+
+      if (renouncedRoles) {
+        expect(participantListItem.text()).to.not.include(renouncedRoles)
+      }
     })
   })
 })
