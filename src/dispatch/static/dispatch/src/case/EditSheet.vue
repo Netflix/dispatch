342be5f184a86bbcf3af5812d66a474d1f175da4
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-02-13 13:25:51 -0800

    Make case edit sheet wider to give tabs some room to breathe

--- src/dispatch/static/dispatch/src/case/EditSheet.vue
+++ src/dispatch/static/dispatch/src/case/EditSheet.vue
@@ -1,6 +1,6 @@
 <template>
   <ValidationObserver v-slot="{ invalid, validated }">
-    <v-navigation-drawer app clipped right width="800">
+    <v-navigation-drawer app clipped right width="900">
       <template v-slot:prepend>
         <v-list-item two-line>
           <v-list-item-content>
