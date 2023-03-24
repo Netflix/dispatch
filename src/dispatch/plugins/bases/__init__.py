720a74a8bb54f376dd7ece2c7674c371934515c4
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2023-03-24 13:34:44 -0700

    Adds plugin for OpenAI's Generative Artificial Intelligence platform (#3159)
    * Adds plugin to setup.py
    
    * Adds plugin code
    
    * Improves config
    
    * ruff ruff ruff
    
    * Switches to OpenAI's official Python library
    
    * ruff ruff ruff
    
    * Returns a dict representation of the OpenAIObject

--- src/dispatch/plugins/bases/__init__.py
+++ src/dispatch/plugins/bases/__init__.py
@@ -1,3 +1,5 @@
+from .artificial_intelligence import ArtificialIntelligencePlugin  # noqa
+from .auth_mfa import MultiFactorAuthenticationPlugin  # noqa
 from .auth_provider import AuthenticationProviderPlugin  # noqa
 from .conference import ConferencePlugin  # noqa
 from .contact import ContactPlugin  # noqa
@@ -6,17 +8,16 @@ from .definition import DefinitionPlugin  # noqa
 from .document import DocumentPlugin  # noqa
 from .document_resolver import DocumentResolverPlugin  # noqa
 from .email import EmailPlugin  # noqa
-from .auth_mfa import MultiFactorAuthenticationPlugin  # noqa
+from .monitor import MonitorPlugin  # noqa
 from .oncall import OncallPlugin  # noqa
 from .participant import ParticipantPlugin  # noqa
 from .participant_group import ParticipantGroupPlugin  # noqa
+from .signal_consumer import SignalConsumerPlugin  # noqa
+from .signal_enrichment import SignalEnrichmentPlugin  # noqa
+from .source import SourcePlugin  # noqa
 from .storage import StoragePlugin  # noqa
 from .tag import TagPlugin  # noqa
 from .task import TaskPlugin  # noqa
 from .term import TermPlugin  # noqa
 from .ticket import TicketPlugin  # noqa
 from .workflow import WorkflowPlugin  # noqa
-from .monitor import MonitorPlugin  # noqa
-from .source import SourcePlugin  # noqa
-from .signal_consumer import SignalConsumerPlugin  # noqa
-from .signal_enrichment import SignalEnrichmentPlugin  # noqa
---
+++ src/dispatch/plugins/dispatch_openai/plugin.py
@@ -0,0 +1,53 @@
+"""
+.. module: dispatch.plugins.openai.plugin
+    :platform: Unix
+    :copyright: (c) 2019 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. moduleauthor:: Marc Vilanova <mvilanova@netflix.com>
+"""
+import logging
+
+import openai
+from openai import util
+
+from dispatch.decorators import apply, counter, timer
+from dispatch.plugins import dispatch_openai as openai_plugin
+from dispatch.plugins.bases import ArtificialIntelligencePlugin
+from dispatch.plugins.dispatch_openai.config import (
+    OpenAIConfiguration,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@apply(counter, exclude=["__init__"])
+@apply(timer, exclude=["__init__"])
+class OpenAIPlugin(ArtificialIntelligencePlugin):
+    title = "OpenAI Plugin - Generative Artificial Intelligence"
+    slug = "openai-artificial-intelligence"
+    description = "Uses OpenAI's platform to allow users to ask questions in natural language."
+    version = openai_plugin.__version__
+
+    author = "Netflix"
+    author_url = "https://github.com/netflix/dispatch.git"
+
+    def __init__(self):
+        self.configuration_schema = OpenAIConfiguration
+
+    def ask(self, prompt: str) -> dict:
+        openai.api_key = self.api_key
+
+        try:
+            response = openai.Completion.create(
+                max_tokens=self.max_tokens,
+                model=self.model,
+                n=self.n,
+                prompt=prompt,
+                stop=self.stop,
+                temperature=self.temperature,
+            )
+        except Exception as e:
+            logger.error(e)
+            raise
+
+        return util.convert_to_dict(response)
