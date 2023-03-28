abbca25a7d86c6b25c94e4aed7b6a42140a40018
Kevin Glisson <kevgliss@gmail.com>
2023-03-28 11:33:58 -0700

    Adding more signal tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -595,6 +595,11 @@ def plugin_instance(session):
 
 
 @pytest.fixture
+def conversation_plugin_instance(session, conversation_plugin):
+    return PluginInstanceFactory(plugin=PluginFactory(slug=conversation_plugin.slug))
+
+
+@pytest.fixture
 def workflow_plugin_instance(session, workflow_plugin):
     return PluginInstanceFactory(plugin=PluginFactory(slug=workflow_plugin.slug))
 
--- tests/signal/test_signal_flow.py
+++ tests/signal/test_signal_flow.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_create_signal_instance(session, signal, case_severity, case_priority, user):
     from dispatch.signal.flows import create_signal_instance
 
@@ -15,3 +18,42 @@ def test_create_signal_instance(session, signal, case_severity, case_priority, u
         signal_instance_data=instance_data,
         current_user=user,
     )
+
+
+def test_create_signal_instance_no_variant(session, signal, case_severity, case_priority, user):
+    from dispatch.signal.flows import create_signal_instance
+
+    case_priority.default = True
+    case_priority.project_id = signal.project_id
+
+    case_severity.default = True
+    case_severity.project_id = signal.project_id
+
+    instance_data = {"variant": "unknown"}
+    with pytest.raises(Exception):
+        create_signal_instance(
+            db_session=session,
+            project=signal.project,
+            signal_instance_data=instance_data,
+            current_user=user,
+        )
+
+
+def test_create_signal_instance_not_enabled(session, signal, case_severity, case_priority, user):
+    from dispatch.signal.flows import create_signal_instance
+
+    case_priority.default = True
+    case_priority.project_id = signal.project_id
+
+    case_severity.default = True
+    case_severity.project_id = signal.project_id
+
+    signal.enabled = False
+    instance_data = {"variant": signal.variant}
+    with pytest.raises(Exception):
+        create_signal_instance(
+            db_session=session,
+            project=signal.project,
+            signal_instance_data=instance_data,
+            current_user=user,
+        )
