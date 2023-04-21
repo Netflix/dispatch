e4e4d8f3397e0d0dc26df9149bbdb4ac8eff4614
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-04-21 12:01:13 -0700

    convert snooze filter mfa to use last_mfa_time and other cleanup

--- src/dispatch/plugins/dispatch_duo/plugin.py
+++ src/dispatch/plugins/dispatch_duo/plugin.py
@@ -77,9 +77,7 @@ class DuoMfaPlugin(MultiFactorAuthenticationPlugin):
         """
         duo_client = duo_service.create_duo_auth_client(self.configuration)
         try:
-            response = duo_client.auth(
-                factor="push", username=username.split("@"), device=device, type=type
-            )
+            response = duo_client.auth(factor="push", username=username, device=device, type=type)
         except RuntimeError as e:
             if "Invalid request parameters (username)" in str(e):
                 username, _ = username.split("@")
