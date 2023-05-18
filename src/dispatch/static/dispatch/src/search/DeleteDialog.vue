313398e3e3d2c4a1301685d337c05ea948f6232c
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2023-05-17 18:15:29 -0700

    Adds a new project settings section for managing search filters (#3398)
    * Adds a new project section for managing search filters
    
    * Adds column for notification in UI table
    
    * Checks for SensitiveProjectActionPermission on filter delete
    
    * Adds TODO

---
+++ src/dispatch/static/dispatch/src/search/DeleteDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog v-model="showRemove" persistent max-width="800px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Delete Search Filter?</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap> Are you sure you would like to delete this search filter? </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="blue en-1" text @click="closeRemove()"> Cancel </v-btn>
+        <v-btn color="red en-1" text @click="remove()"> Delete </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions } from "vuex"
+import { mapFields } from "vuex-map-fields"
+
+export default {
+  name: "SearchFilterDeleteDialog",
+
+  data() {
+    return {}
+  },
+
+  computed: {
+    ...mapFields("search", ["dialogs.showRemove"]),
+  },
+
+  methods: {
+    ...mapActions("search", ["remove", "closeRemove"]),
+  },
+}
+</script>
