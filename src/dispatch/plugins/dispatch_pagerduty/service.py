665318f20744d669bbf038f7c8d6d7b0ee4ac063
Avery Lee <averyl@netflix.com>
2023-07-31 11:49:40 -0700

    Updates logic for checking the end of the oncall shift.

--- src/dispatch/plugins/dispatch_pagerduty/service.py
+++ src/dispatch/plugins/dispatch_pagerduty/service.py
@@ -54,20 +54,30 @@ def get_oncall_schedule(client, service: dict, param={}) -> list[dict]:
             f"No schedule could be found for this pagerduty escalation policy ({escalation_policy_id})."
         )
 
-    # Grabs the oncall schedule from the last 24 hour by default
+    # Grabs the oncall schedule from the last 14 hours by default. Assumes the oncall shifts are at least
+    # 12 hours long. The offset accounts for shift changes at the cutoff time.
     if not param:
-        param["until"] = datetime.now()
-        param["since"] = param["until"] - timedelta(days=1)
+        now = datetime.now()
+        offset = 1
+        param["until"] = now + timedelta(hours=offset)
+        param["since"] = now - timedelta(hours=12 + offset)
 
     param["include[]"] = "schedule_layers"
     return client.rget(f"/schedules/{schedule_id}", param=param)
 
 
-def send_shift_feedback_form_message(client, service_id: str):
+def send_shift_feedback_form_message(client, service_id: str, param={}):
+    """Sends the oncall shift feedback form to the oncalls at the end of their shift.
+    Args:
+        service_id: ID of the PagerDuty service resource
+        param: A dictionary containing the query parameters to be sent to the PagerDuty API.
+               See https://developer.pagerduty.com/api-reference/165ad96a22ffd-get-a-service#Query-Parameters for more information.
+    """
     service = client.rget(f"/services/{service_id}")
-    schedule = get_oncall_schedule(client, service)
+    schedule = get_oncall_schedule(client, service, param)
 
-    # TODO: How do we identify if an override has ended their shift? This could potentially go on indefinitely.
+    # TODO: Identify if an override has ended their shift. The override layer does not identify which
+    # schedule layer it is overriding. This layer could potentially go on indefinitely.
     if schedule["overrides_subschedule"]:
         unique_users = list(
             set(
@@ -84,9 +94,10 @@ def send_shift_feedback_form_message(client, service_id: str):
         if layer["rendered_coverage_percentage"] == 0:
             continue
 
-        # Sends the feedback form to all users who have completed their oncall shifts.
-        unique_users = list(set([entry.user.id for entry in layer["rendered_schedule_entries"]]))
-        for user in unique_users[:-1]:
+        # Checks if the user is still oncall.
+        user_start = layer["rendered_schedule_entries"][0]["user"]["id"]
+        user_end = layer["rendered_schedule_entries"][-1]["user"]["id"]
+        if user_start != user_end:
             # TODO: send_oncall_shift_feedback_message
             pass
 
