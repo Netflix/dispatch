fddf2edbf108cd6d1154e6e7b02e89aa29ca00c2
Kael <kaelwd@gmail.com>
2023-09-09 20:21:01 +1000

    disable explicit emits

--- src/dispatch/static/dispatch/.eslintrc.js
+++ src/dispatch/static/dispatch/.eslintrc.js
@@ -51,5 +51,6 @@ module.exports = {
     "vue/multi-word-component-names": "off",
     "vue/attribute-hyphenation": "off",
     "vue/require-default-prop": "off",
+    "vue/require-explicit-emits": "off",
   },
 }
