01ebc7fd018f0f4a3803961f801f1dd333cb41a4
Kael <kaelwd@gmail.com>
2023-09-09 20:59:10 +1000

    bind drilldown drawer attrs

--- src/dispatch/static/dispatch/src/dashboard/incident/IncidentOverview.vue
+++ src/dispatch/static/dispatch/src/dashboard/incident/IncidentOverview.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid grid-list-xl>
-    <incidents-drill-down-sheet v-if="showDrillDown" :items="detailItems" @close="closeDrillDown" />
+    <incidents-drill-down-sheet v-model="showDrillDown" :items="detailItems" />
     <v-layout row wrap>
       <v-flex class="d-flex justify-start" lg6 sm6 xs12>
         <v-btn color="info" @click="copyView"> Share View </v-btn>
@@ -210,9 +210,6 @@ export default {
           )
         }
       )
-    },
-    closeDrillDown() {
-      this.showDrillDown = false
     },
   },
 
