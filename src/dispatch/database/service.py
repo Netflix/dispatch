2afff1cfc0d77ed9cf3b7cb54023be8ce29b5099
David Whittaker <dwhittaker@netflix.com>
2023-09-28 16:09:59 -0700

    Only intersect for incidents

--- src/dispatch/database/service.py
+++ src/dispatch/database/service.py
@@ -505,13 +505,13 @@ def search_filter_sort_paginate(
             query = search(query_str=query_str, query=query, model=model, sort=sort)
 
         query_restricted = apply_model_specific_filters(model_cls, query, current_user, role)
-        query = query.distinct()
 
         if filter_spec:
             query = apply_filter_specific_joins(model_cls, filter_spec, query)
             query = apply_filters(query, filter_spec, model_cls)
 
-        query = query.intersect(query_restricted)
+        if model == "Incident":
+            query = query.intersect(query_restricted)
 
         if sort_by:
             sort_spec = create_sort_spec(model, sort_by, descending)
