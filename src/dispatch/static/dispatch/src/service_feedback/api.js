7bdfdc1795103edd3665e889d6ca6f6c445f862a
David Whittaker <dwhittaker@netflix.com>
2023-09-28 10:53:30 -0700

    Cleaning up api to only getAll

--- src/dispatch/static/dispatch/src/service_feedback/api.js
+++ src/dispatch/static/dispatch/src/service_feedback/api.js
@@ -6,20 +6,4 @@ export default {
   getAll(options) {
     return API.get(`${resource}`, { params: { ...options } })
   },
-
-  get(feedbackId) {
-    return API.get(`${resource}/${feedbackId}`)
-  },
-
-  create(payload) {
-    return API.post(`${resource}`, payload)
-  },
-
-  update(feedbackId, payload) {
-    return API.put(`${resource}/${feedbackId}`, payload)
-  },
-
-  delete(feedbackId) {
-    return API.delete(`${resource}/${feedbackId}`)
-  },
 }
