930cd4aa92b07d3750db395930e8123a10335de0
Kael <kaelwd@gmail.com>
2023-10-09 22:01:13 +1100

    replace vuedraggable with vueuse sortable

--- src/dispatch/static/dispatch/src/incident_role/PolicyRoleBuilder.vue
+++ src/dispatch/static/dispatch/src/incident_role/PolicyRoleBuilder.vue
@@ -5,8 +5,8 @@
         <v-col>
           {{ label }}
           <v-tooltip location="bottom">
-            <template #activator="{ props }">
-              <v-btn icon variant="text" color="info" v-bind="props" @click="add()">
+            <template #activator="{ props: activatorProps }">
+              <v-btn icon variant="text" color="info" v-bind="activatorProps" @click="add()">
                 <v-icon>mdi-playlist-plus</v-icon>
               </v-btn>
             </template>
@@ -15,12 +15,12 @@
         </v-col>
         <v-col cols="1" align="end">
           <v-tooltip location="bottom">
-            <template #activator="{ props }">
+            <template #activator="{ props: activatorProps }">
               <v-btn
                 icon
                 variant="text"
                 color="info"
-                v-bind="props"
+                v-bind="activatorProps"
                 :loading="loading"
                 @click="save()"
               >
@@ -32,75 +32,70 @@
         </v-col>
       </v-row>
     </v-card-title>
-    <v-card-text v-if="policies.length">
-      <v-expansion-panels>
-        <draggable
-          class="grow"
-          v-model="policies"
-          @update="onUpdate"
-          @start="drag = true"
-          @end="drag = false"
-        >
-          <v-expansion-panel v-for="(policy, idx) in policies" :key="policy.id">
-            <v-expansion-panel-title>
-              <v-row align="center" justify="center">
-                <v-col cols="1">
-                  <v-icon>mdi-drag-horizontal-variant</v-icon>
-                </v-col>
-                <v-col cols="1">
-                  <v-chip v-if="policy.enabled" color="green" size="small" label> Enabled </v-chip>
-                  <v-chip v-if="!policy.enabled" size="small" label>Disabled</v-chip>
-                </v-col>
-                <v-col>
-                  {{ policy.role }} - <span v-if="policy.service">{{ policy.service.name }}</span>
-                </v-col>
-              </v-row>
-            </v-expansion-panel-title>
-            <v-expansion-panel-text>
-              <v-list density="compact">
-                <v-list-item>
-                  <tag-filter-auto-complete label="Tags" :project="project" v-model="policy.tags" />
-                </v-list-item>
-                <v-list-item>
-                  <incident-priority-combobox
-                    :project="project"
-                    v-model="policy.incident_priorities"
-                  />
-                </v-list-item>
-                <v-list-item>
-                  <incident-type-combobox :project="project" v-model="policy.incident_types" />
-                </v-list-item>
-                <v-list-item>
-                  <service-select-new
-                    label="Target Service"
-                    :project="project"
-                    v-model="policy.service"
-                  />
-                </v-list-item>
-                <v-list-item>
-                  <v-checkbox
-                    v-model="policy.enabled"
-                    label="Enabled"
-                    hint="Check this if you would like this policy to be considered when resolving the role."
-                  />
-                </v-list-item>
-                <v-list-item>
-                  <v-btn color="primary" @click="remove(idx)"> Delete Policy </v-btn>
-                </v-list-item>
-              </v-list>
-            </v-expansion-panel-text>
-          </v-expansion-panel>
-        </draggable>
+    <v-card-text>
+      <template v-if="!policies.length">
+        <v-row justify="center"> No {{ label }} policies have been defined. </v-row>
+      </template>
+
+      <v-expansion-panels ref="sortableElement">
+        <v-expansion-panel v-for="(policy, idx) in policies" :key="policy.id">
+          <v-expansion-panel-title>
+            <v-row align="center" justify="center">
+              <v-col cols="1">
+                <v-icon>mdi-drag-horizontal-variant</v-icon>
+              </v-col>
+              <v-col cols="1">
+                <v-chip v-if="policy.enabled" color="green" size="small" label> Enabled </v-chip>
+                <v-chip v-if="!policy.enabled" size="small" label>Disabled</v-chip>
+              </v-col>
+              <v-col>
+                {{ policy.role }} - <span v-if="policy.service">{{ policy.service.name }}</span>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <v-list density="compact">
+              <v-list-item>
+                <tag-filter-auto-complete label="Tags" :project="project" v-model="policy.tags" />
+              </v-list-item>
+              <v-list-item>
+                <incident-priority-combobox
+                  :project="project"
+                  v-model="policy.incident_priorities"
+                />
+              </v-list-item>
+              <v-list-item>
+                <incident-type-combobox :project="project" v-model="policy.incident_types" />
+              </v-list-item>
+              <v-list-item>
+                <service-select-new
+                  label="Target Service"
+                  :project="project"
+                  v-model="policy.service"
+                />
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox
+                  v-model="policy.enabled"
+                  label="Enabled"
+                  hint="Check this if you would like this policy to be considered when resolving the role."
+                />
+              </v-list-item>
+              <v-list-item>
+                <v-btn color="primary" @click="remove(idx)"> Delete Policy </v-btn>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
       </v-expansion-panels>
-    </v-card-text>
-    <v-card-text v-else>
-      <v-row justify="center"> No {{ label }} policies have been defined. </v-row>
+      <pre>{{ policies }}</pre>
     </v-card-text>
   </v-card>
 </template>
 
-<script>
-import draggable from "vuedraggable"
+<script setup>
+import { ref, computed, watch } from "vue"
+import { useSortable } from "@vueuse/integrations/useSortable"
 
 import IncidentRoleApi from "@/incident_role/api"
 
@@ -109,92 +104,75 @@ import IncidentTypeCombobox from "@/incident/type/IncidentTypeCombobox.vue"
 import ServiceSelectNew from "@/service/ServiceSelectNew.vue"
 import TagFilterAutoComplete from "@/tag/TagFilterAutoComplete.vue"
 
-export default {
-  name: "RolePolicyBuilder",
-
-  props: {
-    value: {
-      type: Array,
-      default: function () {
-        return []
-      },
-    },
-    label: {
-      type: String,
-      default: null,
-    },
-    project: {
-      type: [Object],
-      default: null,
-    },
+const props = defineProps({
+  label: {
+    type: String,
+    default: null,
   },
-
-  components: {
-    draggable,
-    IncidentTypeCombobox,
-    IncidentPriorityCombobox,
-    TagFilterAutoComplete,
-    ServiceSelectNew,
+  project: {
+    type: [Object],
+    default: null,
   },
+})
 
-  data() {
-    return {
-      policies: [],
-      loading: false,
-    }
-  },
+const loading = ref(false)
+const sortableElement = ref()
 
-  methods: {
-    add: function () {
-      this.policies.push({
-        role: this.label,
-        project: this.project,
-        enabled: false,
-        service: null,
-        incident_priorities: [],
-        incident_types: [],
-        tags: [],
-      })
-    },
-    remove: function (idx) {
-      this.policies.splice(idx, 1)
-    },
-    save: function () {
-      this.loading = true
-      IncidentRoleApi.updateRole(this.label, this.project.name, { policies: this.policies })
-        .then((response) => {
-          this.$store.commit(
-            "notification_backend/addBeNotification",
-            { text: "Role policies successfully updated.", type: "success" },
-            { root: true }
-          )
-          this.policies = response.data.policies
-        })
-        .finally(() => {
-          this.loading = false
-        })
-    },
-    get: function () {
-      IncidentRoleApi.getRolePolicies(this.label, this.project.name).then((response) => {
-        this.policies = response.data.policies
-      })
-    },
-    onUpdate() {
-      // update the order attr
-      this.policies.forEach((policy, idx) => {
-        policy.order = idx + 1
-      })
-    },
+const _policies = ref([])
+const policies = computed({
+  get() {
+    return _policies.value
   },
+  set(val) {
+    _policies.value = val.map((policy, idx) => ({
+      ...policy,
+      order: idx + 1,
+    }))
+  },
+})
 
-  created() {
-    this.get()
-    this.$watch(
-      (vm) => [vm.project],
-      () => {
-        this.get()
-      }
-    )
+useSortable(
+  computed(() => sortableElement.value?.$el),
+  policies
+)
+
+watch(
+  () => props.project,
+  () => {
+    IncidentRoleApi.getRolePolicies(props.label, props.project.name).then((response) => {
+      policies.value = response.data.policies
+    })
   },
+  { immediate: true }
+)
+
+function add() {
+  policies.value.push({
+    role: props.label,
+    project: props.project,
+    enabled: false,
+    service: null,
+    incident_priorities: [],
+    incident_types: [],
+    tags: [],
+  })
+}
+function remove(idx) {
+  policies.value.splice(idx, 1)
+}
+function save() {
+  loading.value = true
+  IncidentRoleApi.updateRole(props.label, props.project.name, { policies: policies.value })
+    .then((response) => {
+      this.$store.commit(
+        "notification_backend/addBeNotification",
+        { text: "Role policies successfully updated.", type: "success" },
+        { root: true }
+      )
+      policies.value = response.data.policies
+    })
+    .finally(() => {
+      loading.value = false
+    })
 }
 </script>
