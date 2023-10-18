2889c3a307e54ef84ef98f5e663763d5a35ce4c3
Kael <kaelwd@gmail.com>
2023-10-18 20:19:03 +1100

    fix timeline layout

--- src/dispatch/static/dispatch/src/case/TimelineTab.vue
+++ src/dispatch/static/dispatch/src/case/TimelineTab.vue
@@ -11,45 +11,49 @@
         Export
       </v-btn>
     </v-row>
-    <v-timeline v-if="events && events.length" density="compact" clipped>
-      <v-col class="text-right text-caption">(times in UTC)</v-col>
-      <v-timeline-item
-        v-for="event in sortedEvents"
-        :key="event.id"
-        class="mb-4"
-        dot-color="blue"
-        size="small"
-      >
-        <v-row justify="space-between">
-          <v-col cols="7">
-            {{ event.description }}
-            <transition-group name="slide" v-if="showDetails">
-              <template v-for="(value, key) in event.details" :key="key">
-                <v-card flat>
-                  <v-card-title class="text-subtitle-1">
-                    {{ snakeToCamel(key) }}
-                  </v-card-title>
-                  <v-card-text>{{ value }}</v-card-text>
-                </v-card>
-              </template>
-            </transition-group>
-            <div class="text-caption">
-              {{ event.source }}
-            </div>
-          </v-col>
-          <v-col class="text-right" cols="5">
-            <v-tooltip location="bottom">
-              <template #activator="{ props }">
-                <span v-bind="props" class="wavy-underline">{{
-                  formatToUTC(event.started_at)
-                }}</span>
-              </template>
-              <span class="pre-formatted">{{ formatToTimeZones(event.started_at) }}</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-timeline-item>
-    </v-timeline>
+    <template v-if="events && events.length">
+      <v-timeline density="compact" clipped>
+        <v-timeline-item hide-dot>
+          <v-col class="text-right text-caption">(times in UTC)</v-col>
+        </v-timeline-item>
+        <v-timeline-item
+          v-for="event in sortedEvents"
+          :key="event.id"
+          class="mb-4"
+          dot-color="blue"
+          size="small"
+        >
+          <v-row justify="space-between">
+            <v-col cols="7">
+              {{ event.description }}
+              <transition-group name="slide" v-if="showDetails">
+                <template v-for="(value, key) in event.details" :key="key">
+                  <v-card flat>
+                    <v-card-title class="text-subtitle-1">
+                      {{ snakeToCamel(key) }}
+                    </v-card-title>
+                    <v-card-text>{{ value }}</v-card-text>
+                  </v-card>
+                </template>
+              </transition-group>
+              <div class="text-caption">
+                {{ event.source }}
+              </div>
+            </v-col>
+            <v-col class="text-right" cols="5">
+              <v-tooltip location="bottom">
+                <template #activator="{ props }">
+                  <span v-bind="props" class="wavy-underline">{{
+                    formatToUTC(event.started_at)
+                  }}</span>
+                </template>
+                <span class="pre-formatted">{{ formatToTimeZones(event.started_at) }}</span>
+              </v-tooltip>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
+    </template>
     <div v-else>
       <p class="text-center">No timeline data available.</p>
     </div>
