1ccc6918f7111432dfc625f9857b40f712e43f2a
Marc Vilanova <39573146+mvilanova@users.noreply.github.com>
2023-10-23 12:56:19 -0700

    Enhances Signals table (#3898)
    * Enhances Signals table
    
    * removes commented code

--- src/dispatch/database/service.py
+++ src/dispatch/database/service.py
@@ -390,7 +390,7 @@ def composite_search(*, db_session, query_str: str, models: List[Base], current_
 
     # TODO can we do this with composite filtering?
     # for model in models:
-    # 	 query = apply_model_specific_filters(model, query, current_user)
+    #    query = apply_model_specific_filters(model, query, current_user)
 
     return s.search(query=query)
 
@@ -497,6 +497,7 @@ def search_filter_sort_paginate(
 ):
     """Common functionality for searching, filtering, sorting, and pagination."""
     model_cls = get_class_by_tablename(model)
+
     try:
         query = db_session.query(model_cls)
 
