92fc04f3ac8d0b53a3c55a71d761324c8a0ec634
David Whittaker <dwhittaker@netflix.com>
2023-11-01 11:18:48 -0700

    Adding padding for spacing on participants and resources tabs

--- src/dispatch/static/dispatch/src/incident/ResourcesTab.vue
+++ src/dispatch/static/dispatch/src/incident/ResourcesTab.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list>
-    <v-list-item v-if="ticket" :href="ticket.weblink" target="_blank">
+    <v-list-item v-if="ticket" :href="ticket.weblink" target="_blank" class="my-3">
       <v-list-item-title>Ticket</v-list-item-title>
       <v-list-item-subtitle>{{ ticket.description }}</v-list-item-subtitle>
 
@@ -9,7 +9,7 @@
       </template>
     </v-list-item>
     <v-divider />
-    <v-list-item v-if="conference" :href="conference.weblink" target="_blank">
+    <v-list-item v-if="conference" :href="conference.weblink" target="_blank" class="my-3">
       <v-list-item-title>Video Conference</v-list-item-title>
       <v-list-item-subtitle>{{ conference.description }}</v-list-item-subtitle>
 
@@ -18,7 +18,7 @@
       </template>
     </v-list-item>
     <v-divider />
-    <v-list-item v-if="conversation" :href="conversation.weblink" target="_blank">
+    <v-list-item v-if="conversation" :href="conversation.weblink" target="_blank" class="my-3">
       <v-list-item-title>Conversation</v-list-item-title>
       <v-list-item-subtitle>{{ conversation.description }}</v-list-item-subtitle>
 
@@ -27,7 +27,7 @@
       </template>
     </v-list-item>
     <v-divider />
-    <v-list-item v-if="storage" :href="storage.weblink" target="_blank">
+    <v-list-item v-if="storage" :href="storage.weblink" target="_blank" class="my-3">
       <v-list-item-title>Storage</v-list-item-title>
       <v-list-item-subtitle>{{ storage.description }}</v-list-item-subtitle>
 
@@ -37,7 +37,7 @@
     </v-list-item>
     <v-divider />
     <span v-for="document in documents" :key="document.resource_id">
-      <v-list-item :href="document.weblink" target="_blank">
+      <v-list-item :href="document.weblink" target="_blank" class="my-3">
         <v-list-item-title>{{ deslug(document.resource_type) }}</v-list-item-title>
         <v-list-item-subtitle>{{ document.description }}</v-list-item-subtitle>
 
@@ -56,7 +56,7 @@
         (!documents && documentPluginEnabled)
       "
     >
-      <v-list-item v-if="!loading" @click="createAllResources()">
+      <v-list-item v-if="!loading" @click="createAllResources()" class="my-3">
         <v-list-item-title>Recreate Missing Resources</v-list-item-title>
         <v-list-item-subtitle
           >Initiate a retry for creating any missing or unsuccesfully created
@@ -66,7 +66,7 @@
           <v-icon>refresh</v-icon>
         </template>
       </v-list-item>
-      <v-list-item v-else-if="loading">
+      <v-list-item v-else-if="loading" class="my-3">
         <v-list-item-title>Creating resources...</v-list-item-title>
         <v-list-item-subtitle
           >Initiate a retry for creating any missing or unsuccesfully created
