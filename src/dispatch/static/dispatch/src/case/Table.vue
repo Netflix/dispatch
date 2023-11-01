8b77d648403f6bb9059588c6aad1af3e767b00cc
Kevin Glisson <kevgliss@gmail.com>
2023-11-01 11:33:59 -0700

    Reducing font size

--- src/dispatch/static/dispatch/src/case/Table.vue
+++ src/dispatch/static/dispatch/src/case/Table.vue
@@ -45,6 +45,7 @@
             v-model="selected"
             loading-text="Loading... Please wait"
             show-select
+            density="compact"
             @click:row="showCaseEditSheet"
           >
             <template #item.case_severity.name="{ value }">
