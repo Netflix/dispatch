f22694072f4f9014fa3f538e17aad06eda36c1ab
David Whittaker <84562015+whitdog47@users.noreply.github.com>
2023-11-01 13:13:26 -0700

    Fixing styling for incident priority settings, search filter settings, and plugins table (#3918)

--- src/dispatch/static/dispatch/src/search/EditDialog.vue
+++ src/dispatch/static/dispatch/src/search/EditDialog.vue
@@ -11,79 +11,77 @@
           <v-list-item-title class="text-h6"> Edit </v-list-item-title>
           <v-list-item-subtitle>Search Filter</v-list-item-subtitle>
 
-          <v-btn
-            icon
-            variant="text"
-            color="info"
-            :loading="loading"
-            :disabled="!isValid.value"
-            @click="save()"
-          >
-            <v-icon>mdi-content-save</v-icon>
-          </v-btn>
-          <v-btn icon variant="text" color="secondary" @click="closeCreateEdit">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
+          <template #append>
+            <v-btn
+              icon
+              variant="text"
+              color="info"
+              :loading="loading"
+              :disabled="!isValid.value"
+              @click="save()"
+            >
+              <v-icon>mdi-content-save</v-icon>
+            </v-btn>
+            <v-btn icon variant="text" color="secondary" @click="closeCreateEdit">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </template>
         </v-list-item>
-        <v-card>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="name"
-                    label="Name"
-                    name="Name"
-                    hint="Name of type."
-                    clearable
-                    required
-                    :rules="[rules.required]"
-                  />
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea
-                    v-model="description"
-                    label="Description"
-                    name="Description"
-                    hint="Description of type."
-                    clearable
-                    required
-                    :rules="[rules.required]"
-                  />
-                </v-col>
-              </v-row>
-              <div class="text-body-2 ml-1 mt-2">Individuals</div>
-              <v-divider class="mt-2" />
-              <v-list style="max-height: 500px" class="overflow-y-auto">
-                <v-list-item v-for="individual in individuals" :key="individual.id">
-                  <incident-participant :participant="convertToParticipant(individual)" />
-                </v-list-item>
-              </v-list>
-              <div class="text-body-2 ml-1 mt-2">Teams</div>
-              <v-divider class="mt-2" />
-              <v-list style="max-height: 500px" class="overflow-y-auto">
-                <v-list-item v-for="team in teams" :key="team.id">
-                  <v-list-item-title>{{ team.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-              <div class="text-body-2 ml-1 mt-2">Services</div>
-              <v-divider class="mt-2" />
-              <v-list style="max-height: 500px" class="overflow-y-auto">
-                <v-list-item v-for="service in services" :key="service.id">
-                  <v-list-item-title>{{ service.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-              <div class="text-body-2 ml-1 mt-2">Notifications</div>
-              <v-divider class="mt-2" />
-              <v-list style="max-height: 500px" class="overflow-y-auto">
-                <v-list-item v-for="notification in notifications" :key="notification.id">
-                  <v-list-item-title>{{ notification.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-container>
-          </v-card-text>
-        </v-card>
       </template>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="name"
+              label="Name"
+              name="Name"
+              hint="Name of type."
+              clearable
+              required
+              :rules="[rules.required]"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-textarea
+              v-model="description"
+              label="Description"
+              name="Description"
+              hint="Description of type."
+              clearable
+              required
+              :rules="[rules.required]"
+            />
+          </v-col>
+        </v-row>
+        <div class="text-body-2 ml-1 mt-2">Individuals</div>
+        <v-divider class="mt-2" />
+        <v-list style="max-height: 500px">
+          <v-list-item v-for="individual in individuals" :key="individual.id">
+            <incident-participant :participant="convertToParticipant(individual)" />
+          </v-list-item>
+        </v-list>
+        <div class="text-body-2 ml-1 mt-2">Teams</div>
+        <v-divider class="mt-2" />
+        <v-list style="max-height: 500px">
+          <v-list-item v-for="team in teams" :key="team.id">
+            <v-list-item-title>{{ team.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <div class="text-body-2 ml-1 mt-2">Services</div>
+        <v-divider class="mt-2" />
+        <v-list style="max-height: 500px">
+          <v-list-item v-for="service in services" :key="service.id">
+            <v-list-item-title>{{ service.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <div class="text-body-2 ml-1 mt-2">Notifications</div>
+        <v-divider class="mt-2" />
+        <v-list style="max-height: 500px">
+          <v-list-item v-for="notification in notifications" :key="notification.id">
+            <v-list-item-title>{{ notification.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-container>
     </v-navigation-drawer>
   </v-form>
 </template>
