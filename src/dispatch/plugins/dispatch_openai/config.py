c7caa061687a8ec789c40183eb5c8d954b543794
Marc Vilanova <mvilanova@netflix.com>
2023-11-13 17:58:03 -0800

    Updates OpenAI plugin

--- src/dispatch/plugins/dispatch_openai/config.py
+++ src/dispatch/plugins/dispatch_openai/config.py
@@ -1,31 +1,19 @@
 from pydantic import Field, SecretStr
-from typing import List, TypeVar
 
 from dispatch.config import BaseConfigurationModel
-
-Stop = TypeVar("Stop", str, List)
 
 
 class OpenAIConfiguration(BaseConfigurationModel):
     """OpenAI configuration description."""
 
     api_key: SecretStr = Field(title="API Key", description="Your secret OpenAI API key.")
-    model: str = Field("text-davinci-003", title="Model", description="")
-    max_tokens: int = Field(
-        50,
-        title="Max Tokens",
-        description="The maximum number of tokens to generate in the completion.",
+    model: str = Field(
+        "gpt-3.5-turbo",
+        title="Model",
+        description="Available models can be found at https://platform.openai.com/docs/models",
     )
-    temperature: float = Field(
-        1, title="Temperature", description="What sampling temperature to use, between 0 and 2."
-    )
-    n: int = Field(
-        1,
-        title="Number of completions (n)",
-        description="How many completions to generate for each prompt.",
-    )
-    stop: Stop = Field(
-        None,
-        title="Stop",
-        description="Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.",
+    system_message: str = Field(
+        "You are a helpful assistant.",
+        title="System Message",
+        description="The system message to help set the behavior of the assistant.",
     )
