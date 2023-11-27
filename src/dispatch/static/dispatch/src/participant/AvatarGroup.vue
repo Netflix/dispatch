d63c95c71896fbb799f8346e21f555ac90b1af0d
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-11-27 13:56:42 -0800

    Add pariticpant folder with participant AvatarGroup component

---
+++ src/dispatch/static/dispatch/src/participant/AvatarGroup.vue
@@ -0,0 +1,213 @@
+<script setup>
+import { ref, computed, watch } from "vue"
+import Hotkey from "@/atomics/Hotkey.vue"
+import { useHotKey } from "@/composables/useHotkey"
+
+const props = defineProps({
+  participants: {
+    type: Array,
+    required: true,
+  },
+  highlightedParticipants: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const menu = ref(false)
+let orderedParticipants = ref([])
+const hoveredParticipant = ref("")
+const searchQuery = ref("")
+
+useHotKey(["Meta", "Shift", "p"], () => {
+  if (!menu.value) {
+    toggleMenu()
+  }
+})
+
+watch(
+  () => props.participants,
+  (newVal) => {
+    if (newVal) {
+      console.log("Watched new val", newVal)
+      const assignee = newVal.find((participant) =>
+        props.highlightedParticipants.includes(participant.individual.name)
+      )
+      const otherParticipants = newVal.filter(
+        (participant) => !props.highlightedParticipants.includes(participant.individual.name)
+      )
+      if (assignee) {
+        orderedParticipants.value = [assignee, ...otherParticipants]
+      } else {
+        orderedParticipants.value = newVal
+      }
+    }
+  },
+  { immediate: true }
+)
+
+const visibleParticipants = computed(() => orderedParticipants.value.slice(0, 3))
+const hiddenParticipants = computed(() => orderedParticipants.value.slice(1))
+
+const filteredParticipants = computed(() => {
+  const lowerCaseQuery = searchQuery.value.toLowerCase()
+
+  // If searching, only include participants that match the query
+  if (lowerCaseQuery) {
+    return orderedParticipants.value.filter((p) =>
+      p.individual.name.toLowerCase().includes(lowerCaseQuery)
+    )
+  }
+
+  // If not searching, include all participants
+  return orderedParticipants.value
+})
+
+const getAvatarGradient = (participant) => {
+  let hash = 5381
+  for (let i = 0; i < participant.length; i++) {
+    hash = ((hash << 5) + hash) ^ participant.charCodeAt(i) // Using XOR operator for better distribution
+  }
+
+  const hue = Math.abs(hash) % 360 // Ensure hue is a positive number
+  const fromColor = `hsl(${hue}, 95%, 50%)`
+  const toColor = `hsl(${(hue + 120) % 360}, 95%, 50%)` // Getting triadic color by adding 120 to hue
+
+  return `linear-gradient(${fromColor}, ${toColor})`
+}
+
+const toggleMenu = () => {
+  menu.value = !menu.value
+  if (!menu.value) {
+    hoveredParticipant.value = ""
+  }
+}
+</script>
+
+<template>
+  <div>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      location="start"
+      offset="10"
+      transition="false"
+    >
+      <template v-slot:activator="{ props: menu }">
+        <v-btn variant="text" v-bind="menu">
+          <!-- Display Visible Participants -->
+          <div class="avatar-row">
+            <div
+              v-for="(participant, index) in visibleParticipants"
+              :key="index"
+              class="avatar-container"
+            >
+              <v-avatar
+                size="20px"
+                :style="{ background: getAvatarGradient(participant.individual.name) }"
+                v-on="on"
+              >
+              </v-avatar>
+            </div>
+          </div>
+        </v-btn>
+      </template>
+      <v-card min-width="255" class="rounded-lg dispatch-side-card">
+        <v-row no-gutters>
+          <v-col align-self="start">
+            <v-text-field
+              v-model="searchQuery"
+              label="Search participant..."
+              density="compact"
+              variant="solo"
+              single-line
+              hide-details
+              flat
+            >
+              <template v-slot:label>
+                <span class="text-subtitle-2 font-weight-regular"> Change participants... </span>
+              </template>
+            </v-text-field>
+          </v-col>
+          <v-col align-self="end" cols="4" class="pb-2">
+            <Hotkey hotkey="⌘" style="margin-right: 3px" />
+            <Hotkey hotkey="⇧" style="margin-right: 3px" />
+            <Hotkey hotkey="P" />
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-list lines="one">
+          <v-list-item
+            v-for="(participant, index) in filteredParticipants"
+            :key="index"
+            @click="selectParticipant(participant)"
+            @mouseover="hoveredParticipant = participant"
+            @mouseleave="hoveredParticipant = ''"
+            density="compact"
+            rounded="lg"
+            active-class="ma-4"
+          >
+            <template v-slot:prepend>
+              <v-avatar
+                class="mr-n2"
+                size="12px"
+                :style="{ background: getAvatarGradient(participant.individual.name) }"
+              ></v-avatar>
+              <!-- <v-icon class="mr-n6 ml-n2" size="x-small" icon="mdi-account"></v-icon> -->
+            </template>
+            <v-list-item-title class="item-title-font">{{
+              participant.individual.name
+            }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-menu>
+  </div>
+</template>
+
+<style scoped>
+.avatar-row {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: row-reverse;
+  position: relative;
+}
+
+.avatar-container {
+  border: 2px solid white; /* Add a border around the avatar */
+  border-radius: 50%; /* Make the border circular */
+  position: relative;
+  margin-right: -5px; /* Adjust this value to change the overlapping amount */
+}
+
+.hotkey {
+  vertical-align: baseline;
+  text-transform: capitalize;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.816);
+  font-size: 11px;
+  line-height: 110%;
+  border-radius: 4px;
+  padding-left: 4px;
+  padding-right: 4px;
+  padding-top: 1px;
+  min-width: 17px;
+  border: 0.5px solid rgb(216, 216, 216);
+  background-color: rgb(254, 255, 254);
+  box-shadow: rgba(0, 0, 0, 0.086) 0px 2px 0px 0px;
+}
+
+.dispatch-side-card {
+  backdrop-filter: blur(12px) saturate(190%) contrast(50%) brightness(130%) !important;
+  border: 0.5px solid rgb(216, 216, 216) !important;
+  border-radius: 8px !important;
+  box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px !important;
+  color: rgb(60, 65, 73) !important;
+  opacity: 2 !important;
+}
+
+.item-title-font {
+  font-size: 13px !important;
+}
+</style>
