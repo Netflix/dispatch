f543b697f62f475493da60e769a21d48c82a5283
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-11-29 07:36:14 -0800

    Add escalate button

---
+++ src/dispatch/static/dispatch/src/components/EscalateButton.vue
@@ -0,0 +1,26 @@
+<!-- EscalateButton.vue -->
+<template>
+  <div>
+    <FancyTooltip text="Escalate case" :hotkeys="['E']">
+      <template #activator="{ tooltip }">
+        <v-btn variant="plain" :ripple="false" v-bind="tooltip" @click="showDialog">
+          <v-icon>mdi-fire</v-icon>
+        </v-btn>
+      </template>
+    </FancyTooltip>
+    <CaseEscalateDialog />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from "vuex"
+import FancyTooltip from "@/components/FancyTooltip.vue"
+import CaseEscalateDialog from "@/case/EscalateDialog.vue"
+
+const store = useStore()
+
+const showDialog = () => {
+  const caseDetails = store.state.case_management.selected
+  store.dispatch("case_management/showEscalateDialog", caseDetails)
+}
+</script>
