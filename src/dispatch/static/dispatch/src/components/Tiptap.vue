91c7497c1d9345607df54c65b385fd8abe150957
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-11-29 07:36:27 -0800

    Add new tiptap component

--- src/dispatch/static/dispatch/src/components/Tiptap.vue
+++ src/dispatch/static/dispatch/src/components/Tiptap.vue
@@ -1,18 +1,38 @@
+<template>
+  <div>
+    <editor-content :editor="editor" @keydown.enter.prevent />
+  </div>
+</template>
+
 <script setup>
 import { ref, onMounted, onBeforeUnmount, watch } from "vue"
-import { Editor, EditorContent, BubbleMenu } from "@tiptap/vue-3"
+import { Editor, EditorContent } from "@tiptap/vue-3"
 import StarterKit from "@tiptap/starter-kit"
+import CaseApi from "@/case/api"
+import { useStore } from "vuex"
 
 const props = defineProps({
   modelValue: {
     type: String,
     default: "",
   },
+  title: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: Boolean,
+    default: false,
+  },
+  resolution: {
+    type: Boolean,
+    default: false,
+  },
 })
 
-const emit = defineEmits(["update:modelValue"])
-
+const store = useStore()
 const editor = ref(null)
+const plainTextValue = ref("")
 
 watch(
   () => props.modelValue,
@@ -23,16 +43,41 @@ watch(
       return
     }
 
-    editor.value?.chain().focus().setContent(value, false).run()
+    if (props.title) {
+      editor.value?.chain().focus().setContent(`<h2>${value}</h2>`, false).run()
+    } else {
+      editor.value?.chain().focus().setContent(`${value}`, false).run()
+    }
   }
 )
 
 onMounted(() => {
   editor.value = new Editor({
-    extensions: [StarterKit],
+    extensions: [StarterKit.configure({ heading: { levels: [2] } })],
     content: props.modelValue,
     onUpdate: () => {
-      emit("update:modelValue", editor.value?.getHTML())
+      let content = editor.value?.getHTML()
+      // remove the HTML tags
+      plainTextValue.value = content.replace(/<\/?[^>]+(>|$)/g, "")
+      // Get the case details from the Vuex store
+      const caseDetails = store.state.case_management.selected
+      // Update the title value
+      if (props.title) {
+        caseDetails.title = plainTextValue.value
+      }
+
+      if (props.description) {
+        caseDetails.description = plainTextValue.value
+      }
+
+      if (props.resolution) {
+        caseDetails.resolution = plainTextValue.value
+      }
+      // Save the changes
+      CaseApi.update(caseDetails.id, caseDetails)
+    },
+    keyboardShortcuts: {
+      Enter: () => {}, // Override Enter key to do nothing
     },
   })
 })
@@ -40,42 +85,17 @@ onMounted(() => {
 onBeforeUnmount(() => {
   editor.value?.destroy()
 })
+
+// Expose plainTextValue for parent component to use
+defineExpose({ plainTextValue })
 </script>
 
-<template>
-  <div>
-    <bubble-menu :editor="editor" v-if="editor">
-      <v-btn-toggle v-model="toggle" color="primary" variant="outlined" density="compact">
-        <v-btn
-          @click="editor.chain().focus().toggleBold().run()"
-          :class="{ 'is-active': editor.isActive('bold') }"
-        >
-          <v-icon size="large">mdi-format-bold</v-icon>
-        </v-btn>
-        <v-btn
-          @click="editor.chain().focus().toggleItalic().run()"
-          :class="{ 'is-active': editor.isActive('italic') }"
-          icon="mdi-format-italic"
-        ></v-btn>
-        <v-btn
-          @click="editor.chain().focus().toggleStrike().run()"
-          :class="{ 'is-active': editor.isActive('strike') }"
-          icon="mdi-format-strikethrough-variant"
-        ></v-btn>
-      </v-btn-toggle>
-    </bubble-menu>
-    <editor-content :editor="editor" />
-  </div>
-</template>
-
-<style>
-.bubble-menu {
-  backdrop-filter: blur(12px) saturate(190%) contrast(50%) brightness(130%) !important;
-  border: 0.5px solid rgb(216, 216, 216) !important;
-  border-radius: 8px !important;
-  box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px !important;
-  color: rgb(60, 65, 73) !important;
-  opacity: 2 !important;
+<style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  > * + * {
+    margin-top: 0.75em;
+  }
 }
 
 input[type="checkbox"] {
