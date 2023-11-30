6c0e3a00faee3f1d0717d5382941c71d086ac2bc
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-11-30 08:14:14 -0800

    Polish row/col sizing of signal tab and move resolution up in drawer

--- src/dispatch/static/dispatch/src/case/CaseAttributesDrawer.vue
+++ src/dispatch/static/dispatch/src/case/CaseAttributesDrawer.vue
@@ -141,6 +141,31 @@ const saveCaseDetails = async () => {
 
       <v-divider class="mt-8 mb-8" />
 
+      <div class="pl-6 dispatch-font-title">Investigation Write-Up</div>
+      <div class="pl-6 dispatch-font">
+        Document your findings and provide the rationale for any decisions you made as part of this
+        case.
+      </div>
+
+      <v-card flat color="grey-lighten-5" class="rounded-lg mt-6 ml-2 mr-2">
+        <RichEditor
+          :resolution="true"
+          v-model="modelValue.resolution"
+          @update:model-value="handleResolutionUpdate"
+          style="min-height: 400px; margin: 10px; font-size: 0.9125rem; font-weight: 400"
+        />
+        <v-row class="pb-2 pr-4 pl-4">
+          <v-col cols="12" class="d-flex justify-end align-center">
+            <CaseResolutionSearchPopover
+              :case-resolution="modelValue.resolution_reason"
+              class="pl-6"
+            />
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-divider class="mt-8 mb-8" />
+
       <div class="pl-3">
         <v-row no-gutters align="center">
           <v-col cols="8">
@@ -188,7 +213,7 @@ const saveCaseDetails = async () => {
           </v-col>
         </v-row>
 
-        <v-row no-gutters class="pt-6" align="center">
+        <v-row no-gutters class="pt-6 pb-6" align="center">
           <v-col cols="8">
             <v-btn
               class="text-subtitle-2 font-weight-regular"
@@ -203,31 +228,6 @@ const saveCaseDetails = async () => {
           </v-col>
         </v-row>
       </div>
-
-      <v-divider class="mt-8 mb-8" />
-
-      <div class="pl-6 dispatch-font-title">Investigation Write-Up</div>
-      <div class="pl-6 dispatch-font">
-        Document your findings and provide the rationale for any decisions you made as part of this
-        case.
-      </div>
-
-      <v-card flat color="grey-lighten-5" class="rounded-lg mt-6 ml-2 mr-2">
-        <RichEditor
-          :resolution="true"
-          v-model="modelValue.resolution"
-          @update:model-value="handleResolutionUpdate"
-          style="min-height: 400px; margin: 10px; font-size: 0.9125rem; font-weight: 400"
-        />
-        <v-row class="pb-2 pr-4 pl-4">
-          <v-col cols="12" class="d-flex justify-end align-center">
-            <CaseResolutionSearchPopover
-              :case-resolution="modelValue.resolution_reason"
-              class="pl-6"
-            />
-          </v-col>
-        </v-row>
-      </v-card>
     </v-navigation-drawer>
   </div>
 </template>
