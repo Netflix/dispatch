156b5f9e4dbab42909c522cabbfa134d28640105
David Whittaker <84562015+whitdog47@users.noreply.github.com>
2023-12-18 11:51:33 -0800

    Restricting oncall feedback permission (#4144)

--- src/dispatch/static/dispatch/src/feedback/service/store.js
+++ src/dispatch/static/dispatch/src/feedback/service/store.js
@@ -53,10 +53,14 @@ const actions = {
       { ...state.table.options },
       "ServiceFeedback"
     )
-    return ServiceFeedbackApi.getAll(params).then((response) => {
-      commit("SET_TABLE_LOADING", false)
-      commit("SET_TABLE_ROWS", response.data)
-    })
+    return ServiceFeedbackApi.getAll(params)
+      .then((response) => {
+        commit("SET_TABLE_LOADING", false)
+        commit("SET_TABLE_ROWS", response.data)
+      })
+      .catch(() => {
+        commit("SET_TABLE_LOADING", false)
+      })
   }, 500),
   removeShow({ commit }, feedback) {
     commit("SET_DIALOG_DELETE", true)
