3a2f36abcc5e48ca112755f5fa1ee236d2b1f986
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2023-12-19 09:39:12 -0800

    Add IndividualContact interface in TypeScript

---
+++ src/dispatch/static/dispatch/src/individual/IndividualContact.ts
@@ -0,0 +1,19 @@
+export interface IndividualContact {
+  company: string | null
+  contact_type: string | null
+  created_at: string
+  email: string
+  external_id: string | null
+  filters: any[]
+  id: number
+  is_active: boolean
+  is_external: boolean
+  mobile_phone: string | null
+  name: string
+  notes: string | null
+  office_phone: string | null
+  owner: string | null
+  title: string | null
+  updated_at: string
+  weblink: string
+}
