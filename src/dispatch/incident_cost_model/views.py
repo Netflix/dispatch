58e2dec3483c2ca28cbb41dbec99747cce8d914d
Avery Lee <averyl@netflix.com>
2024-01-05 10:38:54 -0800

    Code cleanup.

--- src/dispatch/incident_cost_model/views.py
+++ src/dispatch/incident_cost_model/views.py
@@ -23,22 +23,6 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 
-def get_current_incident_cost_model(db_session: DbSession, request: Request) -> IncidentCostModel:
-    """Fetches the current incident cost model or returns a 404."""
-    incident_cost_model = get_cost_model_by_id(
-        db_session=db_session, incident_cost_model_id=request.path_params["incident_cost_model_id"]
-    )
-    if not incident_cost_model:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=[{"msg": "An incident cost model with this id does not exist."}],
-        )
-    return incident_cost_model
-
-
-CurrentIncidentCostModel = Annotated[IncidentCostModel, Depends(get_current_incident_cost_model)]
-
-
 @router.get("", response_model=IncidentCostModelPagination)
 def get_incident_cost_models(common: CommonParameters):
     """Get all incident cost models, or only those matching a given search term."""
@@ -83,11 +67,10 @@ def update_incident_cost_model(
 def delete_incident_cost_model(
     incident_cost_model_id: PrimaryKey,
     db_session: DbSession,
-    current_incident_cost_model: CurrentIncidentCostModel,
 ):
     """Deletes an incident cost model and its external resources."""
     try:
-        delete(incident_cost_model_id=current_incident_cost_model.id, db_session=db_session)
+        delete(incident_cost_model_id=incident_cost_model_id, db_session=db_session)
     except IntegrityError as e:
         log.exception(e)
         raise HTTPException(
@@ -95,7 +78,7 @@ def delete_incident_cost_model(
             detail=[
                 {
                     "msg": (
-                        f"Incident Cost Model {current_incident_cost_model.name} could not be deleted. "
+                        f"Incident Cost Model with id {incident_cost_model_id} could not be deleted. "
                     )
                 }
             ],
