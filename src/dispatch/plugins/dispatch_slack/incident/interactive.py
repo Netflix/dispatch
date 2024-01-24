eb886c7d8da2acfa8dc9c1085a2766e8ff61d959
Avery Lee <averyl@netflix.com>
2024-01-24 14:14:52 -0800

    Do not add cost model select if none exist.

--- src/dispatch/plugins/dispatch_slack/incident/interactive.py
+++ src/dispatch/plugins/dispatch_slack/incident/interactive.py
@@ -321,7 +321,9 @@ def handle_update_incident_project_select_action(
         ),
         cost_model_select(
             db_session=db_session,
-            initial_option={"text": incident.cost_model.name, "value": incident.cost_model.id},
+            initial_option={"text": incident.cost_model.name, "value": incident.cost_model.id}
+            if incident.cost_model
+            else None,
             project_id=incident.project.id,
             optional=True,
         ),
@@ -1819,15 +1821,19 @@ def handle_update_incident_command(
             optional=True,
             initial_options=[{"text": t.name, "value": t.id} for t in incident.tags],
         ),
-        cost_model_select(
-            db_session=db_session,
-            initial_option={"text": incident.cost_model.name, "value": incident.cost_model.id}
-            if incident.cost_model
-            else None,
-            project_id=incident.project.id,
-            optional=True,
-        ),
     ]
+
+    cost_model_block = cost_model_select(
+        db_session=db_session,
+        initial_option={"text": incident.cost_model.name, "value": incident.cost_model.id}
+        if incident.cost_model
+        else None,
+        project_id=incident.project.id,
+        optional=True,
+    )
+
+    if cost_model_block:
+        blocks.append(cost_model_block)
 
     modal = Modal(
         title="Update Incident",
@@ -1874,10 +1880,12 @@ def handle_update_incident_submission_event(
         tag = tag_service.get(db_session=db_session, tag_id=int(t["value"]))
         tags.append(tag)
 
-    cost_model = cost_model_service.get_cost_model_by_id(
-        db_session=db_session,
-        cost_model_id=int(form_data[DefaultBlockIds.cost_model_select]["value"]),
-    )
+    cost_model = None
+    if form_data.get(DefaultBlockIds.cost_model_select):
+        cost_model = cost_model_service.get_cost_model_by_id(
+            db_session=db_session,
+            cost_model_id=int(form_data[DefaultBlockIds.cost_model_select]["value"]),
+        )
     incident_in = IncidentUpdate(
         title=form_data[DefaultBlockIds.title_input],
         description=form_data[DefaultBlockIds.description_input],
