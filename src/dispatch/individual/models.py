79c0c35748e8faa8595fc8fa6d70c0b4143bf99b
kevgliss <kevgliss@gmail.com>
2024-02-05 12:10:12 -0800

    Adds http url validation to all weblinks (#4340)

--- src/dispatch/individual/models.py
+++ src/dispatch/individual/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from typing import List, Optional
-from pydantic import Field
+from typing import List, Optional, Union
+from pydantic import Field, HttpUrl, validator
 
 from sqlalchemy import Column, ForeignKey, Integer, PrimaryKeyConstraint, String, Table
 from sqlalchemy.sql.schema import UniqueConstraint
@@ -63,11 +63,17 @@ class IndividualContact(Base, ContactMixin, ProjectMixin):
 
 
 class IndividualContactBase(ContactBase):
-    weblink: Optional[str] = Field(None, nullable=True)
+    weblink: Union[HttpUrl, None, str] = Field(None, nullable=True)
     mobile_phone: Optional[str] = Field(None, nullable=True)
     office_phone: Optional[str] = Field(None, nullable=True)
     title: Optional[str] = Field(None, nullable=True)
     external_id: Optional[str] = Field(None, nullable=True)
+
+    @validator("weblink")
+    def weblink_validator(cls, v):
+        if v is None or isinstance(v, HttpUrl) or v == "":
+            return v
+        raise ValueError("weblink is not an empty string or a valid weblink")
 
 
 class IndividualContactCreate(IndividualContactBase):
