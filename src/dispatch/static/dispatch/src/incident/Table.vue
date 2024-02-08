85c3cf4c95aa23115e9b51242f1d1cfe392bf6d8
David Whittaker <84562015+whitdog47@users.noreply.github.com>
2024-02-08 09:56:27 -0800

    Change new button on incident and case pages to point to report form (#4374)

--- src/dispatch/static/dispatch/src/incident/Table.vue
+++ src/dispatch/static/dispatch/src/incident/Table.vue
@@ -14,7 +14,10 @@
       <v-col class="text-right">
         <table-filter-dialog :projects="defaultUserProjects" />
         <table-export-dialog />
-        <v-btn color="info" class="ml-2" @click="showNewSheet()"> New </v-btn>
+        <v-btn nav variant="flat" color="error" :to="{ name: 'report' }" class="ml-2" hide-details>
+          <v-icon start color="white">mdi-fire</v-icon>
+          <span class="text-uppercase text-body-2 font-weight-bold">Report incident</span>
+        </v-btn>
       </v-col>
     </v-row>
     <v-row no-gutters>
@@ -231,7 +234,7 @@ export default {
   },
 
   methods: {
-    ...mapActions("incident", ["getAll", "showNewSheet", "showDeleteDialog", "showReportDialog"]),
+    ...mapActions("incident", ["getAll", "showDeleteDialog", "showReportDialog"]),
     ...mapActions("workflow", ["showRun"]),
     showIncidentEditSheet(e, { item }) {
       this.$router.push({ name: "IncidentTableEdit", params: { name: item.name } })
