f5abfb763b15c7ac6740f8c1f783d6cfe1984df8
David Whittaker <dwhittaker@netflix.com>
2024-03-05 13:21:41 -0800

    Adding incident resources to export

--- src/dispatch/static/dispatch/src/util/index.js
+++ src/dispatch/static/dispatch/src/util/index.js
@@ -38,7 +38,19 @@ const exportCSV = function (items, fileName) {
   link.click()
 }
 
+// usage: fieldOrder is a list of field names in the order you want them to appear in the CSV
+const exportCSVOrdered = function (items, fileName, fieldOrder) {
+  const json2csvParser = new Parser({ transforms: [flatten()], fields: fieldOrder })
+  const csv = json2csvParser.parse(items)
+  const data = "data:text/csv;charset=utf-8," + encodeURIComponent(csv)
+  const link = document.createElement("a")
+  link.setAttribute("href", data)
+  link.setAttribute("download", fileName)
+  link.click()
+}
+
 export default {
   toggleFullScreen,
   exportCSV,
+  exportCSVOrdered,
 }
