b46b3b1417b2d1b1a54a4196e916d68f05c001d9
David Whittaker <84562015+whitdog47@users.noreply.github.com>
2024-04-23 15:41:46 -0700

    Fixing Report Case card to ensure case type reflows on project change (#4651)

--- src/dispatch/static/dispatch/src/case/type/CaseTypeSelect.vue
+++ src/dispatch/static/dispatch/src/case/type/CaseTypeSelect.vue
@@ -129,31 +129,26 @@ export default {
       this.loading = "error"
 
       let filterOptions = {
-        q: "",
         sortBy: ["name"],
         descending: [false],
-        itemsPerPage: -1,
+        itemsPerPage: this.numItems,
       }
 
       if (this.project) {
         filterOptions = {
+          ...filterOptions,
           filters: {
             project: [this.project],
             enabled: ["true"],
           },
-          ...filterOptions,
         }
       }
 
       filterOptions = SearchUtils.createParametersFromTableOptions({ ...filterOptions })
 
-      if (this.items.length == 0) {
-        CaseTypeApi.getAll(filterOptions).then((response) => {
-          this.items = response.data.items
-        })
-      }
-      filterOptions.itemsPerPage = this.numItems
       CaseTypeApi.getAll(filterOptions).then((response) => {
+        this.items = response.data.items
+
         this.total = response.data.total
         this.loading = false
 
