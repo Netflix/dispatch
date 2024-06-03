1d763be05852b0fc4be32bc5132392f9a210766d
Jason Schroth <jrobsch@hotmail.com>
2024-06-03 15:53:40 -0700

    add device push check

--- src/dispatch/plugins/dispatch_duo/plugin.py
+++ src/dispatch/plugins/dispatch_duo/plugin.py
@@ -91,6 +91,10 @@ class DuoMfaPlugin(MultiFactorAuthenticationPlugin):
         elif userstatus["result"] == "allow":
             return PushResponseResult.allow
         elif userstatus["result"] == "auth":
+            push_devs = [row.get("device") for row in userstatus.get("devices") if "push" in row.get("capabilities")]
+            if len(push_devs) < 1:
+                log.error(f"ERROR: Duo account found for {username}, but no devices support Push")
+                return PushResponseResult.deny
             try:
                 response = duo_client.auth(factor="push", username=username, device=device, type=type)
             except RuntimeError as e:
