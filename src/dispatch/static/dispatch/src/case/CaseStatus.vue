0bcb2be0e959339d504c04c509b4a3c0c73cd464
Will Sheldon <114631109+wssheldon@users.noreply.github.com>
2024-07-02 13:38:42 -0400

    feat: add self-join button and api to cases (#4907)
    * feat: add self-join button and api to cases
    
    * only allow adding to dedicated channel cases
    
    * change references to incident logic and language to cases
    
    * reference to incident id in CaseJoinPermission
    
    * remove unused value and setup map actions in caseStatus.vue

--- src/dispatch/static/dispatch/src/case/CaseStatus.vue
+++ src/dispatch/static/dispatch/src/case/CaseStatus.vue
@@ -1,10 +1,28 @@
 <template>
-  <v-badge bordered :color="color" dot location="left" offset-x="-16">
-    {{ status }}
-  </v-badge>
+  <div class="text-no-wrap">
+    <v-badge bordered :color="color" dot location="left" offset-x="-16">
+      {{ status }}
+    </v-badge>
+    <template v-if="status !== 'Closed' && dedicatedChannel">
+      <v-tooltip location="bottom" text="Join" v-if="allowSelfJoin">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            icon="mdi-account-plus"
+            variant="text"
+            density="comfortable"
+            class="ml-1"
+            @click.stop="joinCase(id)"
+          />
+        </template>
+      </v-tooltip>
+    </template>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex"
+
 export default {
   name: "CaseStatus",
 
@@ -15,6 +33,14 @@ export default {
     },
     id: {
       type: Number,
+      required: true,
+    },
+    allowSelfJoin: {
+      type: Boolean,
+      required: true,
+    },
+    dedicatedChannel: {
+      type: Boolean,
       required: true,
     },
   },
@@ -30,6 +56,10 @@ export default {
         }[this.status] || "error"
       )
     },
+  },
+
+  methods: {
+    ...mapActions("case_management", ["joinCase"]),
   },
 }
 </script>
