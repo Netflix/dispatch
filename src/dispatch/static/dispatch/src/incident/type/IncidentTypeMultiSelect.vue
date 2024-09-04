cb36204532b4583eb44426e29de1b6bd402ff335
David Whittaker <dwhittaker@netflix.com>
2024-09-04 15:06:28 -0700

    Fixes selection filtering to project

--- src/dispatch/static/dispatch/src/incident/type/IncidentTypeMultiSelect.vue
+++ src/dispatch/static/dispatch/src/incident/type/IncidentTypeMultiSelect.vue
@@ -96,6 +96,7 @@ export default {
 
     filterOptions = SearchUtils.createParametersFromTableOptions(
       { ...filterOptions },
+      "IncidentType",
       enabledFilter
     )
 
--- src/dispatch/static/dispatch/src/workflow/WorkflowSelect.vue
+++ src/dispatch/static/dispatch/src/workflow/WorkflowSelect.vue
@@ -88,6 +88,7 @@ export default {
 
       filterOptions = SearchUtils.createParametersFromTableOptions(
         { ...filterOptions },
+        "Workflow",
         enabledFilter
       )
 
