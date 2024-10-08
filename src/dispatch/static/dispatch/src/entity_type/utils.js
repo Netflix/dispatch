0a069b879fff35b53dd9b5ab16e2a76d903b0c14
Avery Lee <averyl@netflix.com>
2024-10-08 11:01:58 -0700

    Does not execute jpath if it is not sanitized.

--- src/dispatch/static/dispatch/src/entity_type/utils.js
+++ src/dispatch/static/dispatch/src/entity_type/utils.js
@@ -9,8 +9,27 @@ export function isValidRegex(pattern) {
   }
 }
 
+function sanitizeString(str) {
+  return str.replace(/[&<>"'`=/]/g, function (char) {
+    return {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&#39;",
+      "`": "&#x60;",
+      "=": "&#x3D;",
+      "/": "&#x2F;",
+    }[char]
+  })
+}
+
 export function isValidJsonPath(jpath) {
   try {
+    if (sanitizeString(jpath) !== jpath) {
+      return false
+    }
+
     jsonpath.parse(jpath)
     return true
   } catch (e) {
