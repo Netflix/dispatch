cfa072b7f5a44ad38949982df506174eec8e6eae
David Whittaker <dwhittaker@netflix.com>
2024-10-17 15:11:16 -0700

    removing incident id

--- src/dispatch/feedback/incident/service.py
+++ src/dispatch/feedback/incident/service.py
@@ -16,7 +16,7 @@ def get(*, db_session, feedback_id: int) -> Optional[Feedback]:
 
 def get_all(*, db_session):
     """Gets all pieces of feedback."""
-    return db_session.query(Feedback).filter(Feedback.incident_id.isnot(None)).all()
+    return db_session.query(Feedback)
 
 
 def get_all_last_x_hours_by_project_id(
