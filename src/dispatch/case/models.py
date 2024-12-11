c03f15ec8c04e11acca9b19ba715fecbe3d8646a
Avery Lee <averyl@netflix.com>
2024-12-10 18:08:51 -0800

    Attempt to move include parameter in GET requests to Cases into database service.

--- src/dispatch/case/models.py
+++ src/dispatch/case/models.py
@@ -288,8 +288,6 @@ class CaseReadMinimal(CaseBase):
     project: ProjectRead
     reporter: Optional[ParticipantReadMinimal]
     reported_at: Optional[datetime] = None
-    tags: Optional[List[TagRead]] = []
-    ticket: Optional[TicketRead] = None
     total_cost: float | None
     triage_at: Optional[datetime] = None
 
