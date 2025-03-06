cc31b4b6e268500ccc6f9438484835f5a83f232b
Marc Vilanova <mvilanova@netflix.com>
2025-03-06 14:46:21 -0800

    revert

--- src/dispatch/monitor/flows.py
+++ src/dispatch/monitor/flows.py
@@ -19,6 +19,9 @@ def send_monitor_notification(
     project_id: int, conversation_id: int, message_template: str, db_session: Session, **kwargs
 ):
     """Sends a monitor notification."""
+    notification_text = "Incident Notification"
+    notification_type = "incident-notification"
+
     plugin = plugin_service.get_active_instance(
         db_session=db_session, project_id=project_id, plugin_type="conversation"
     )
@@ -26,4 +29,6 @@ def send_monitor_notification(
         log.warning("Monitor notification not sent. No conversation plugin enabled.")
         return
 
-    plugin.instance.send(conversation_id, "Monitor Notification", message_template, **kwargs)
+    plugin.instance.send(
+        conversation_id, notification_text, message_template, notification_type, **kwargs
+    )
