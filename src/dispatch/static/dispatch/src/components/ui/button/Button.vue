4daeb3221bc8917d2edb3a590a4a8e064e13ba05
Will Sheldon <wshel@netflix.com>
2025-04-18 12:57:42 -0700

    ui(components): add shadcn-vue and tailwind v4

--- src/dispatch/static/dispatch/src/components/ui/button/Button.vue
+++ src/dispatch/static/dispatch/src/components/ui/button/Button.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
+import { cn } from '@/utils/tw'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
 import { type ButtonVariants, buttonVariants } from '.'
 
