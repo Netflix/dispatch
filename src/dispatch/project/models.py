5f5bc2c2bec79fcca62dd9ba794e453035571342
Marc Vilanova <mvilanova@netflix.com>
2025-05-07 15:47:27 -0700

    snooze_extension_oncall_service

--- src/dispatch/project/models.py
+++ src/dispatch/project/models.py
@@ -122,7 +122,7 @@ class ProjectBase(DispatchBase):
     report_incident_instructions: Optional[str] = Field(None, nullable=True)
     report_incident_title_hint: Optional[str] = Field(None, nullable=True)
     report_incident_description_hint: Optional[str] = Field(None, nullable=True)
-    snooze_extension_oncall_service: Optional[Service]
+    snooze_extension_oncall_service: Optional[Service] = None
 
 
 class ProjectCreate(ProjectBase):
