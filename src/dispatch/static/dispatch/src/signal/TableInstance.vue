8ab0e4117c1a3a0005da9f8142444b5c39ea18d5
Alicia Matsumoto <amats@netflix.com>
2025-05-29 23:25:43 -0400

    buttons

--- src/dispatch/static/dispatch/src/signal/TableInstance.vue
+++ src/dispatch/static/dispatch/src/signal/TableInstance.vue
@@ -1,10 +1,41 @@
 <template>
   <v-container fluid>
-    <v-row no-gutters>
+   <v-row no-gutters>
       <v-col>
         <div class="text-h5">Signals</div>
       </v-col>
-      <v-col class="text-right">
+    </v-row>
+    <v-row no-gutters class="pb-3" align="center">
+      <v-col cols="auto" class="pr-4"><div class="text">Group by:</div></v-col>
+      <v-col cols="auto" class="pr-4">
+        <v-btn
+          :class="activeView === 'triggers' ? 'selectedViewButton' : 'viewButton'"
+          @click="setActiveView('triggers')"
+        >
+          <v-icon>mdi-broadcast</v-icon>
+          <div class="pl-1">Triggers</div>
+        </v-btn>
+      </v-col>
+      <!--      todo (amats) we should have routes to allow navigation to a certain tab via link-->
+      <v-col cols="auto" class="pr-4">
+        <v-btn
+          :class="activeView === 'entities' ? 'selectedViewButton' : 'viewButton'"
+          @click="setActiveView('entities')"
+        >
+          <v-icon>mdi-cube-outline</v-icon>
+          <div class="pl-1">Entities</div>
+        </v-btn>
+      </v-col>
+      <v-col cols="auto" class="pr-4">
+        <v-btn
+          :class="activeView === 'snoozes' ? 'selectedViewButton' : 'viewButton'"
+          @click="setActiveView('snoozes')"
+        >
+          <v-icon>mdi-bell-off</v-icon>
+          <div class="pl-1">Snoozes</div>
+        </v-btn>
+      </v-col>
+      <v-col class="text-right" style="padding-right: 4px">
         <table-filter-dialog />
       </v-col>
     </v-row>
